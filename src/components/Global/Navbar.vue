475befe80a8c33faedef10d28cfecb1970796d06
Hideo Hattori <hhatto.jp@gmail.com>
2022-05-07 10:52:39 +0900

    fix

--- src/components/Global/Navbar.vue
+++ src/components/Global/Navbar.vue
@@ -1,27 +1,9 @@
 <template>
   <header
-    class="
-      text-black
-      dark:border-b-0 dark:text-white dark:shadow-xl
-      burger:flex
-      burger:items-center
-      burger:justify-between
-      burger:px-4
-      burger:py-3
-    "
+    class="text-black dark:border-b-0 dark:text-white dark:shadow-xl burger:flex burger:items-center burger:justify-between burger:px-4 burger:py-3"
   >
     <div
-      class="
-        flex
-        w-full
-        flex-wrap
-        items-center
-        justify-between
-        px-4
-        py-3
-        align-baseline
-        burger:flex-nowrap burger:p-0
-      "
+      class="flex w-full flex-wrap items-center justify-between px-4 py-3 align-baseline burger:flex-nowrap burger:p-0"
     >
       <a href="/" class="py-4 text-gray-900 dark:text-white md:p-4">D Club</a>
     </div>
