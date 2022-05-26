455ae9eb89a39c4d2ceea00d2cba0ca05a434ccb
aggre <hiroyuki.aggre@gmail.com>
2022-05-26 13:37:45 +0900

    use the new clients api (#12)

--- src/fixtures/dev-kit/index.ts
+++ src/fixtures/dev-kit/index.ts
@@ -1,14 +1,5 @@
 import { providers } from 'ethers'
-import {
-  contractFactory,
-  DevkitContract,
-  addresses,
-} from '@devprotocol/dev-kit'
-import {
-  contractFactory as l2ContractFactory,
-  DevkitContract as L2DevkitContract,
-  createPropertyContract,
-} from '@devprotocol/dev-kit/l2'
+import { clientsSTokens, clientsProperty } from '@devprotocol/dev-kit/agent'
 import { UndefinedOr } from '@devprotocol/util-ts'
 
 export type ChainName = UndefinedOr<
@@ -19,28 +10,6 @@ export type ChainName = UndefinedOr<
   | 'polygon'
   | 'polygon-mumbai'
 >
-
-const newClient = async (
-  prov: providers.BaseProvider
-): Promise<
-  [
-    UndefinedOr<DevkitContract>,
-    UndefinedOr<L2DevkitContract>,
-    UndefinedOr<DevkitContract | L2DevkitContract>
-  ]
-> => {
-  const isL2 = await getNetwork(prov).then(
-    (name) => name !== 'ethereum' && name !== 'ropsten'
-  )
-  const contracts = !isL2 ? contractFactory(prov) : undefined
-  const l2Contracts = isL2 ? l2ContractFactory(prov) : undefined
-  return [contracts, l2Contracts, contracts || l2Contracts]
-}
-
-const getNetwork = async (prov: providers.BaseProvider) => {
-  const net = await detectChain(prov)
-  return net.name
-}
 
 export const detectChain = async (ethersProvider?: providers.BaseProvider) => {
   const res = await ethersProvider?.getNetwork()
@@ -63,72 +32,36 @@ export const detectChain = async (ethersProvider?: providers.BaseProvider) => {
   return { chainId, name }
 }
 
-const getSTokensAddress = async (prov: providers.BaseProvider) => {
-  const net = await getNetwork(prov)
-  return net === 'ethereum'
-    ? addresses.eth.main.sTokens
-    : net === 'ropsten'
-    ? addresses.eth.ropsten.sTokens
-    : net === 'arbitrum-one'
-    ? addresses.arbitrum.one.sTokens
-    : net === 'arbitrum-rinkeby'
-    ? addresses.arbitrum.rinkeby.sTokens
-    : net === 'polygon'
-    ? addresses.polygon.mainnet.sTokens
-    : net === 'polygon-mumbai'
-    ? addresses.polygon.mumbai.sTokens
-    : undefined
-}
-
 export const getStokenPositions = async (
   prov: providers.BaseProvider,
   sTokenID: number
 ) => {
-  const [, , client] = await newClient(prov)
-  const address = await getSTokensAddress(prov)
-  if (client && address) {
-    return client.sTokens(address).positions(sTokenID)
-  }
-  return undefined
+  const [l1, l2] = await clientsSTokens(prov)
+  return (l1 || l2)?.positions(sTokenID)
 }
 
 export const getStokenOwnerOf = async (
   prov: providers.BaseProvider,
   sTokenID: number
 ) => {
-  const [, , client] = await newClient(prov)
-  const address = await getSTokensAddress(prov)
-  if (client && address) {
-    return client.sTokens(address).ownerOf(sTokenID)
-  }
-  return undefined
+  const [l1, l2] = await clientsSTokens(prov)
+  return (l1 || l2)?.ownerOf(sTokenID)
 }
 
 export const getStokenTokenURI = async (
   prov: providers.BaseProvider,
   sTokenID: number
 ) => {
-  const [, , client] = await newClient(prov)
-  const address = await getSTokensAddress(prov)
-  if (client && address) {
-    return client.sTokens(address).tokenURI(sTokenID)
-  }
-  return undefined
+  const [l1, l2] = await clientsSTokens(prov)
+  return (l1 || l2)?.tokenURI(sTokenID)
 }
 
 export const detectStokensByPropertyAddress = async (
   prov: providers.BaseProvider,
   propertyAddress: string
 ) => {
-  const [, , client] = await newClient(prov)
-  const address = await getSTokensAddress(prov)
-  if (client && address) {
-    const TokenIdList = await (
-      client.sTokens(address) as any
-    ).positionsOfProperty(propertyAddress)
-    return TokenIdList
-  }
-  return undefined
+  const [l1, l2] = await clientsSTokens(prov)
+  return (l1 || l2)?.positionsOfProperty(propertyAddress)
 }
 
 export const balanceOfProperty = async (
@@ -136,9 +69,9 @@ export const balanceOfProperty = async (
   propertyAddress: string,
   accountAddress: string
 ) => {
-  const [, , client] = await newClient(prov)
-  if (client && accountAddress) {
-    return client.property(propertyAddress).balanceOf(accountAddress)
+  const [l1, l2] = await clientsProperty(prov, propertyAddress)
+  if (accountAddress) {
+    return (l1 || l2)?.balanceOf(accountAddress)
   }
   return undefined
 }
@@ -148,5 +81,6 @@ export const getBalances = async (
   propertyAddress: string
 ) => {
   // only for L2
-  return await createPropertyContract(prov)(propertyAddress).getBalances()
+  const [, l2] = await clientsProperty(prov, propertyAddress)
+  return l2?.getBalances()
 }
