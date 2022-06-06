1600be036f538f8fbd681bbec75274962c530d65
aggre <hiroyuki.aggre@gmail.com>
2022-06-06 19:48:46 +0900

    tweaks for perks

--- src/constants/tier.ts
+++ src/constants/tier.ts
@@ -2,6 +2,7 @@ export type Tier = Readonly<{
   title: string
   id: string
   amount: number | string
+  badgeImageDescription?: string
   badgeImageSrc?: string
 }>
 
@@ -12,20 +13,24 @@ export const tiers: Tiers = [
     title: 'Bronze',
     id: 'bronze',
     amount: 400,
+    badgeImageDescription: `"Yasuragi dan Renge (a Buddhist altar, lotus)" embossed gilt lacquer work with inlaid mother-of-pearl`,
   },
   {
     title: 'Silver',
     id: 'silver',
     amount: 4000,
+    badgeImageDescription: `"Yasuragi dan Koki (a Buddhist altar, brightness)" embossed gilt lacquer work`,
   },
   {
     title: 'Gold',
     id: 'gold',
     amount: 20000,
+    badgeImageDescription: `Sojiij Head Monastry : Nioh statue (statues of the two Deva kings)`,
   },
   {
     title: 'Super',
     id: 'super',
     amount: 80000,
+    badgeImageDescription: `Gokokuji= Head Monastry : Nyoirin Kannon statue (Cintāmaṇicakra statue)`,
   },
 ] as const
