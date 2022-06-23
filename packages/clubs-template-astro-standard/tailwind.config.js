5cf82aa6315ad30da10c19bccd5d3ea0e23ee6cd
Shubham Kukreti <shubhamkukreti204@gmail.com>
2022-06-23 18:05:48 +0530

    removed: unused components from clubs-template

--- packages/clubs-template-astro-standard/tailwind.config.js
+++ packages/clubs-template-astro-standard/tailwind.config.js
@@ -1,1 +1,8 @@
-module.exports = require('@clubs/clubs-core/presentations/config/tailwind/tailwind.config.js')
+module.exports = {
+  content: [
+    './node_modules/@clubs/clubs-core/presentations/components/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
+    './node_modules/@clubs/clubs-core/presentations/config/tailwind/tailwind.config.js',
+    'src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
+  ],
+  // ...
+}
