8e550ccfb25a3691500c320307ea37271ac92f6e
aggre <hiroyuki.aggre@gmail.com>
2022-07-28 19:56:23 +0900

    rename

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 export function middleware(req) {
+  console.log(`req.headers.get('host')`, req.headers.get('host'))
   const [site] = (req.headers.get('host') || 'demo.example.pub').split('.')
 
   if (site) {
