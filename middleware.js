cf0187fda11cadc0373cd3787b7616b147d4003f
aggre <hiroyuki.aggre@gmail.com>
2022-07-28 20:04:25 +0900

    test

--- middleware.js
+++ middleware.js
@@ -1,8 +1,8 @@
 export function middleware(req) {
-  console.log(`req.headers.get('host')`, req.headers.get('host'))
-  const [site] = (req.headers.get('host') || 'demo.example.pub').split('.')
+  console.log(req)
+  // const [site] = (req.headers.get('host') || 'demo.example.pub').split('.')
 
-  if (site) {
-    return new Response(new URL(`/${site}`, req.url))
-  }
+  // if (site) {
+  //   return new Response(new URL(`/${site}`, req.url))
+  // }
 }
