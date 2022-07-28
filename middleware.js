2de3389263fa6baf1afc399c94bcfbc0bacaeb1d
aggre <hiroyuki.aggre@gmail.com>
2022-07-29 07:38:52 +0900

    use next helper

--- middleware.js
+++ middleware.js
@@ -1,8 +1,28 @@
-export function middleware(req) {
-  console.log(req)
-  // const [site] = (req.headers.get('host') || 'demo.example.pub').split('.')
+import { NextResponse } from 'next/server'
 
-  // if (site) {
-  //   return new Response(new URL(`/${site}`, req.url))
-  // }
+export const config = {
+  matcher: [
+    '/',
+    '/([^/.]*)', // exclude `/public` files by matching all paths except for paths containing `.` (e.g. /logo.png)
+    '/_sites/:path*', // for all custom hostnames under the `/_sites/[site]*` dynamic route (demo.vercel.pub, platformize.co)
+  ],
+}
+
+export default function middleware(req) {
+  const url = req.nextUrl
+
+  const hostname = req.headers.get('host') || 'demo.vercel.pub'
+  const [tenant] = hostname.split('.')
+  const html = req.headers.get('accept')?.includes('text/html')
+
+  if (tenant === 'localhost' || tenant === 'clubsx') {
+    return req
+  }
+
+  if (html) {
+    url.pathname = `/_sites/${tenant}${url.pathname}`
+    return NextResponse.rewrite(url)
+  }
+
+  return req
 }
