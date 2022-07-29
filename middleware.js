99655bdcf38a6156c04a567b67e9dad1645866df
aggre <hiroyuki.aggre@gmail.com>
2022-07-30 01:17:49 +0900

    fix

--- middleware.js
+++ middleware.js
@@ -14,15 +14,16 @@ export default function middleware(req) {
   const [tenant] = hostname
   const html = req.headers.get('accept')?.includes('text/html')
 
-  if (html && hostname.length < 4) {
+  const headers = new Headers()
+  if (html && hostname.length > 3) {
     console.log('***', 3)
     url.pathname = `/_sites/${tenant}${url.pathname}`
-    const headers = new Headers()
     headers.set('x-middleware-rewrite', url.href)
     headers.set('x-rewrited-url', url.href)
     return new Response(null, { headers })
   }
 
   console.log('***', 4)
-  return new Response(null, null)
+  headers.set('x-middleware-rewrite', url.href)
+  return new Response(null, { headers })
 }
