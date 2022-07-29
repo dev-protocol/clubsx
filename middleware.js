e76105a3ee9d5e9da9c68099ee2d3eacf7aa0251
aggre <hiroyuki.aggre@gmail.com>
2022-07-30 02:01:30 +0900

    Fix/platform (#14)
    - Add Edge Middleware to rewrite the multi-tenant domain request. i.g., `https://abc.domain.xyz/me` -> `https://abc.domain.xyz/sites_/abc/me`
    - Astro cannot handle the path `_sites` correctly, so I renamed it to `sites_`.
    - On the local server, it hooks into `astro:server:setup` and rewrites the request URL.

---
+++ middleware.js
@@ -0,0 +1,25 @@
+export const config = {
+  matcher: [
+    '/',
+    '/([^/.]*)', // exclude `/public` files by matching all paths except for paths containing `.` (e.g. /logo.png)
+    '/sites_/:path*', // for all custom hostnames under the `/_sites/[site]*` dynamic route (demo.vercel.pub, platformize.co)
+  ],
+}
+
+export default function middleware(req) {
+  const url = new URL(req.url)
+
+  const hostname = (req.headers.get('host') || 'demo.vercel.pub').split('.')
+  const [tenant] = hostname
+  const html = req.headers.get('accept')?.includes('text/html')
+
+  const headers = new Headers()
+  if (html && hostname.length > 3) {
+    url.pathname = `/sites_/${tenant}${url.pathname}`
+    headers.set('x-middleware-rewrite', url.href)
+    headers.set('x-rewritten-url', url.href)
+    return new Response(null, { headers })
+  }
+
+  return new Response(null, { headers })
+}
---
+++ ugly.mjs
@@ -0,0 +1,12 @@
+import fs from 'fs-extra'
+
+const path =
+  './node_modules/@astrojs/vercel/dist/serverless/request-transform.js'
+fs.outputFileSync(
+  path,
+  ((file) =>
+    file.replace(
+      'base + req.url',
+      `headers['x-rewritten-url'] ? headers['x-rewritten-url'] : base + req.url`
+    ))(fs.readFileSync(path, 'utf8'))
+)
