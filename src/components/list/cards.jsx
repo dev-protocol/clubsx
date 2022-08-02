77257349dc941b0724814d982428ce623e36bacf
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-08-02 15:27:18 +0900

    make products constants

--- src/components/list/cards.jsx
+++ src/components/list/cards.jsx
@@ -14,74 +14,8 @@
   }
   ```
 */
-const products = [
-  {
-    id: 1,
-    name: 'Name',
-    href: '/buy/tier-3',
-    price: '0.3',
-    description:
-      'Get the full lineup of our Basic Tees. Have a fresh shirt all week, and an extra for laundry day.',
-    left: '32',
-    imageSrc:
-      'https://bafybeib745w7vjcsh37mepaluvbmqrbjq4gax46oirkmrjeqeh55gjiyzi.ipfs.nftstorage.link/',
-    imageAlt:
-      'Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.',
-  },
-  {
-    id: 2,
-    name: 'Name',
-    href: '/buy/tier-2',
-    price: '0.3',
-    left: '32',
-    imageSrc:
-      'https://bafybeic3d2otapykfdp3ktqbdonn4ylrove5ccs5vv2udydwbgudstrcwu.ipfs.nftstorage.link/',
-    imageAlt: 'Front of plain black t-shirt.',
-  },
-  {
-    id: 3,
-    name: 'Name',
-    href: '#',
-    price: '/buy/tier-1',
-    left: '32',
-    imageSrc:
-      'https://bafybeiagvn4exdbwokm4g6t6a2s3xvl2fu7zutvlf6sgwd4fjrdrc23hsu.ipfs.nftstorage.link/',
-    imageAlt: 'Front of plain black t-shirt.',
-  },
-  {
-    id: 4,
-    name: 'Name',
-    href: '#',
-    price: '/buy/super',
-    left: '32',
-    imageSrc:
-      'https://bafybeiav46h6zegh4e7zfdcgk6xjpg6if2kdxtvp3ejtyicvpgc2iucpim.ipfs.nftstorage.link/',
-    imageAlt: 'Front of plain black t-shirt.',
-  },
-  {
-    id: 5,
-    name: 'Name',
-    href: '/buy/tier-1',
-    price: '0.3',
-    left: '32',
-    imageSrc:
-      'https://bafybeib745w7vjcsh37mepaluvbmqrbjq4gax46oirkmrjeqeh55gjiyzi.ipfs.nftstorage.link/',
-    imageAlt:
-      'Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.',
-  },
-  {
-    id: 6,
-    name: 'Name',
-    href: '/buy/tier-2',
-    price: '0.3',
-    left: '32',
-    imageSrc:
-      'https://bafybeiagvn4exdbwokm4g6t6a2s3xvl2fu7zutvlf6sgwd4fjrdrc23hsu.ipfs.nftstorage.link/',
-    imageAlt: 'Front of plain black t-shirt.',
-  },
 
-  // More products...
-]
+import { products } from '@constants/products'
 
 export default function Cards() {
   return (
@@ -102,7 +36,7 @@ export default function Cards() {
               </div>
               <div className="flex flex-1 flex-col space-y-2 p-4">
                 <h3 className="font-Syne mt-[32px] text-4xl font-normal text-gray-900">
-                  <a href={product.href}>
+                  <a href={`buy/${product.id}`}>
                     <span aria-hidden="true" className="absolute inset-0" />
                     {product.name}
                   </a>
