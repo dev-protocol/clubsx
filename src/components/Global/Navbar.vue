e0ea02692eda7cf19a4e24737e15ba319803059a
Matteu <matteugt@gmail.com>
2022-08-05 04:13:56 +0800

    UPDATE: Use HSButton

--- src/components/Global/Navbar.vue
+++ src/components/Global/Navbar.vue
@@ -11,22 +11,24 @@
     <div class="flex items-center gap-4">
       <ConnectButton client:only="vue" />
       <div class="relative" ref="menu">
-        <button class="rounded border p-2" @click="toggle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
-        </button>
+        <HSButton type="outlined" @click.prevent="toggle">
+          <slot name="icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </slot>
+        </HSButton>
         <ul
           v-if="menuIsOpen"
           class="absolute right-0 z-50 mt-2 w-48 rounded border border-slate-800 bg-black p-2 shadow"
@@ -68,7 +70,8 @@
 </template>
 
 <script>
-import ConnectButton from '@components/Wallet/ConnectButton.vue'
+import ConnectButton from '../Wallet/ConnectButton.vue'
+import HSButton from '../Primitives/Hashi/HSButton.vue'
 
 export default {
   data() {
