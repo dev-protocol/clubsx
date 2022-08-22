e5b31913fea2335176766f1bc12b14b64ba3b2c0
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-08-22 11:17:20 +0900

    moves conflicting pages to plugins

---
+++ src/plugins/community/index.ts
@@ -0,0 +1,17 @@
+import {
+  ClubsFunctionGetAdminPaths,
+  ClubsFunctionGetPagePaths,
+  ClubsFunctionPlugin,
+} from '@devprotocol/clubs-core'
+import { default as Index } from './index.astro'
+
+export const getPagePaths: ClubsFunctionGetPagePaths = async () => [
+  { paths: ['community'], component: Index },
+]
+
+export const getAdminPaths: ClubsFunctionGetAdminPaths = async () => []
+
+export default {
+  getPagePaths,
+  getAdminPaths,
+} as ClubsFunctionPlugin
