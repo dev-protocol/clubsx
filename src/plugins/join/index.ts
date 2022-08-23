e1d5f81d69f093ac1ec2878a07b443fb9037115a
aggre <hiroyuki.aggre@gmail.com>
2022-08-23 11:15:45 +0900

    use plugin options

--- src/plugins/join/index.ts
+++ src/plugins/join/index.ts
@@ -1,4 +1,5 @@
-import { tiers } from '@constants/tier'
+import type { Tiers } from '@constants/tier'
+import type { UndefinedOr } from '@devprotocol/util-ts'
 import {
   ClubsFunctionGetAdminPaths,
   ClubsFunctionGetPagePaths,
@@ -7,10 +8,28 @@ import {
 import { default as Index } from './index.astro'
 import { default as Id } from './[id].astro'
 
-export const getPagePaths: ClubsFunctionGetPagePaths = async () => [
-  { paths: ['join'], component: Index },
-  ...tiers.map(({ id }) => ({ paths: ['join', id], component: Id })),
-]
+export const getPagePaths: ClubsFunctionGetPagePaths = async (
+  options,
+  { propertyAddress, name }
+) => {
+  const tiers = options.find((opt) => opt.key === 'tiers')
+    ?.value as UndefinedOr<Tiers>
+
+  return tiers
+    ? [
+        {
+          paths: ['join'],
+          component: Index,
+          props: { tiers, propertyAddress, name },
+        },
+        ...tiers.map(({ id, amount, currency }) => ({
+          paths: ['join', id],
+          component: Id,
+          props: { amount, currency, propertyAddress },
+        })),
+      ]
+    : []
+}
 
 export const getAdminPaths: ClubsFunctionGetAdminPaths = async () => []
 
