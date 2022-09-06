076e695ad7cdf539b85a753e82bdcacd5d1ec1bc
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-09-06 15:26:07 +0900

    some general layout

--- src/plugins/admin/index.ts
+++ src/plugins/admin/index.ts
@@ -7,8 +7,15 @@ import { default as Index } from './index.astro'
 
 export const getPagePaths: ClubsFunctionGetPagePaths = async () => []
 
-export const getAdminPaths: ClubsFunctionGetAdminPaths = async () => [
-  { paths: ['admin'], component: Index },
+export const getAdminPaths: ClubsFunctionGetAdminPaths = async (
+  _,
+  { name, description, twitterHandle, propertyAddress }
+) => [
+  {
+    paths: [''],
+    component: Index,
+    props: { name, description, twitterHandle, propertyAddress },
+  },
 ]
 
 export default {
