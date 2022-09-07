d03d9a8c0d9ae381169c4dac66ef7c64c2800c2f
Yash Agrawal <yashagrawal2299@gmail.com>
2022-09-07 10:47:53 +0530

    feat: add all plugins in the plugin panel

--- src/components/AdminPlugins/AdminPlugins.jsx
+++ src/components/AdminPlugins/AdminPlugins.jsx
@@ -2,14 +2,14 @@ const AdminPlugins = (props) => {
   const { plugins = [] } = props
 
   return (
-    <div className="flex w-full flex-col items-start gap-[64px] p-0">
+    <div className="flex ml-auto mr-auto w-[51.05%] max-w-full flex-col items-start gap-[64px] p-0">
       <p className="font-Syne h-9 p-0 text-2xl font-bold leading-9">Plugins</p>
       {plugins.map((plugin, i) => (
         <div className="flex w-full flex-row items-center justify-between gap-[10px] p-0">
           <p className="h-[24px] font-body text-base font-normal leading-6">
             {plugin.name[0].toUpperCase() + plugin.name.slice(1)}
           </p>
-          <div className="border-[##040B10] flex h-[68px] flex-row items-center justify-start gap-[16px] rounded-[99px] border-[3px] border-[#040B10] p-[6px]">
+          <div className="border-[##040B10] flex h-[68px] flex-row items-center justify-start gap-[16px] rounded-[99px] border-[3px] border-[#040B10] p-1.5">
             {!plugin.enable ? (
               <>
                 <div className="h-[56px] rounded-[999px] bg-[#040B10] p-[16px]">
@@ -17,15 +17,19 @@ const AdminPlugins = (props) => {
                     Disabled
                   </p>
                 </div>
-                <p className="h-[24px] font-body text-base font-normal leading-6">
-                  Enable
-                </p>
+                <div className="h-[56px] rounded-[999px] bg-[#040B10] p-[16px]">
+                  <p className="h-[24px] font-body text-base font-normal leading-6">
+                    Enabled
+                  </p>
+                </div>
               </>
             ) : (
               <>
-                <p className="h-[24px] font-body text-base font-normal leading-6">
-                  Disabled
-                </p>
+                <div className="h-[56px] rounded-[999px] bg-[#040B10] p-[16px]">
+                  <p className="h-[24px] font-body text-base font-normal leading-6">
+                    Disabled
+                  </p>
+                </div>
                 <div className="h-[56px] rounded-[999px] bg-[#040B10] p-[16px]">
                   <p className="h-[24px] font-body text-base font-normal leading-6">
                     Enabled
