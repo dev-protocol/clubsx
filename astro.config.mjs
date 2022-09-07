65f271722fa4da41738999ac53a7d639dc922002
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-09-07 17:50:40 +0900

    changes form to svelte; setConfig on form changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
 import react from '@astrojs/react'
-
+import svelte from '@astrojs/svelte'
 config()
 
+// https://astro.build/config
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL,
   server: {
@@ -22,10 +23,12 @@ export default defineConfig({
           server.middlewares.use((req, _, next) => {
             if (req.headers.accept?.includes('text/html')) {
               const host = req.headers.host.split('.')
+
               if (host.length > 1) {
                 req.url = `/sites_/${host[0]}${req.url}`
               }
             }
+
             next()
           })
         },
@@ -40,8 +43,11 @@ export default defineConfig({
     }),
     react(),
     tailwind({
-      config: { path: './tailwind.config.js' },
+      config: {
+        path: './tailwind.config.js',
+      },
     }),
+    svelte(),
   ],
   vite: {
     resolve: {
