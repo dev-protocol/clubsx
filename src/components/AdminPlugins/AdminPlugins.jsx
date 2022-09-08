a989a2c2fe8077bbb2fc0103291642a3ca851328
Yash Agrawal <yashagrawal2299@gmail.com>
2022-09-08 08:53:27 +0530

    feat: switch back to react

--- src/components/AdminPlugins/AdminPlugins.jsx
+++ src/components/AdminPlugins/AdminPlugins.jsx
@@ -1,47 +1,71 @@
-const AdminPlugins = (props) => {
-  const { plugins = [] } = props
+import React from "react";
 
-  return (
-    <div className="ml-auto mr-auto flex w-[51.05%] max-w-full flex-col items-start gap-[64px] p-0">
-      <p className="font-Syne h-9 p-0 text-2xl font-bold leading-9">Plugins</p>
-      {plugins.map((plugin, i) => (
-        <div className="flex w-full flex-row items-center justify-between gap-[10px] p-0">
-          <p className="h-[24px] font-body text-base font-normal leading-6">
-            {plugin.name[0].toUpperCase() + plugin.name.slice(1)}
-          </p>
-          <div className="border-[##040B10] flex h-[68px] flex-row items-center justify-start gap-[16px] rounded-[99px] border-[3px] border-[#040B10] p-1.5">
-            {!plugin.enable ? (
-              <>
-                <div className="h-[56px] rounded-[999px] bg-[#040B10] p-[16px]">
-                  <p className="h-[24px] font-body text-base font-normal leading-6">
-                    Disabled
-                  </p>
-                </div>
-                <div className="h-[56px] rounded-[999px] bg-[#040B10] p-[16px]">
-                  <p className="h-[24px] font-body text-base font-normal leading-6">
-                    Enabled
-                  </p>
-                </div>
-              </>
-            ) : (
-              <>
-                <div className="h-[56px] rounded-[999px] bg-[#040B10] p-[16px]">
-                  <p className="h-[24px] font-body text-base font-normal leading-6">
-                    Disabled
-                  </p>
-                </div>
-                <div className="h-[56px] rounded-[999px] bg-[#040B10] p-[16px]">
-                  <p className="h-[24px] font-body text-base font-normal leading-6">
-                    Enabled
-                  </p>
-                </div>
-              </>
-            )}
+class AdminPlugins extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {plugins: props.plugins};
+    this.toggleActivation = this.toggleActivation.bind(this);
+  }
+
+  toggleActivation = (index) => {
+    this.setState(prevState => {
+      const plugins = prevState.plugins
+      const pluginDetails =  { ...plugins[index] }
+      pluginDetails.enable = !pluginDetails.enable
+      plugins[index] = pluginDetails;
+      return { plugins }
+    })
+  }
+
+  render() {
+    const toggleEnableClasses = "h-[56px] rounded-[999px] bg-[#040B10] p-[16px] cursor-pointer"
+    const toggleDisableClasses = "h-[56px] rounded-[999px] b-0 p-[16px] cursor-pointer"
+
+    console.log('This', this.state.plugins);
+
+    return (
+      <div className="relative w-[51.05%] left-[5.555%] flex flex-col items-start gap-[64px] p-0">
+        <p className="font-Syne h-9 p-0 text-2xl font-bold leading-9">Plugins</p>
+        {this.state.plugins.map((plugin, i) => (
+          <div className="flex w-full flex-row items-center justify-between gap-[10px] p-0">
+            <p className="h-[24px] font-body text-base font-normal leading-6">
+              {plugin.name[0].toUpperCase() + plugin.name.slice(1)}
+            </p>
+            <div className="border-[##040B10] flex h-[68px] flex-row items-center justify-start gap-[16px] rounded-[99px] border-[3px] border-[#040B10] p-1.5">
+              <div
+                onClick={!plugin.enable ? () => {} : () => this.toggleActivation(i)}
+                className={!plugin.enable ? toggleEnableClasses : toggleDisableClasses}
+              >
+                <button
+                  disabled={!plugin.enable}
+                  onClick={() => this.toggleActivation(i)}
+                  className="h-[24px] font-body text-base font-normal leading-6"
+                >
+                  {
+                    !plugin.enable ? "Disabled" : "Disable"
+                  }
+                </button>
+              </div>
+              <div
+                onClick={plugin.enable ? () => {} : () => this.toggleActivation(i)}
+                className={plugin.enable ? toggleEnableClasses : toggleDisableClasses}
+              >
+                <button
+                  disabled={plugin.enable}
+                  onClick={() => this.toggleActivation(i)}
+                  className="h-[24px] font-body text-base font-normal leading-6"
+                >
+                  {
+                    plugin.enable ? "Enabled" : "Enable"
+                  }
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  )
+        ))}
+      </div>
+    )
+  }
 }
 
 export default AdminPlugins
