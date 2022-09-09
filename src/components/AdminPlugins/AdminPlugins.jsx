66f8addd3011c94b7d2b7ab1f90f5efe940a4f79
Yash Agrawal <yashagrawal2299@gmail.com>
2022-09-09 08:13:31 +0530

    feat: add setConfig and encodedConfig in AdminPlugins

--- src/components/AdminPlugins/AdminPlugins.jsx
+++ src/components/AdminPlugins/AdminPlugins.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
+import { decode, setConfig } from '@devprotocol/clubs-core'
 
 class AdminPlugins extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { plugins: props.plugins }
+    this.state = { config: decode(props.encodedConfig)  }
     this.toggleActivation = this.toggleActivation.bind(this)
   }
 
-  toggleActivation = (index) => {
+  toggleActivation = (pluginName) => {
     this.setState((prevState) => {
-      const plugins = prevState.plugins
+      const plugins = prevState.config.plugins
+
+      // 1. Find index of plugin.
+      const index = plugins.findIndex(plugin => plugin.name === pluginName);
+      if (index === -1) return prevState;
+
+      // 2. Extract it's details.
       const pluginDetails = { ...plugins[index] }
+      // 3. Toggle plugin.
       pluginDetails.enable = !pluginDetails.enable
+      // 4. Update the toggle w.r.t all plugins.
       plugins[index] = pluginDetails
-      return { plugins }
+      // 5. Update the config.
+      const config = { ...prevState.config, plugins }
+      // 6. Set config.
+      setConfig(config)
+      // 7. Update component state.
+      return { ...prevState.config, plugins }
     })
   }
 
@@ -28,15 +42,15 @@ class AdminPlugins extends React.Component {
         <p className="font-Syne h-9 p-0 text-2xl font-bold leading-9">
           Plugins
         </p>
-        {this.state.plugins.map((plugin, i) => (
+        {this.state.config.plugins.map((plugin, i) => (
           <div className="flex w-full flex-row items-center justify-between gap-[10px] p-0">
-            <p className="h-[24px] font-body text-base font-normal leading-6">
-              {plugin.name[0].toUpperCase() + plugin.name.slice(1)}
+            <p className="h-[24px] font-body text-base font-normal leading-6 capitalize">
+              {plugin.name}
             </p>
             <div className="border-[##040B10] flex h-[68px] flex-row items-center justify-start gap-[16px] rounded-[99px] border-[3px] border-[#040B10] p-1.5">
               <div
                 onClick={
-                  !plugin.enable ? () => {} : () => this.toggleActivation(i)
+                  !plugin.enable ? () => {} : () => this.toggleActivation(plugin.name)
                 }
                 className={
                   !plugin.enable ? toggleEnableClasses : toggleDisableClasses
@@ -51,7 +65,7 @@ class AdminPlugins extends React.Component {
               </div>
               <div
                 onClick={
-                  plugin.enable ? () => {} : () => this.toggleActivation(i)
+                  plugin.enable ? () => {} : () => this.toggleActivation(plugin.name)
                 }
                 className={
                   plugin.enable ? toggleEnableClasses : toggleDisableClasses
