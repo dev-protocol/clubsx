ed66a44731a2421055f3cccb16b55037ca2b55b0
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-09-09 16:58:08 +0900

    changes to on input

--- src/plugins/admin-community/AdminCommunityForm.svelte
+++ src/plugins/admin-community/AdminCommunityForm.svelte
@@ -3,16 +3,17 @@
   export let currentPluginIndex: number
   import { setOptions } from '@devprotocol/clubs-core'
 
-  const submit = () => {
+  const updateCommunityOptions = () => {
     setOptions([{ key: 'guildUrl', value: guildUrl }], currentPluginIndex)
   }
 </script>
 
-<form on:submit|preventDefault={(_) => submit()}>
+<form>
   <div class="mb-10 flex flex-col">
     <label class="mb-1" for="guild-url">Guild URL</label>
     <input
       bind:value={guildUrl}
+      on:input={updateCommunityOptions}
       class="rounded bg-[#040B10] px-8 py-4"
       id="guild-url"
       name="guild-url"
