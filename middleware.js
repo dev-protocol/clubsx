31f3ab625cac70832bed156da49fa7482d11e8a6
aggre <hiroyuki.aggre@gmail.com>
2022-09-15 13:52:46 +0900

    add logger

--- middleware.js
+++ middleware.js
@@ -18,6 +18,7 @@ export default function middleware(req) {
     url.pathname = `/sites_/${tenant}${url.pathname}`
     headers.set('x-middleware-rewrite', url.href)
     headers.set('x-rewritten-url', url.href)
+    console.log('url.href', url.href)
     return new Response(null, { headers })
   }
 
