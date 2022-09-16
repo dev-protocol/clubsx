59451d79d5c6ec020191b43974293cbbf8d46f82
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-09-16 10:48:48 +0900

    authentication working

--- src/pages/sites_/[site]/admin/updateConfig.ts
+++ src/pages/sites_/[site]/admin/updateConfig.ts
@@ -1,20 +1,18 @@
-import { utils } from 'ethers'
+import { authenticate } from '@devprotocol/clubs-core'
+import { providers } from 'ethers'
 import { createClient } from 'redis'
 
 export const post = async ({ request }: { request: Request }) => {
-  const { site, config, sig, expectedAddress } = (await request.json()) as {
+  const { site, config, sig, hash } = (await request.json()) as {
     site: string
     config: string
     hash: string
     sig: string
-    expectedAddress: string
   }
 
-  const digest = utils.hashMessage(config)
-  const actual = utils.recoverAddress(digest, sig)
-  console.log('actual is: ', actual)
-
-  return
+  const provider = providers.getDefaultProvider(
+    import.meta.env.PUBLIC_WEB3_PROVIDER_URL
+  )
 
   const client = createClient({
     url: process.env.REDIS_URL,
@@ -27,9 +25,36 @@ export const post = async ({ request }: { request: Request }) => {
     console.error('redis connection error: ', e)
   })
 
-  await client.set(site, config)
+  const previousConfiguration = await client.get(site)
+  if (!previousConfiguration) {
+    return new Response(JSON.stringify({ error: 'Encoded config not found' }), {
+      status: 401,
+    })
+  }
 
-  await client.quit()
+  const authenticated = await authenticate({
+    message: hash,
+    signature: sig,
+    previousConfiguration,
+    provider,
+  })
 
-  return new Response(JSON.stringify({}), { status: 200 })
+  if (!authenticated) {
+    return new Response(JSON.stringify({}), { status: 401 })
+  }
+
+  try {
+    await client.set(site, config)
+    await client.quit()
+    return new Response(JSON.stringify({}), { status: 200 })
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), { status: 500 })
+  }
+}
+
+export type ClubsFunctionAuthenticationAdminParams = {
+  readonly signature: string
+  readonly message: string
+  readonly previousConfiguration: string
+  readonly provider: providers.BaseProvider
 }
