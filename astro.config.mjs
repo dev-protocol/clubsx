90e8c4b923a70375f43ac5d707335e04a882444b
Yash Agrawal <yashagrawal2299@gmail.com>
2022-09-16 07:08:10 +0530

    build(deps): add @astrojs/prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,57 +1,55 @@
-import { config } from 'dotenv'
-import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/serverless'
-import tailwind from '@astrojs/tailwind'
-import vue from '@astrojs/vue'
-import react from '@astrojs/react'
-import svelte from '@astrojs/svelte'
-config()
+import { config } from 'dotenv';
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
+import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
+import prefetch from "@astrojs/prefetch";
+config();
+
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL,
   server: {
-    port: 3000,
+    port: 3000
   },
   output: 'server',
   adapter: vercel(),
-  integrations: [
-    {
-      name: 'clubs:multi-tenant',
-      hooks: {
-        'astro:server:setup': ({ server }) => {
-          server.middlewares.use((req, _, next) => {
-            if (req.headers.accept?.includes('text/html')) {
-              const host = req.headers.host.split('.')
+  integrations: [{
+    name: 'clubs:multi-tenant',
+    hooks: {
+      'astro:server:setup': ({
+        server
+      }) => {
+        server.middlewares.use((req, _, next) => {
+          if (req.headers.accept?.includes('text/html')) {
+            const host = req.headers.host.split('.');
 
-              if (host.length > 1) {
-                req.url = `/sites_/${host[0]}${req.url}`
-              }
+            if (host.length > 1) {
+              req.url = `/sites_/${host[0]}${req.url}`;
             }
+          }
 
-            next()
-          })
-        },
-      },
-    },
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.includes('-'),
-        },
-      },
-    }),
-    react(),
-    tailwind({
-      config: {
-        path: './tailwind.config.js',
-      },
-    }),
-    svelte(),
-  ],
+          next();
+        });
+      }
+    }
+  }, vue({
+    template: {
+      compilerOptions: {
+        isCustomElement: tag => tag.includes('-')
+      }
+    }
+  }), react(), tailwind({
+    config: {
+      path: './tailwind.config.js'
+    }
+  }), svelte(), prefetch()],
   vite: {
     resolve: {
-      conditions: [],
-    },
-  },
-})
+      conditions: []
+    }
+  }
+});
