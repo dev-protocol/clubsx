0ab8f124351ece3225836f6a82c0d29acf8371f2
Yash Agrawal <yashagrawal2299@gmail.com>
2022-09-28 08:08:11 +0530

    feat: use forms instead of db in nft gated contact form

--- src/plugins/message/index.ts
+++ src/plugins/message/index.ts
@@ -11,69 +11,15 @@ import { GatedMessage } from './types'
 
 const forms: GatedMessage[] = json
 
-const testMemberships = [
-  {
-    id: 'tier-3',
-    name: 'Tier 3',
-    description: undefined,
-    price: 400,
-    currency: 'DEV',
-    imageSrc:
-      'https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/6/2017/07/Kiyomizudera-Temple-Kyoto.jpg',
-    payload: new Uint8Array(),
-    fee: undefined,
-  },
-  {
-    id: 'tier-2',
-    name: 'Tier 2',
-    description: undefined,
-    price: 4000,
-    currency: 'DEV',
-    imageSrc:
-      'https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/6/2017/07/Kiyomizudera-Temple-Kyoto.jpg',
-    payload: new Uint8Array(),
-    fee: undefined,
-  },
-  {
-    id: 'tier-1',
-    name: 'Tier 1',
-    description: undefined,
-    price: 10000,
-    currency: 'DEV',
-    imageSrc:
-      'https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/6/2017/07/Kiyomizudera-Temple-Kyoto.jpg',
-    payload: new Uint8Array(),
-    fee: undefined,
-  },
-  {
-    id: 'super',
-    name: 'Super',
-    description: undefined,
-    price: 15000,
-    currency: 'DEV',
-    imageSrc:
-      'https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/6/2017/07/Kiyomizudera-Temple-Kyoto.jpg',
-    payload: new Uint8Array(),
-    fee: undefined,
-  },
-]
-
 export const getPagePaths: ClubsFunctionGetPagePaths = async (
   options,
   { name, propertyAddress }
 ) => {
-  const formName = options.find((opt) => opt.key === 'name')?.value || 'Contact'
-  const description =
-    options.find((opt) => opt.key === 'description')?.value ||
-    'Contact Temples DAO using form. This will help you get your queries answered in the case you have some issues or bugs'
-  const memberships =
-    options.find((opt) => opt.key === 'memberships')?.value || testMemberships
-
   return [
     {
       paths: ['message'],
       component: Index,
-      props: { name, propertyAddress, formName, description, memberships },
+      props: { name, propertyAddress, forms },
     },
     ...forms.map((form) => ({
       paths: ['message', String(form.id)],
