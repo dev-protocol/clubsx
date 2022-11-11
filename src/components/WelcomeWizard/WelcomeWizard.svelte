80ce293e19b6ec0419a61592d5c958072a605a4a
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-11-11 18:23:23 +0900

    basic wizard stepper

---
+++ src/components/WelcomeWizard/WelcomeWizard.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import WelcomeWizardStep from './WelcomeWizardStep.svelte'
+
+  export let step: 'HOME' | 'MEMBERSHIPS' | 'PLUGINS'
+</script>
+
+<div class="h-screen py-12 px-16">
+  <WelcomeWizardStep
+    label="Homepage"
+    isActive={step === 'HOME'}
+    isComplete={step === 'MEMBERSHIPS' || step === 'PLUGINS'}
+  />
+
+  <WelcomeWizardStep
+    label="Memberships"
+    isActive={step === 'MEMBERSHIPS'}
+    isComplete={step === 'PLUGINS'}
+  />
+
+  <WelcomeWizardStep
+    label="Plugins"
+    isActive={step === 'PLUGINS'}
+    isComplete={false}
+  />
+</div>
---
+++ src/components/WelcomeWizard/WelcomeWizardStep.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let label: string
+  export let isActive: boolean
+  export let isComplete: boolean
+
+  const isActiveClass = 'font-bold underline'
+</script>
+
+<div class={`mb-4 flex flex-row text-white ${isActive ? isActiveClass : ''}`}>
+  {#if isComplete}
+    <span>✅</span>
+  {:else}
+    <span>☑️</span>
+  {/if}
+  <span>{label}</span>
+</div>
