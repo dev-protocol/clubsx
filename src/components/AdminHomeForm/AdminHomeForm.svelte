c48bb79f6cd0a764838a35c908f476ce30620fd9
Yash Agrawal <yashagrawal2299@gmail.com>
2022-11-24 08:05:57 +0530

    feat: make what we do image dynamic

--- src/components/AdminHomeForm/AdminHomeForm.svelte
+++ src/components/AdminHomeForm/AdminHomeForm.svelte
@@ -49,54 +49,32 @@
         />
       </div>
 
-      {#if homeConfig.whatWeDo.images[0]}
-        <div class="mb-10 flex flex-col">
-          <label class="mb-1" for="what-we-do-image-1"> Image 1 </label>
-          <input
-            class="rounded bg-[#040B10] px-8 py-4"
-            bind:value={homeConfig.whatWeDo.images[0].image}
-            id="what-we-do-image-1"
-            name="what-we-do-image-1"
-          />
-        </div>
+      {#each homeConfig.whatWeDo.images as image, i}
+        <div>
+          <div class="mb-10 flex flex-col">
+            <label class="mb-1" for={`perk-image-${i}`}>Image {i+1} </label>
+            <input
+              class="rounded bg-[#040B10] px-8 py-4"
+              bind:value={image.image}
+              id={`perk-image-${i}`}
+              name={`perk-image-${i}`}
+            />
+          </div>
 
-        <div class="mb-10 flex flex-col">
-          <label class="mb-1" for="what-we-do-image-1-description">
-            Image 1 Description
-          </label>
-          <input
-            class="rounded bg-[#040B10] px-8 py-4"
-            bind:value={homeConfig.whatWeDo.images[0].description}
-            id="what-we-do-image-1-description"
-            name="what-we-do-image-1-description"
-          />
+          <div class="mb-10 flex flex-col">
+            <label class="mb-1" for={`perk-image-${i}-description`}>
+              Image {i+1} Description
+            </label>
+            <input
+              class="rounded bg-[#040B10] px-8 py-4"
+              bind:value={image.description}
+              id={`perk-image-${i}-description`}
+              name={`perk-image-${i}-description`}
+            />
+          </div>
         </div>
-      {/if}
+      {/each}
 
-      {#if homeConfig.whatWeDo.images[1]}
-        <div class="mb-10 flex flex-col">
-          <label class="mb-1" for="what-we-do-image-1"> Image 2 </label>
-          <input
-            class="rounded bg-[#040B10] px-8 py-4"
-            bind:value={homeConfig.whatWeDo.images[1].image}
-            id="what-we-do-image-2"
-            name="what-we-do-image-2"
-          />
-        </div>
-
-        <div class="mb-10 flex flex-col">
-          <label class="mb-1" for="what-we-do-image-2-description">
-            Image 2 Description
-          </label>
-          <input
-            class="rounded bg-[#040B10] px-8 py-4"
-            bind:value={homeConfig.whatWeDo.images[0].description}
-            id="what-we-do-image-2-description"
-            name="what-we-do-image-2-description"
-          />
-        </div>
-      {/if}
-    </div>
 
     <div class="my-4 flex flex-col pt-8">
       <span class="mb-4 text-lg font-bold">Perks</span>
@@ -126,7 +104,7 @@
       {#each homeConfig.perks.images as perkImage, i}
         <div>
           <div class="mb-10 flex flex-col">
-            <label class="mb-1" for={`perk-image-${i}`}> Perk Image {i} </label>
+            <label class="mb-1" for={`perk-image-${i}`}> Perk Image {i+1} </label>
             <input
               class="rounded bg-[#040B10] px-8 py-4"
               bind:value={perkImage.image}
@@ -137,7 +115,7 @@
 
           <div class="mb-10 flex flex-col">
             <label class="mb-1" for={`perk-image-${i}-description`}>
-              Perk Image {i} Description
+              Perk Image {i+1} Description
             </label>
             <input
               class="rounded bg-[#040B10] px-8 py-4"
