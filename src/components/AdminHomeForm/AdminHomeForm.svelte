b2b846221f06c125d98d36e40d1f299135053c28
Yash Agrawal <yashagrawal2299@gmail.com>
2022-11-25 09:50:02 +0530

    feat: add upload image basic teplate

--- src/components/AdminHomeForm/AdminHomeForm.svelte
+++ src/components/AdminHomeForm/AdminHomeForm.svelte
@@ -2,10 +2,12 @@
   import { setOptions } from '@devprotocol/clubs-core'
   import type { HomeConfig } from '../../constants/homeConfig'
 
+  let fileinput: any;
   export let homeConfig: HomeConfig
   export let currentPluginIndex: number
 
-  const update = () => {
+  const update = (e: any) => {
+    if (e.target.id.includes('-image-') || e.target.type == 'file') return;  // We don't want to store file in config
     setOptions([{ key: 'homeConfig', value: homeConfig }], currentPluginIndex)
   }
 
@@ -31,9 +33,33 @@
       },
     }
   }
+
+  const uploadImageAndGetPath = (image: string) => {
+    if (!image) return '';
+    // Upload the image and return path
+    return 'ABC';
+  }
+
+  const updateWhatWeDoImgUrl = (i: number, imageURl: string) => {
+    homeConfig.whatWeDo.images[i] = {
+      image: imageURl,
+      description: homeConfig.whatWeDo.images[i].description
+    }
+  }
+
+  const onWhatWeDoFileSelected = (i: number, e: any) => {
+    let image = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload = e => {
+      const imgData = e?.target?.result?.toString() || '';
+      const path = uploadImageAndGetPath(imgData);
+      updateWhatWeDoImgUrl(i, path);
+    };
+  }
 </script>
 
-<form on:change|preventDefault={(_) => update()}>
+<form on:change|preventDefault={(e) => update(e)}>
   <div class="divide-y">
     <div class="mb-4 flex flex-col">
       <span class="mb-4 text-lg font-bold">Hero</span>
@@ -75,24 +101,29 @@
       {#each homeConfig.whatWeDo.images as image, i}
         <div>
           <div class="mb-10 flex flex-col">
-            <label class="mb-1" for={`perk-image-${i}`}>Image {i + 1} </label>
-            <input
-              class="rounded bg-[#040B10] px-8 py-4"
-              bind:value={image.image}
-              id={`perk-image-${i}`}
-              name={`perk-image-${i}`}
-            />
+            <label class="mb-1" for={`whatwedo-image-${i}`}>Image {i + 1} </label>
+            {#if image.image && image.image != ''}
+              <input
+                class="rounded bg-[#040B10] px-8 py-4"
+                bind:value={image.image}
+                id={`whatwedo-image-${i}`}
+                name={`whatwedo-image-${i}`}
+              />
+            {:else}
+              <button for={`whatwedo-image-${i}`} class="hs-button is-filled" type="button" on:click={()=>{fileinput.click();}}>Choose Image</button>
+              <input id={`whatwedo-image-${i}`} name={`whatwedo-image-${i}`} style="display:none" type="file" on:change={(e)=> onWhatWeDoFileSelected(i, e)} bind:this={fileinput} >
+            {/if}
           </div>
 
           <div class="mb-10 flex flex-col">
-            <label class="mb-1" for={`perk-image-${i}-description`}>
+            <label class="mb-1" for={`whatwedo-image-${i}-description`}>
               Image {i + 1} Description
             </label>
             <input
               class="rounded bg-[#040B10] px-8 py-4"
               bind:value={image.description}
-              id={`perk-image-${i}-description`}
-              name={`perk-image-${i}-description`}
+              id={`whatwedo-image-${i}-description`}
+              name={`whatwedo-image-${i}-description`}
             />
           </div>
 
