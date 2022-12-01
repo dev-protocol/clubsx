febe4c72bbfa7ec6372bb9c3931c791b94e1ef54
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-12-01 17:41:22 -0500

    sending email via firebase

--- src/components/WelcomeConnect/WelcomeConnect.svelte
+++ src/components/WelcomeConnect/WelcomeConnect.svelte
@@ -1,9 +1,40 @@
 <script lang="ts">
+  import { initializeFirebase } from '../../fixtures/firebase';
+  import { sendSignInLinkToEmail } from "firebase/auth";
+
 
   let email = '';
+  let emailErrorMessage = '';
 
-  const sendMagicLink = async () => {}
+  const sendMagicLink = async () => {
 
+    const actionCodeSettings = {
+      // URL you want to redirect back to. The domain (www.example.com) for this
+      // URL must be in the authorized domains list in the Firebase Console.
+      // url: 'https://www.example.com/finishSignUp?cartId=1234',
+      url: 'http://localhost:3000/test',
+      // This must be true.
+      handleCodeInApp: true,
+    };
+
+    const auth = initializeFirebase();
+    console.log('auth is: ', auth)
+
+    sendSignInLinkToEmail(auth, email, actionCodeSettings)
+      .then(() => {
+        // The link was successfully sent. Inform the user.
+        // Save the email locally so you don't need to ask the user for it again
+        // if they open the link on the same device.
+        window.localStorage.setItem('emailForSignIn', email);
+        // ...
+      })
+      .catch((error) => {
+        emailErrorMessage = error.message;
+        console.log('error is: ', error)
+        // ...
+      });
+
+  }
   const walletConnect = async () => {}
 
 </script>
@@ -26,6 +57,10 @@
       </div>
 
       <button on:click|preventDefault={(_) => sendMagicLink()} class="text-sm bg-blue-500 px-4 rounded-xl py-3 px-6">Send Magic Link</button>
+
+      {#if emailErrorMessage.length > 0}
+        <span class="text-sm">{emailErrorMessage}</span>
+      {/if}
 
     </div>
 
---
+++ src/env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_FIREBASE_API_KEY: string
+  readonly PUBLIC_FIREBASE_PROJECT_ID: string
+  readonly PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string
+  readonly PUBLIC_FIREBASE_APP_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
