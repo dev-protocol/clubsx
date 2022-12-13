c773e04581edcaea7a98a72741cc18c9d4775d1f
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-12-12 20:06:09 -0500

    profile form setup

--- src/components/ProfileSetupForm/ProfileSetupForm.svelte
+++ src/components/ProfileSetupForm/ProfileSetupForm.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import { ClubsConfiguration, setConfig } from '@devprotocol/clubs-core'
   import { uploadImageAndGetPath } from '@fixtures/imgur'
 
-  let clubName = ''
+  export let config: ClubsConfiguration
+
+  let { name, twitterHandle } = config
+
   const projectCategories = [
     {
       label: 'YouTube',
@@ -17,7 +21,6 @@
     },
   ]
   let projectCategory = 'GITHUB'
-  let twitterHandle = ''
   let avatarPath = ''
 
   const onFileSelected = async (
@@ -33,9 +36,33 @@
 
     avatarPath = await uploadImageAndGetPath(file)
   }
+
+  const update = async () => {
+    if (!config.options) {
+      config = Object.assign({}, config, { options: [] })
+    }
+
+    const draftOptions =
+      config.options?.find((opt) => opt.key === '__draftOptions') ?? {}
+
+    const options = [
+      {
+        key: 'avatarImgSrc',
+        value: avatarPath ?? '',
+      },
+      {
+        key: '__draftOptions',
+        value: Object.assign({}, draftOptions, { category: projectCategory }),
+      },
+    ]
+
+    config = Object.assign({}, config, { name, twitterHandle, options })
+
+    setConfig(config)
+  }
 </script>
 
-<div>
+<form on:change|preventDefault={(e) => update()}>
   <div class="mb-10 flex flex-col">
     <label class="mb-1" for="club-name">
       Club Name
@@ -43,7 +70,7 @@
     </label>
     <input
       class="rounded bg-[#040B10] px-8 py-4"
-      bind:value={clubName}
+      bind:value={name}
       id="club-name"
       name="club-name"
     />
@@ -58,6 +85,7 @@
       bind:value={projectCategory}
       id="project-category"
       name="project-category"
+      class="rounded bg-[#040B10] px-8 py-4"
     >
       {#each projectCategories as cat}
         <option value={cat.value}>{cat.label}</option>
@@ -76,8 +104,8 @@
   </div>
 
   <div class="mb-10 flex flex-col">
-    <label class="mb-1" for="avatar">
-      <span>Avatar</span>
+    <label class="mb-1 flex flex-col" for="avatarPath">
+      <span class="mb-1">Avatar</span>
 
       {#if avatarPath && avatarPath != ''}
         <input
@@ -87,15 +115,20 @@
           name={`avatar`}
         />
       {:else}
-        <button class="hs-button is-filled" type="button">Choose Image</button>
+        <div class="float-left">
+          <span
+            class="text-sm font-medium px-12 py-4 rounded-lg bg-[#040B10] cursor-pointer"
+            type="button">Choose Image</span
+          >
+        </div>
       {/if}
       <input
-        id={`avatarPath`}
-        name={`avatarPath`}
+        id="avatarPath"
+        name="avatarPath"
         style="display:none"
         type="file"
         on:change={onFileSelected}
       />
     </label>
   </div>
-</div>
+</form>
