6e9be9c5f099170321a0f681d88bf6fc053ecbe0
aggre <hiroyuki.aggre@gmail.com>
2022-12-13 15:03:17 +0900

    add a new middleware that following the latest Vercel APIs

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { rewrite } from '@vercel/edge'
+
+export const config = {
+  matcher: ['/((?!api|[\\w-]+\\.\\w+).*)'],
+}
+
+export default function middleware(req: Request) {
+  const url = new URL(req.url)
+
+  const hostname = req.headers.get('host')?.split('.') ?? []
+  const [tenant] = hostname
+  const html = req.headers.get('accept')?.includes('text/html')
+
+  if (html) {
+    url.pathname = `/sites_/${tenant}${url.pathname}`
+    return rewrite(new URL(url.pathname, 'https://clubs.place'))
+  }
+}
