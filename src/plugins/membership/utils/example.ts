ba03bc6d20fcffd8ee9c6f7fd90aa63c5d571207
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2022-12-15 14:03:14 +0530

    wip

--- src/plugins/membership/utils/example.ts
+++ src/plugins/membership/utils/example.ts
@@ -1,14 +1,12 @@
-import {callSimpleCollections} from './simpleCollections'
-import { BaseProvider } from '@ethersproject/providers';
-import {setImagesProps} from './types/setImageArg';
+import { callSimpleCollections } from './simpleCollections'
+import { BaseProvider } from '@ethersproject/providers'
+import { setImagesProps } from './types/setImageArg'
 import { ClubsConfiguration, setConfig } from '@devprotocol/clubs-core'
 
-const args: setImagesProps = {
-}
+const args: setImagesProps = {}
 const config: ClubsConfiguration
-const {propertyAddress} = config
-
+const { propertyAddress } = config
 
 export const example = async (provider: BaseProvider) => {
-    const setImage = await callSimpleCollections(provider, 'setImage', [])
-    }
+  const setImage = await callSimpleCollections(provider, 'setImage', [])
+}
