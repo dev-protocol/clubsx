82bb7c5bb768a384bd9ec2594b5091d6379aa1ee
aggre <hiroyuki.aggre@gmail.com>
2022-12-16 15:34:41 +0900

    run prettier-plugin-svelte

--- src/components/PreviewBanner/PreviewBanner.svelte
+++ src/components/PreviewBanner/PreviewBanner.svelte
@@ -2,10 +2,10 @@
 </script>
 
 <div
-  class="fixed bottom-0 left-1 right-1 backdrop-blur-3xl rounded-t-3xl py-4 px-8 bg-black/50 z-50"
+  class="fixed bottom-0 left-1 right-1 z-50 rounded-t-3xl bg-black/50 py-4 px-8 backdrop-blur-3xl"
 >
   <div class="flex items-center justify-center">
-    <span class="border rounded mr-4 py-1 px-4">Preview</span>
+    <span class="mr-4 rounded border py-1 px-4">Preview</span>
 
     <span class="font-bold"
       >Send blockchain transactions to complete process</span
