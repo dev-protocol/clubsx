1de9553251a9e0f7920acf24cbdd541e47978ba2
aggre <hiroyuki.aggre@gmail.com>
2022-12-16 15:26:48 +0900

    patched (#364)

--- src/components/WelcomeWizard/WelcomeWizardStep.svelte
+++ src/components/WelcomeWizard/WelcomeWizardStep.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
   export let label: string
   export let isActive: boolean
-  export let isComplete: boolean
-
-  const isActiveClass = 'font-bold underline'
+  export let link: string
 </script>
 
-<div class={`mb-4 flex flex-row text-white ${isActive ? isActiveClass : ''}`}>
-  {#if isComplete}
-    <span>✅</span>
-  {:else}
-    <span>☑️</span>
-  {/if}
-  <span>{label}</span>
-</div>
+<a href={link} class={`flex flex-row w-full p-3 -mx-3 rounded transition-colors duration-500 gap-4 text-white ${isActive ? 'border' : 'hover:bg-white/30'}`}>
+  <i role="presentation" class={`${isActive ? '' : 'opacity-50'}`}><slot /></i>
+  <span class="font-bold">{label}</span>
+</a>
