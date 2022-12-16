35f1e894afd19556538f9d9a4cb5e3c1136a8648
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-12-16 16:34:20 -0500

    setup design area working

--- src/components/SetupDesign/SetupDesign.svelte
+++ src/components/SetupDesign/SetupDesign.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { setOptions } from '@devprotocol/clubs-core'
+  import { uploadImageAndGetPath } from '@fixtures/imgur'
   import type { HomeConfig } from '../../constants/homeConfig'
 
   export let homeConfig: HomeConfig
@@ -8,17 +9,48 @@
   const update = async () => {
     setOptions([{ key: 'homeConfig', value: homeConfig }], currentPluginIndex)
   }
+
+  const onFileSelected = async (
+    e: Event & {
+      currentTarget: EventTarget & HTMLInputElement
+    }
+  ) => {
+    if (!e.currentTarget.files) {
+      return
+    }
+
+    const file = e.currentTarget.files[0]
+
+    homeConfig.hero.image = await uploadImageAndGetPath(file)
+    homeConfig = homeConfig
+  }
 </script>
 
 <form on:change|preventDefault={(e) => update()}>
-  <div class="mb-10 flex flex-col">
-    <label class="mb-1" for="hero-image"> Image </label>
-    <input
-      class="rounded bg-[#040B10] px-8 py-4"
-      bind:value={homeConfig.hero.image}
-      id="hero-image"
-      name="hero-image"
-    />
+  <div class="mb-10">
+    <label class="mb-1 flex flex-col" for="avatarPath">
+      <label class="mb-1" for="hero-image"> Image </label>
+      {#if homeConfig.hero.image && homeConfig.hero.image != ''}
+        <div class="w-32 cursor-pointer">
+          <img src={homeConfig.hero.image} alt="Hero" />
+        </div>
+      {:else}
+        <div class="float-left">
+          <span
+            class="text-sm font-medium px-12 py-4 rounded-lg bg-[#040B10] cursor-pointer"
+            type="button">Choose Image</span
+          >
+        </div>
+      {/if}
+
+      <input
+        id="hero-image"
+        name="hero-image"
+        style="display:none"
+        type="file"
+        on:change={onFileSelected}
+      />
+    </label>
   </div>
 
   <div class="mb-10 flex flex-col">
@@ -33,7 +65,7 @@
 
   <div class="mb-10 flex flex-col">
     <label class="mb-1" for="club-description"> Club Description </label>
-    <input
+    <textarea
       class="rounded bg-[#040B10] px-8 py-4"
       bind:value={homeConfig.body}
       id="club-description"
