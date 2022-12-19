bf9862c296bcf47a05e4724c5d52d6d92860d8fe
Yash Agrawal <yashagrawal2299@gmail.com>
2022-12-19 10:32:02 +0530

    feat: move setDb to connect/

---
+++ src/constants/defaultConfig.ts
@@ -0,0 +1,80 @@
+import { ClubsConfiguration } from '@devprotocol/clubs-core'
+
+export const defaultConfig: ClubsConfiguration = {
+  name: '',
+  twitterHandle: '',
+  description: '',
+  url: 'https://<USERS_SITE_NAME_HERE>.clubs.place',
+  propertyAddress: '0x0000000000000000000000000000000000000000',
+  adminRolePoints: 50,
+  chainId: 137,
+  rpcUrl: 'https://polygon-rpc.com',
+  options: [
+    {
+      key: 'headerLinks',
+      value: [],
+    },
+    {
+      key: 'socialLinks',
+      value: [],
+    },
+    {
+      key: 'sidebarPrimaryLinks',
+      value: [
+        {
+          display: 'Join',
+          path: '/join',
+        },
+      ],
+    },
+    {
+      key: 'sidebarLinks',
+      value: [],
+    },
+    {
+      key: 'fullPageViewPaths',
+      value: [{ path: '', exact: true }, { path: 'join/' }],
+    },
+    {
+      key: 'avatarImgSrc',
+      value: '',
+    },
+  ],
+  plugins: [
+    {
+      name: 'admin',
+      options: [],
+    },
+    {
+      name: 'home',
+      options: [
+        {
+          key: 'homeConfig',
+          value: {
+            hero: {
+              image: 'https://i.imgur.com/MyGIFqA.jpg',
+              text: '',
+            },
+            body: '',
+          },
+        },
+      ],
+    },
+    {
+      name: 'join',
+      options: [],
+    },
+    {
+      name: 'me',
+      options: [],
+    },
+    {
+      name: 'members',
+      options: [],
+    },
+    {
+      name: 'memberships',
+      options: [],
+    },
+  ],
+}
