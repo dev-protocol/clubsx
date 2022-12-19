8b7a5e93de22df373d48186dcb6f1d98de55fb10
aggre <hiroyuki.aggre@gmail.com>
2022-12-19 11:53:28 +0900

    patch design

--- src/components/SetupDesign/SetupDesign.svelte
+++ src/components/SetupDesign/SetupDesign.svelte
@@ -26,22 +26,21 @@
   }
 </script>
 
-<form on:change|preventDefault={(e) => update()}>
-  <div class="mb-10">
-    <label class="mb-1 flex flex-col" for="avatarPath">
-      <label class="mb-1" for="hero-image"> Image </label>
+<form on:change|preventDefault={(e) => update()} class="grid gap-16">
+  <div>
+    <label class="grid justify-items-start gap-2" for="avatarPath">
+      <label class="" for="hero-image"> Cover image </label>
       {#if homeConfig.hero.image && homeConfig.hero.image != ''}
-        <div class="w-32 cursor-pointer">
-          <img src={homeConfig.hero.image} alt="Hero" />
-        </div>
-      {:else}
-        <div class="float-left">
-          <span
-            class="cursor-pointer rounded-lg bg-[#040B10] px-12 py-4 text-sm font-medium"
-            type="button">Choose Image</span
-          >
-        </div>
+        <img
+          src={homeConfig.hero.image}
+          class="h-auto max-w-full rounded"
+          alt="Hero"
+        />
       {/if}
+      <span
+        class="cursor-pointer rounded bg-[#040B10] px-12 py-4 text-sm font-medium"
+        type="button">Upload to change</span
+      >
 
       <input
         id="hero-image"
@@ -53,9 +52,10 @@
     </label>
   </div>
 
-  <div class="mb-10 flex flex-col">
-    <label class="mb-1" for="hero-text"> Text </label>
+  <div class="grid justify-start gap-2">
+    <label for="hero-text"> Main copy </label>
     <input
+      size="50"
       class="rounded bg-[#040B10] px-8 py-4"
       bind:value={homeConfig.hero.text}
       id="hero-text"
@@ -63,13 +63,15 @@
     />
   </div>
 
-  <div class="mb-10 flex flex-col">
-    <label class="mb-1" for="club-description"> Club Description </label>
+  <div class="grid gap-2">
+    <label for="club-description"> Description to introduce about you </label>
     <textarea
+      rows="10"
       class="rounded bg-[#040B10] px-8 py-4"
       bind:value={homeConfig.body}
       id="club-description"
       name="club-description"
     />
+    <p class="text-sm">Markdown is available</p>
   </div>
 </form>
