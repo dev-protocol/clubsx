25d6333f5fe8229210f4091b435ba6808e874d67
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2022-12-22 16:00:58 +0530

    default reward value 0

--- src/plugins/admin/AdminOverview.svelte
+++ src/plugins/admin/AdminOverview.svelte
@@ -12,7 +12,7 @@
   let { propertyAddress, rpcUrl } = config
   const provider = new providers.JsonRpcProvider(rpcUrl)
   let members: number | undefined = 0
-  let earnings: number
+  let earnings: number | undefined = 0
   async function getData() {
     await detectStokensByPropertyAddress(provider, propertyAddress).then(
       (res) => {
