3daae30877b8ac4703d1cd97d50ce327659b6609
Yash Agrawal <iagrawalyash@gmail.com>
2023-01-10 12:23:56 +0530

    feat(integration): update publish step2 dynamic color logic

--- src/components/PublishNetworkSelection/PublishNetworkSelection.vue
+++ src/components/PublishNetworkSelection/PublishNetworkSelection.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- Step 2 -->
   <section class="flex grid-cols-2 items-center justify-start gap-8">
     <section class="grid w-[49%] gap-3">
       <p class="font-mono text-base font-normal">2/</p>
@@ -6,7 +7,7 @@
         class="align-items-center flex items-center justify-items-center gap-3"
       >
         <img alt="Status" :src="checkImage" class="h-6 w-6" />
-        <h2 v-bind:class="stepTextClasses">Choose network</h2>
+        <h2 v-bind:class="step2TextClasses">Choose network</h2>
       </section>
       <p class="text-base">What is the different between networks?</p>
     </section>
@@ -15,8 +16,15 @@
         @click="changeNetwork('polygon')"
         class="rounded border-[3px] border-[#000000] bg-[#040B10] py-6 text-center"
         v-bind:class="
-          connected && networkSelected === 'polygon' ? 'opacity-50' : ''
+          !connected
+            ? 'opacity-50'
+            : networkSelected === ''
+            ? ''
+            : networkSelected === 'polygon'
+            ? 'opacity-50'
+            : 'opacity-20'
         "
+        :disabled="!connected"
       >
         <p class="font-DMSans text-center text-base font-bold text-[#FFFFFF]">
           Polygon
@@ -29,8 +37,15 @@
         @click="changeNetwork('arbitrum')"
         class="rounded border-[3px] border-[#000000] bg-[#040B10] py-6 text-center"
         v-bind:class="
-          connected && networkSelected === 'arbitrum' ? 'opacity-50' : ''
+          !connected
+            ? 'opacity-50'
+            : networkSelected === ''
+            ? ''
+            : networkSelected === 'arbitrum'
+            ? 'opacity-50'
+            : 'opacity-20'
         "
+        :disabled="!connected"
       >
         <p class="font-DMSans text-center text-base font-bold text-[#FFFFFF]">
           Arbitrum
@@ -40,13 +55,62 @@
         @click="changeNetwork('ethereum')"
         class="rounded border-[3px] border-[#000000] bg-[#040B10] py-6 text-center"
         v-bind:class="
-          connected && networkSelected === 'ethereum' ? 'opacity-50' : ''
+          !connected
+            ? 'opacity-50'
+            : networkSelected === ''
+            ? ''
+            : networkSelected === 'ethereum'
+            ? 'opacity-50'
+            : 'opacity-20'
         "
+        :disabled="!connected"
       >
         <p class="font-DMSans text-center text-base font-bold text-[#FFFFFF]">
           Ethereum
         </p>
       </button>
+    </section>
+  </section>
+
+  <!-- Step 3 -->
+  <section class="flex grid-cols-2 items-start justify-start gap-8">
+    <section class="grid w-[49%] gap-3">
+      <p class="font-mono text-base font-normal">3/</p>
+      <h2 class="font-title text-2xl font-bold">Activate</h2>
+    </section>
+    <section class="w-[49%] p-4">
+      <section class="mb-5 flex items-center justify-between">
+        <section
+          class="align-items-center flex items-center justify-items-center gap-2"
+        >
+          <img alt="Status" :src="roundedSquareImage" class="h-3 w-3" />
+          <p class="font-DMSans text-base font-bold">1</p>
+        </section>
+        <div class="ml-4 mr-7 h-0 flex-1 border-[1px] border-white"></div>
+        <section
+          class="align-items-center flex items-center justify-items-center gap-2"
+        >
+          <img alt="Status" :src="roundedSquareImage" class="h-3 w-3" />
+          <p class="font-DMSans text-base font-bold">2</p>
+        </section>
+        <div class="ml-4 mr-7 h-0 flex-1 border-[1px] border-white"></div>
+        <section
+          class="align-items-center flex items-center justify-items-center gap-2"
+        >
+          <img alt="Status" :src="roundedSquareImage" class="h-3 w-3" />
+          <p class="font-DMSans text-base font-bold">3</p>
+        </section>
+      </section>
+      <button
+        class="mb-4 w-full rounded border-[3px] border-[#000000] bg-[#040B10] py-6 text-center"
+      >
+        <p class="font-DMSans text-center text-base font-bold text-[#FFFFFF]">
+          Activate
+        </p>
+      </button>
+      <p class="font-DMSans text-base font-normal text-white">
+        What is activating?
+      </p>
     </section>
   </section>
 </template>
@@ -62,12 +126,14 @@ type Data = {
   connected: boolean
   modalProvider?: Web3Modal
   connection?: typeof Connection
+  popupWindow: Window | null
 }
 
 export default defineComponent({
   name: 'PublishNetworkSelection',
   props: {
     checkImage: String,
+    roundedSquareImage: String,
   },
   data(): Data {
     return {
@@ -75,6 +141,7 @@ export default defineComponent({
       connection: undefined,
       networkSelected: '',
       connected: false,
+      popupWindow: null as Window | null,
     }
   },
   computed: {
@@ -84,9 +151,8 @@ export default defineComponent({
 
       return this.connected ? classes + ' opacity-50' : classes
     },
-    stepTextClasses() {
+    step2TextClasses() {
       const classes = 'font-title text-2xl font-bold'
-      console.log('This', this.networkSelected, !this.networkSelected)
       return this.connected
         ? this.networkSelected === '' || !this.networkSelected
           ? classes
@@ -116,6 +182,33 @@ export default defineComponent({
       if (!this.connected) return
 
       this.networkSelected = network
+    },
+
+    openNiwa(link: string) {
+      const popupLink = link + '?popup=true'
+      this.popupWindow = window.open(
+        popupLink,
+        'Niwa',
+        'popup,width=500,height=700'
+      )
+      if (this.popupWindow) {
+        this.popupWindow.addEventListener(
+          'message',
+          this.listenForAddress,
+          false
+        )
+      }
+    },
+
+    listenForAddress(event: MessageEvent<any>) {
+      if (event.source !== this.popupWindow) return
+
+      const { address } = event.data
+      if (!address) return
+
+      const link = `/almost-there?network=${this.networkSelected}&address=${address}`
+      window.location.href = link
+      return
     },
   },
 })
