d92306e7cdca67077b6928db239ccf7aa2ad052c
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-01-11 15:35:51 +0900

    user clubs page

---
+++ src/components/UserClubs/UserClubItem.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { ClubsConfiguration } from '@devprotocol/clubs-core'
+
+  export let config: ClubsConfiguration
+
+  const imagePath =
+    (config.options?.find((option) => option.key === 'avatarImgSrc')
+      ?.value as string) ?? ''
+</script>
+
+<div class="flex items-center rounded bg-white py-2 px-4 text-black">
+  <div class="h-16 w-16 overflow-hidden rounded-full">
+    <img class="w-full" src={imagePath} alt={config.name} />
+  </div>
+
+  <span class="ml-4 font-bold">{config.name}</span>
+</div>
