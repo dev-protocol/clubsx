ce75084992b8a50f65fbee55a4da59dfb098a3bb
Yash Agrawal <iagrawalyash@gmail.com>
2023-01-13 15:50:12 +0530

    feat(design): add designs for the theme page

--- src/components/AdminThemeForm/AdminThemeForm.svelte
+++ src/components/AdminThemeForm/AdminThemeForm.svelte
@@ -156,4 +156,44 @@
       Add link
     </button>
   </section>
+
+  <section class="mb-16 w-full">
+    <h2 class="mb-16 font-title text-2xl font-bold text-white">Theme</h2>
+    <img
+      alt="Cover"
+      src={whiteRightArrowImgSrc}
+      class="mb-16 h-[484px] w-[95.23%] border-[2px] border-white"
+    />
+    <p class="font-DMSans mb-[7px] text-base font-normal text-white">
+      Cover image
+    </p>
+    <button
+      class="hs-button is-filled mb-[62px] h-[72px] w-fit rounded border-[3px] border-[#000000] bg-[#040B10] py-6 px-8 text-base"
+      type="button"
+    >
+      Upload to change
+    </button>
+    <p class="font-DMSans mb-[7px] text-base font-normal text-white">
+      Main copy
+    </p>
+    <input
+      class="font-DMSans mb-[62px] h-[72px] rounded border-[3px] border-[#000000] bg-[#040B10] px-8 py-6 text-base font-normal text-[#C4C4C4]"
+      value={'Join'}
+      id="hero-image"
+      name="hero-image"
+      disabled={true}
+    />
+    <p class="font-DMSans mb-[7px] text-base font-normal text-white">
+      Description to introduce about you
+    </p>
+    <input
+      class="font-DMSans mb-[7px] h-[203px] w-[95.23%] rounded border-[3px] border-[#000000] bg-[#040B10] px-8 py-6 text-base font-normal text-[#C4C4C4]"
+      id="hero-image"
+      name="hero-image"
+      disabled={true}
+    />
+    <p class="font-DMSans text-xs font-normal text-white">
+      Markdown is available
+    </p>
+  </section>
 </form>
