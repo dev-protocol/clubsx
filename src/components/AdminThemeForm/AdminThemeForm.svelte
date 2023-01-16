eff1d884ab7eaccd6308bcbfed6a15a479527e17
Yash Agrawal <iagrawalyash@gmail.com>
2023-01-16 14:38:47 +0530

    feat(integration): add integration for homeConfig in theme page

--- src/components/AdminThemeForm/AdminThemeForm.svelte
+++ src/components/AdminThemeForm/AdminThemeForm.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  import Skeleton from '@components/Global/Skeleton.svelte'
   import { NavLink } from '@constants/navLink'
   import { setOptions } from '@devprotocol/clubs-core'
+  import { uploadImageAndGetPath } from '@fixtures/imgur'
   import type { HomeConfig } from '../../constants/homeConfig'
 
   export let navLinks: NavLink[]
@@ -11,6 +13,8 @@
   export let homeConfig: HomeConfig
   export let currentPluginIndex: number
   export let whiteRightArrowImgSrc: string
+  export let homeHeroDefaultImgSrc: string
+  let uploading = false
 
   const update = (e?: any) => {
     // We don't want to store file in config
@@ -77,6 +81,27 @@
   const removeNavLink = (display: string) => {
     navLinks = navLinks.filter((link) => link.display !== display)
   }
+
+  const onFileSelected = async (
+    e: Event & {
+      currentTarget: EventTarget & HTMLInputElement
+    }
+  ) => {
+    if (!e.currentTarget.files) {
+      return
+    }
+    uploading = true
+
+    const file = e.currentTarget.files[0]
+
+    homeConfig.hero.image = await uploadImageAndGetPath(file)
+    homeConfig = homeConfig
+    uploading = false
+  }
+
+  const onUploadClick = (id: string) => {
+    document.getElementById(id)?.click()
+  }
 </script>
 
 <form on:change|preventDefault={(e) => update(e)}>
@@ -91,7 +116,7 @@
           bind:value={link.display}
           id={`sidebarPrimaryLinks-{i}-display`}
           name={`sidebarPrimaryLinks-{i}-display`}
-          disabled={!!link.display && link.display !== ''}
+          placeholder={link.display}
         />
         <img alt="Status" src={whiteRightArrowImgSrc} class="h-6 w-6" />
         <input
@@ -99,6 +124,7 @@
           bind:value={link.path}
           id={`sidebarPrimaryLinks-{i}-path`}
           name={`sidebarPrimaryLinks-{i}-path`}
+          placeholder={link.path}
         />
         <button
           class="hs-button is-filled h-[72px] rounded border-[3px] border-[#000000] bg-[#040B10] py-4 px-6 text-base"
@@ -131,7 +157,7 @@
           bind:value={link.display}
           id={`sidebarLinks-{i}-display`}
           name={`sidebarLinks-{i}-display`}
-          disabled={!!link.display && link.display !== ''}
+          placeholder={link.display}
         />
         <img alt="Status" src={whiteRightArrowImgSrc} class="h-6 w-6" />
         <input
@@ -139,6 +165,7 @@
           bind:value={link.path}
           id={`sidebarLinks-{i}-path`}
           name={`sidebarLinks-{i}-path`}
+          placeholder={link.path}
         />
         <button
           class="hs-button is-filled h-[72px] rounded border-[3px] border-[#000000] bg-[#040B10] py-4 px-6 text-base"
@@ -171,7 +198,7 @@
           bind:value={link.display}
           id={`headerLinks-{i}-display`}
           name={`headerLinks-{i}-display`}
-          disabled={!!link.display && link.display !== ''}
+          placeholder={link.display}
         />
         <img alt="Status" src={whiteRightArrowImgSrc} class="h-6 w-6" />
         <input
@@ -179,6 +206,7 @@
           bind:value={link.path}
           id={`headerLinks-{i}-path`}
           name={`headerLinks-{i}-path`}
+          placeholder={link.path}
         />
         <button
           class="hs-button is-filled h-[72px] rounded border-[3px] border-[#000000] bg-[#040B10] py-4 px-6 text-base"
@@ -211,7 +239,7 @@
           bind:value={link.display}
           id={`socialLinks-{i}-display`}
           name={`socialLinks-{i}-display`}
-          disabled={!!link.display && link.display !== ''}
+          placeholder={link.display}
         />
         <img alt="Status" src={whiteRightArrowImgSrc} class="h-6 w-6" />
         <input
@@ -219,6 +247,7 @@
           bind:value={link.path}
           id={`socialLinks-{i}-path`}
           name={`socialLinks-{i}-path`}
+          placeholder={link.path}
         />
         <button
           class="hs-button is-filled h-[72px] rounded border-[3px] border-[#000000] bg-[#040B10] py-4 px-6 text-base"
@@ -249,7 +278,7 @@
           bind:value={link.display}
           id={`navLinks-{i}-display`}
           name={`navLinks-{i}-display`}
-          disabled={!!link.display && link.display !== ''}
+          placeholder={link.display}
         />
         <img alt="Status" src={whiteRightArrowImgSrc} class="h-6 w-6" />
         <input
@@ -257,6 +286,7 @@
           bind:value={link.path}
           id={`navLinks-{i}-path`}
           name={`navLinks-{i}-path`}
+          placeholder={link.path}
         />
         <button
           class="hs-button is-filled h-[72px] rounded border-[3px] border-[#000000] bg-[#040B10] py-4 px-6 text-base"
@@ -280,38 +310,59 @@
 
   <section class="mb-16 w-full">
     <h2 class="mb-16 font-title text-2xl font-bold text-white">Theme</h2>
-    <img
-      alt="Cover"
-      src={whiteRightArrowImgSrc}
-      class="mb-16 h-[484px] w-[95.23%] border-[2px] border-white"
-    />
+    {#if homeConfig.hero.image && homeConfig.hero.image != '' && uploading === false}
+      <img
+        alt="Cover"
+        src={homeConfig.hero.image === '' && homeConfig.hero.image
+          ? homeHeroDefaultImgSrc
+          : homeConfig.hero.image}
+        class="mb-16 h-[484px] w-[95.23%] rounded-[5px]"
+      />
+    {/if}
+    {#if uploading}
+      <div role="presentation" class="h-64 w-full">
+        <Skeleton />
+      </div>
+    {/if}
     <p class="font-DMSans mb-[7px] text-base font-normal text-white">
       Cover image
     </p>
-    <button
-      class="hs-button is-filled mb-[62px] h-[72px] w-fit rounded border-[3px] border-[#000000] bg-[#040B10] py-6 px-8 text-base"
-      type="button"
-    >
-      Upload to change
-    </button>
+    <div>
+      <button
+        on:click={() => onUploadClick(`hero-image`)}
+        type="button"
+        class="hs-button is-filled mb-[62px] h-[72px] w-fit rounded border-[3px] border-[#000000] bg-[#040B10] py-6 px-8 text-base"
+      >
+        Upload to change
+      </button>
+      <input
+        class="hs-button is-filled mb-[62px] h-[72px] w-fit rounded border-[3px] border-[#000000] bg-[#040B10] py-6 px-8 text-base"
+        style="display:none"
+        type="file"
+        id="hero-image"
+        on:change={onFileSelected}
+      />
+    </div>
     <p class="font-DMSans mb-[7px] text-base font-normal text-white">
       Main copy
     </p>
     <input
-      class="font-DMSans mb-[62px] h-[72px] rounded border-[3px] border-[#000000] bg-[#040B10] px-8 py-6 text-base font-normal text-[#C4C4C4]"
-      value={'Join'}
-      id="hero-image"
-      name="hero-image"
-      disabled={true}
+      class="font-DMSans mb-[62px] h-[72px] w-[95.23%] rounded border-[3px] border-[#000000] bg-[#040B10] px-8 py-6 text-base font-normal text-[#C4C4C4]"
+      bind:value={homeConfig.hero.text}
+      id="hero-text"
+      name="hero-text"
+      placeholder={homeConfig.hero.text}
     />
     <p class="font-DMSans mb-[7px] text-base font-normal text-white">
       Description to introduce about you
     </p>
-    <input
+    <textarea
+      rows="10"
       class="font-DMSans mb-[7px] h-[203px] w-[95.23%] rounded border-[3px] border-[#000000] bg-[#040B10] px-8 py-6 text-base font-normal text-[#C4C4C4]"
-      id="hero-image"
-      name="hero-image"
-      disabled={true}
+      id="club-description"
+      name="club-description"
+      placeholder={homeConfig.body}
+      bind:value={homeConfig.body}
     />
     <p class="font-DMSans text-xs font-normal text-white">
       Markdown is available
