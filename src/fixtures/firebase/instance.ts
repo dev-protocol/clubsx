0429c284a3777388a4cca3c4220813acbcbe0ea4
aggre <hiroyuki.aggre@gmail.com>
2023-01-19 08:03:15 +0900

    add singleton store for firebase (#456)

---
+++ src/fixtures/firebase/instance.ts
@@ -0,0 +1,14 @@
+import { Auth } from 'firebase-admin/auth'
+import { initializeFirebaseAdmin } from '@fixtures/firebase/initializeFirebaseAdmin'
+
+const store = new Set<Auth>()
+
+export const instanceStore = {
+  get initializedAdminApp() {
+    const i = store.values()
+    const fromStore = i.next().value as Auth | undefined
+    const res = fromStore ? fromStore : initializeFirebaseAdmin()
+    store.add(res)
+    return res
+  },
+}
