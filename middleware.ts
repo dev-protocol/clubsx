68b84e36e277f2ace2df39401415110cc1cc6700
aggre <hiroyuki.aggre@gmail.com>
2023-01-19 23:16:05 +0900

    Fix Vercel Bugs (#462)

--- middleware.ts
+++ middleware.ts
@@ -7,11 +7,11 @@ export const config = {
 export default function middleware(req: Request) {
   const url = new URL(req.url)
 
-  const hostname = req.headers.get('host')?.split('.') ?? []
-  const [tenant] = hostname
+  const hostnames = req.headers.get('host')?.split('.') ?? []
+  const [tenant] = hostnames
   const html = req.headers.get('accept')?.includes('text/html')
 
-  if (html && hostname.length > 2) {
+  if (html && hostnames.length > Number(process.env.DOMAIN_LENGTH)) {
     const pathname = `/sites_/${tenant}${url.pathname}`
     const destination = new URL(pathname, url.origin)
     return rewrite(destination, {
