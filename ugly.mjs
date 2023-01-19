311bbdbb7be059696399a86151a585d10ba4d20a
aggre <hiroyuki.aggre@gmail.com>
2023-01-19 22:43:50 +0900

    test

--- ugly.mjs
+++ ugly.mjs
@@ -1,12 +1,20 @@
+/* eslint-disable no-undef */
 import fs from 'fs-extra'
 
-const path =
-  './node_modules/@astrojs/vercel/dist/serverless/request-transform/node18.js'
+const fn = () => {
+  const paths = req.headers.host.split('.')
+  const urlFromHeader = headers['x-rewritten-url']
+  return (
+    urlFromHeader ||
+    (/\w+\.(preview)\.\w+\.\w+$/.test(req.headers.host)
+      ? base + '/sites_/' + paths[0] + req.url
+      : base + req.url)
+  )
+}
+
 fs.outputFileSync(
   path,
-  ((file) =>
-    file.replace(
-      'base + req.url',
-      `(() => headers['x-rewritten-url'] ? headers['x-rewritten-url'] : ((paths) => /\\w+\\.(preview)\\.\\w+\\.\\w+$/.test(req.headers.host) ? base + '/sites_/' + paths[0] + req.url : base + req.url)(req.headers.host.split('.')))()`
-    ))(fs.readFileSync(path, 'utf8'))
+  ((file) => file.replace('base + req.url', `(${fn.toString()})()`))(
+    fs.readFileSync(path, 'utf8')
+  )
 )
