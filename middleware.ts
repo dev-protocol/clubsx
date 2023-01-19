8d36c853e847eda39a57f8b4122209ba07e0112d
aggre <hiroyuki.aggre@gmail.com>
2023-01-19 22:05:28 +0900

    test

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 import { rewrite, next } from '@vercel/edge'
 
+const knownApp = /\w+\.(preview|preview-preview-preview)\.\w+\.\w+$/
+
 export const config = {
   matcher: ['/((?!api|assets|chunks|_vercel|[\\w-]+\\.\\w+).*)'],
 }
@@ -7,11 +9,12 @@ export const config = {
 export default function middleware(req: Request) {
   const url = new URL(req.url)
 
-  const hostname = req.headers.get('host')?.split('.') ?? []
-  const [tenant] = hostname
+  const host = req.headers.get('host') ?? ''
+  const hostnames = host.split('.') ?? []
+  const [tenant] = hostnames
   const html = req.headers.get('accept')?.includes('text/html')
 
-  if (html && hostname.length > 2) {
+  if (html && knownApp.test(host)) {
     const pathname = `/sites_/${tenant}${url.pathname}`
     const destination = new URL(pathname, url.origin)
     return rewrite(destination, {
