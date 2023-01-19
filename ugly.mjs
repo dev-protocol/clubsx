ea0625ee3073968be2041500f18480b5f7a8c425
aggre <hiroyuki.aggre@gmail.com>
2023-01-19 22:54:31 +0900

    test

--- ugly.mjs
+++ ugly.mjs
@@ -5,11 +5,12 @@ const path =
   './node_modules/@astrojs/vercel/dist/serverless/request-transform/node18.js'
 
 const fn = () => {
+  console.log('@', process.env.DOMAIN_LENGTH)
   const paths = req.headers.host.split('.')
   const urlFromHeader = headers['x-rewritten-url']
   return (
     urlFromHeader ||
-    (/\w+\.(preview)\.\w+\.\w+$/.test(req.headers.host)
+    (paths.length > process.env.DOMAIN_LENGTH
       ? base + '/sites_/' + paths[0] + req.url
       : base + req.url)
   )
