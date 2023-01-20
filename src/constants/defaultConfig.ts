3f4f05333085e5160ab5e7427e6b6aca8de50a43
aggre <hiroyuki.aggre@gmail.com>
2023-01-20 14:12:07 +0900

    add community/quests to default config

--- src/constants/defaultConfig.ts
+++ src/constants/defaultConfig.ts
@@ -69,6 +69,14 @@ export const defaultConfig: ClubsConfiguration = {
       options: [],
     },
     {
+      name: 'community',
+      options: [],
+    },
+    {
+      name: 'quests',
+      options: [],
+    },
+    {
       name: 'members',
       options: [],
     },
