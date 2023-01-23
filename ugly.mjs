266d33bb675c5c6d96c5bbd5491272688fc9c021
aggre <hiroyuki.aggre@gmail.com>
2023-01-23 20:06:38 +0900

    Revert "add logger to vercel build"
    This reverts commit 9a15fe4d98aad6a944118ecfb9302b888bd58e22.

--- ugly.mjs
+++ ugly.mjs
@@ -1,23 +1,12 @@
 import fs from 'fs-extra'
 
-const pathToRequestTransform =
+const path =
   './node_modules/@astrojs/vercel/dist/serverless/request-transform/node18.js'
-const pathToEntrypoint =
-  './node_modules/@astrojs/vercel/dist/serverless/entrypoint.js'
-
 fs.outputFileSync(
-  pathToRequestTransform,
+  path,
   ((file) =>
     file.replace(
       'base + req.url',
       `headers['x-rewritten-url'] ? headers['x-rewritten-url'] : base + req.url`
-    ))(fs.readFileSync(pathToRequestTransform, 'utf8'))
-)
-
-fs.outputFileSync(
-  pathToEntrypoint,
-  ((file) =>
-    file.replace('err.status', `(()=>err.status)(console.log({err}))`))(
-    fs.readFileSync(pathToEntrypoint, 'utf8')
-  )
+    ))(fs.readFileSync(path, 'utf8'))
 )
