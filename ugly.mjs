9a15fe4d98aad6a944118ecfb9302b888bd58e22
aggre <hiroyuki.aggre@gmail.com>
2023-01-23 19:59:57 +0900

    add logger to vercel build

--- ugly.mjs
+++ ugly.mjs
@@ -1,12 +1,23 @@
 import fs from 'fs-extra'
 
-const path =
+const pathToRequestTransform =
   './node_modules/@astrojs/vercel/dist/serverless/request-transform/node18.js'
+const pathToEntrypoint =
+  './node_modules/@astrojs/vercel/dist/serverless/entrypoint.js'
+
 fs.outputFileSync(
-  path,
+  pathToRequestTransform,
   ((file) =>
     file.replace(
       'base + req.url',
       `headers['x-rewritten-url'] ? headers['x-rewritten-url'] : base + req.url`
-    ))(fs.readFileSync(path, 'utf8'))
+    ))(fs.readFileSync(pathToRequestTransform, 'utf8'))
+)
+
+fs.outputFileSync(
+  pathToEntrypoint,
+  ((file) =>
+    file.replace('err.status', `(()=>err.status)(console.log({err}))`))(
+    fs.readFileSync(pathToEntrypoint, 'utf8')
+  )
 )
