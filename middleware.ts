8f00723c8f42aa0b1f38c58a1a89d182d9cd40ff
aggre <hiroyuki.aggre@gmail.com>
2023-01-24 11:02:26 +0900

    add redirects to middleware

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,34 @@ export const config = {
   matcher: ['/((?!api|assets|chunks|_vercel|[\\w-]+\\.\\w+).*)'],
 }
 
+const redirects = [
+  {
+    tenant: 'temples',
+    host: 'temples.clubs.stakes.social',
+    destination: 'https://temples.clubs.place',
+  },
+  {
+    tenant: 'kougenji',
+    host: 'kougenji.clubs.stakes.social',
+    destination: 'https://kougenji.clubs.place',
+  },
+]
+
 export default function middleware(req: Request) {
   const url = new URL(req.url)
 
-  const hostnames = req.headers.get('host')?.split('.') ?? []
+  const matchToRedirects = redirects.find(({ host }) => host === url.host)
+
+  if (matchToRedirects) {
+    return Response.redirect(
+      new URL(
+        `${url.pathname}${url.search ? url.search : ''}`,
+        matchToRedirects.destination
+      )
+    )
+  }
+
+  const hostnames = url.host.split('.') ?? []
   const [tenant] = hostnames
   const html = req.headers.get('accept')?.includes('text/html')
 
