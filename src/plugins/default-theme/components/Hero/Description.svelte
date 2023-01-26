9a672a6aaa47399da6e1301566c76d72d0b3fc79
aggre <hiroyuki.aggre@gmail.com>
2023-01-26 17:11:07 +0900

    wip: the default theme implementation

---
+++ src/plugins/default-theme/components/Hero/Description.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  let open = false
+
+  const onClick = () => {
+    console.log({ open })
+    open = !open
+  }
+</script>
+
+<button on:click={onClick}>
+  <p
+    class={`overflow-hidden text-left ${
+      open ? '' : 'close h-[10ex] lg:h-[30ex]'
+    }`}
+  >
+    <slot />
+  </p>
+</button>
+
+<style>
+  .close {
+    display: box;
+    box-orient: vertical;
+    line-clamp: 3;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+  }
+  @media (min-width: 890px) {
+    .close {
+      line-clamp: 10;
+      -webkit-line-clamp: 10;
+    }
+  }
+</style>
--- src/plugins/default-theme/index.ts
+++ src/plugins/default-theme/index.ts
@@ -1,29 +1,199 @@
-import type { Tiers } from '@constants/tier'
 import type { UndefinedOr } from '@devprotocol/util-ts'
-import {
+import type {
+  ClubsConfiguration,
   ClubsFunctionGetAdminPaths,
   ClubsFunctionGetLayout,
   ClubsFunctionGetPagePaths,
   ClubsFunctionThemePlugin,
-  ClubsPluginCategory,
+  ClubsPluginOptions,
   ClubsThemePluginMeta,
 } from '@devprotocol/clubs-core'
-import { default as Layout } from '@layouts/Default.astro'
-import { default as Index } from '@plugins/home/index.astro'
+import { ClubsPluginCategory } from '@devprotocol/clubs-core'
+import { default as Layout } from './layouts/Default.astro'
+import { default as Index } from './pages/index.astro'
 import { default as Admin } from './admin.astro'
 import type { HomeConfig } from '../../constants/homeConfig'
 import type { NavLink } from '@constants/navLink'
-import { default as Temples } from '@plugins/home/index-for-temples.astro'
 import type { Membership } from '@plugins/memberships'
 
-export const getPagePaths: ClubsFunctionGetPagePaths = async (
-  options,
-  { name, propertyAddress, rpcUrl, chainId, plugins, ...config }
-) => {
-  const tiers = options.find((opt) => opt.key === 'tiers')
-    ?.value as UndefinedOr<Tiers>
+import membershipOpt1 from '@assets/membership-opt-1.png'
+import membershipOpt2 from '@assets/membership-opt-2.png'
+import membershipOpt3 from '@assets/membership-opt-3.png'
+import { utils } from 'ethers'
 
-  const memberships = plugins
+const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+// TODO: THIS CONFIGURATION IS JUST FOR DEVELOPMENT
+const options: ClubsPluginOptions = [
+  {
+    key: 'homeConfig',
+    value: {
+      hero: {
+        image:
+          'https://images.unsplash.com/photo-1673422627994-ba82ef264160?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340',
+      },
+      description: lorem,
+      sections: [
+        {
+          title: 'About us',
+          body: {
+            image:
+              'https://images.unsplash.com/photo-1668977309586-50c338c48cf5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80',
+            description: lorem,
+          },
+        },
+        {
+          title: 'Mission',
+          body: {
+            image:
+              'https://images.unsplash.com/photo-1633150444294-e2c4848e504b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+            description: lorem,
+          },
+        },
+        {
+          title: 'Perks',
+          body: {
+            image:
+              'https://images.unsplash.com/photo-1674614076460-2e6a391cd5ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80',
+            description: lorem,
+          },
+        },
+      ],
+    },
+  },
+]
+// TODO: THIS CONFIGURATION IS JUST FOR DEVELOPMENT
+const config: ClubsConfiguration = {
+  name: 'WaveDAO',
+  twitterHandle: '@aggre_',
+  description: '',
+  url: '',
+  propertyAddress: '0x70a8B9a4B2d407a542c205adBbEA38289c3285eB',
+  chainId: 80001,
+  rpcUrl:
+    'https://polygon-mumbai.infura.io/v3/fa1acbd68f5c4484b1082e1cf876b920',
+  adminRolePoints: 0,
+  options: [
+    {
+      key: 'ogp',
+      value: { image: 'https://i.imgur.com/lcyO9Bn.jpg' },
+    },
+    {
+      key: 'headerLinks',
+      value: [
+        {
+          display: 'Kougenji',
+          path: 'https://kougenji.clubs.stakes.social/',
+        },
+      ],
+    },
+    {
+      key: 'socialLinks',
+      value: [
+        {
+          display: 'Twitter',
+          path: '#',
+          kind: 'twitter',
+        },
+        {
+          display: 'Discord',
+          path: '#',
+          kind: 'discord',
+        },
+      ],
+    },
+    {
+      key: 'sidebarPrimaryLinks',
+      value: [
+        {
+          display: 'Join',
+          path: '/join',
+        },
+      ],
+    },
+    {
+      key: 'navigationLinks',
+      value: [
+        {
+          display: 'Community',
+          path: '/community',
+        },
+        {
+          display: 'Perks',
+          path: '/perks',
+        },
+        {
+          display: 'Quests',
+          path: '#',
+          enable: false,
+        },
+        {
+          display: 'Updates',
+          path: '#',
+          enable: false,
+        },
+        {
+          display: 'Vote',
+          path: '#',
+          enable: false,
+        },
+      ],
+    },
+    {
+      key: 'avatarImgSrc',
+      value:
+        'https://images.unsplash.com/photo-1643631961497-238f8c59d171?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLWxpa2VkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=700&q=60',
+    },
+  ],
+  plugins: [
+    {
+      name: 'defaultTheme',
+      enable: true,
+      options: options,
+    },
+    {
+      name: 'memberships',
+      options: [
+        {
+          key: 'memberships',
+          value: [
+            {
+              id: 'preset-casual',
+              name: 'Casual',
+              imageSrc: membershipOpt1,
+              currency: 'ETH',
+              price: 0.003,
+              description: 'lorem ipsum',
+              payload: utils.toUtf8Bytes('Casual'),
+            },
+            {
+              id: 'preset-luxury',
+              name: 'Luxury',
+              imageSrc: membershipOpt2,
+              currency: 'ETH',
+              price: 1,
+              description: 'lorem ipsum',
+              payload: utils.toUtf8Bytes('Luxury'),
+            },
+            {
+              id: 'preset-edge',
+              name: 'Edge',
+              imageSrc: membershipOpt3,
+              currency: 'ETH',
+              price: 0.5,
+              description: 'lorem ipsum',
+              payload: utils.toUtf8Bytes('Edge'),
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
+export const getPagePaths: ClubsFunctionGetPagePaths = async (_, __) => {
+  const { name, propertyAddress, rpcUrl, chainId } = config
+
+  const memberships = config.plugins
     .find((plg) => plg.name === 'memberships')
     ?.options.find((opt) => opt.key === 'memberships')?.value as UndefinedOr<
     Membership[]
@@ -44,18 +214,14 @@ export const getPagePaths: ClubsFunctionGetPagePaths = async (
     (option) => option.key === 'avatarImgSrc'
   )?.value
 
-  const IS_TEMPLES_DAO =
-    propertyAddress === '0x541f7914ed2a4a8b477edc711fa349a77983f3ad'
-
   return homeConfig
     ? [
         {
           paths: [],
-          component: IS_TEMPLES_DAO ? Temples : Index,
+          component: Index,
           props: {
             name,
             propertyAddress,
-            tiers,
             memberships,
             homeConfig,
             rpcUrl,
@@ -80,10 +246,13 @@ export const getAdminPaths: ClubsFunctionGetAdminPaths = async (
   },
 ]
 
-export const getLayout: ClubsFunctionGetLayout = async () => ({
-  layout: Layout,
-  props: {},
-})
+export const getLayout: ClubsFunctionGetLayout = async (_, __) => {
+  const homeConfig = options.find((opt) => opt.key === 'homeConfig')?.value
+  return {
+    layout: Layout,
+    props: { config, homeConfig },
+  }
+}
 
 export const meta: ClubsThemePluginMeta = {
   displayName: 'Default theme',
