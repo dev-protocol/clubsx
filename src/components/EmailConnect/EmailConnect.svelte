1a0aeb4a6fce4e7f2fea34a8909b963204ba4d85
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-02-02 14:07:01 +0900

    reverts auth after changing backend

--- src/components/EmailConnect/EmailConnect.svelte
+++ src/components/EmailConnect/EmailConnect.svelte
@@ -26,6 +26,10 @@
         : JSON.stringify({ email }),
     })
       .then(() => {
+        // The link was successfully sent. Inform the user.
+        // Save the email locally so you don't need to ask the user for it again
+        // if they open the link on the same device.
+        window.localStorage.setItem('emailForSignIn', email)
         emailSent = true
       })
       .catch((err) => {
