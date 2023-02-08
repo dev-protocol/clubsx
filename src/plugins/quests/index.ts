f4fa649cd896b1a80f1effda32a1378cd94f47f8
aggre <hiroyuki.aggre@gmail.com>
2023-02-08 10:53:07 +0900

    impr: Add [text] to the menu buttons (#578)

--- src/plugins/quests/index.ts
+++ src/plugins/quests/index.ts
@@ -8,6 +8,8 @@ import {
 import { default as Index } from './index.astro'
 import { default as Admin } from './admin.astro'
 import { default as Id } from './[id].astro'
+import { default as AddNavigationLink } from '@components/AddNavigationLink/AddNavigationLink.astro'
+import type { NavLink } from '@constants/navLink'
 
 const questParams = ['quest_starter', 'quest_stake_100', 'quest_stake_500']
 
@@ -21,12 +23,22 @@ export const getPagePaths: ClubsFunctionGetPagePaths = async (_) => [
 
 export const getAdminPaths: ClubsFunctionGetAdminPaths = async (
   options,
-  {}
+  config
 ) => [
   {
     paths: ['quests'],
     component: Admin,
-    props: { options },
+    props: {
+      options,
+      forAddNavigationLink: {
+        config,
+        label: `Add 'Quests' to the menu`,
+        link: { display: 'Quests', path: '/quests' } as NavLink,
+      },
+    },
+    slots: {
+      'aside:after-built-in-buttons': AddNavigationLink,
+    },
   },
 ]
 
