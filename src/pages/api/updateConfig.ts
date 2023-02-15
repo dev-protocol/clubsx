08f79c98e614b5d719315020445b8f91b05c07ac
Yash Agrawal <iagrawalyash@gmail.com>
2023-02-15 15:44:22 +0530

    fix(api): fix updateConfig to use clubs-core authenticate()

--- src/pages/api/updateConfig.ts
+++ src/pages/api/updateConfig.ts
@@ -32,26 +32,14 @@ export const post = async ({ request }: { request: Request }) => {
     })
   }
 
-  const isInDraft = decode(config).options?.some(
-    (x) =>
-      x.key === '__draft' &&
-      (
-        x.value as {
-          isInDraft: boolean
-        }
-      ).isInDraft === true
-  )
-
-  const authenticated = isInDraft
-    ? true
-    : await authenticate({
-        message: hash,
-        signature: sig,
-        previousConfiguration,
-        provider: providers.getDefaultProvider(
-          decode(previousConfiguration).rpcUrl
-        ),
-      })
+  const authenticated = await authenticate({
+    message: hash,
+    signature: sig,
+    previousConfiguration,
+    provider: providers.getDefaultProvider(
+      decode(previousConfiguration).rpcUrl
+    ),
+  })
 
   if (!authenticated) {
     return new Response(JSON.stringify({}), { status: 401 })
