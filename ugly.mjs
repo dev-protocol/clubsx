181728f460918e1fd18655f1277c48bb213689b0
aggre <hiroyuki.aggre@gmail.com>
2023-03-01 12:31:47 +0900

    add {duplex: true} option

--- ugly.mjs
+++ ugly.mjs
@@ -1,38 +1,37 @@
 import fs from 'fs-extra'
 
-const path =
+const pathRequestTransform =
   './node_modules/@astrojs/vercel/dist/serverless/request-transform.js'
+const pathEntrypoint =
+  './node_modules/@astrojs/vercel/dist/serverless/entrypoint.js'
+
+// Rewrite the requested URL
+// Note: Requests rewritten by middleware (./middleware.ts) have `x-rewritten-url` header
 fs.outputFileSync(
-  path,
+  pathRequestTransform,
   ((file) =>
     file.replace(
       `base + req.url,`,
       `(()=>headers['x-rewritten-url'] ? headers['x-rewritten-url'] : base + req.url)(),`
-    ))(fs.readFileSync(path, 'utf8'))
+    ))(fs.readFileSync(pathRequestTransform, 'utf8'))
 )
+
+// Add duplex option (ref: https://github.com/nodejs/node/issues/46221#issuecomment-1426707013)
 fs.outputFileSync(
-  './node_modules/@astrojs/vercel/dist/serverless/entrypoint.js',
+  pathRequestTransform,
   ((file) =>
     file.replace(
-      `, req);`,
-      `, req).then(resultofgetrequest => { console.log({resultofgetrequest}); return resultofgetrequest;});`
-    ))(
-    fs.readFileSync(
-      './node_modules/@astrojs/vercel/dist/serverless/entrypoint.js',
-      'utf8'
-    )
-  )
+      `body: get_raw_body(req, bodySizeLimit)`,
+      `body: get_raw_body(req, bodySizeLimit), ...{duplex: true},`
+    ))(fs.readFileSync(pathRequestTransform, 'utf8'))
 )
+
+// Add error logger
 fs.outputFileSync(
-  './node_modules/@astrojs/vercel/dist/serverless/entrypoint.js',
+  pathEntrypoint,
   ((file) =>
     file.replace(
       `err.status || 400`,
       `(()=> { console.log({err}); return err.status || 400;})()`
-    ))(
-    fs.readFileSync(
-      './node_modules/@astrojs/vercel/dist/serverless/entrypoint.js',
-      'utf8'
-    )
-  )
+    ))(fs.readFileSync(pathEntrypoint, 'utf8'))
 )
