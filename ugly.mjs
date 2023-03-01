f50e0e0cff44999b090a930f0147fc2e7926ce41
aggre <hiroyuki.aggre@gmail.com>
2023-03-01 12:11:00 +0900

    add logger

--- ugly.mjs
+++ ugly.mjs
@@ -10,3 +10,29 @@ fs.outputFileSync(
       `(()=>headers['x-rewritten-url'] ? headers['x-rewritten-url'] : base + req.url)(),`
     ))(fs.readFileSync(path, 'utf8'))
 )
+fs.outputFileSync(
+  './node_modules/@astrojs/vercel/dist/serverless/entrypoint.js',
+  ((file) =>
+    file.replace(
+      `, req);`,
+      `, req).then(resultofgetrequest => { console.log({resultofgetrequest}); return resultofgetrequest;});`
+    ))(
+    fs.readFileSync(
+      './node_modules/@astrojs/vercel/dist/serverless/entrypoint.js',
+      'utf8'
+    )
+  )
+)
+fs.outputFileSync(
+  './node_modules/@astrojs/vercel/dist/serverless/entrypoint.js',
+  ((file) =>
+    file.replace(
+      `err.status || 400`,
+      `(()=> { console.log({err}); return err.status || 400;})()`
+    ))(
+    fs.readFileSync(
+      './node_modules/@astrojs/vercel/dist/serverless/entrypoint.js',
+      'utf8'
+    )
+  )
+)
