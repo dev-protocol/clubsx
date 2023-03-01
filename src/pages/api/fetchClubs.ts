a62fc146964ce00c5d69997312b9f93e6556abb2
aggre <hiroyuki.aggre@gmail.com>
2023-03-01 12:49:38 +0900

    Revert "add loggers to /api/fetchClubs"
    This reverts commit 589ea9ae434467ab39a60dfb33f349b85e3f8abc.

--- src/pages/api/fetchClubs.ts
+++ src/pages/api/fetchClubs.ts
@@ -8,14 +8,11 @@ export type ClubsData = {
 }
 
 export const post = async ({ request }: { request: Request }) => {
-  console.log('@@@', 1, { request })
   const { identifier } = (await request.json()) as {
     identifier: string
   }
-  console.log('@@@', 2, { identifier })
 
   if (!identifier) {
-    console.log('***', 1, { identifier })
     return new Response(
       JSON.stringify({ error: 'No user identifier passed' }),
       {
@@ -23,7 +20,6 @@ export const post = async ({ request }: { request: Request }) => {
       }
     )
   }
-  console.log('@@@', 3)
 
   const client = createClient({
     url: process.env.REDIS_URL,
@@ -36,8 +32,6 @@ export const post = async ({ request }: { request: Request }) => {
   })
   await client.connect()
 
-  console.log('@@@', 4, { client })
-
   client.on('error', (e) => {
     console.error('redis connection error: ', e)
   })
@@ -49,13 +43,10 @@ export const post = async ({ request }: { request: Request }) => {
     (await client.get(generateId(identifier))) ?? '[]'
   ) as ClubsData[] | null
   if (!userSites) {
-    console.log('***', 2, { userSites })
     return new Response(JSON.stringify({ error: 'No user sites found' }), {
       status: 400,
     })
   }
-
-  console.log('@@@', 5, { userSites })
 
   /**
    * Fetch user clubs configs
@@ -63,15 +54,12 @@ export const post = async ({ request }: { request: Request }) => {
   const configs = []
 
   for (const site of userSites) {
-    console.log('@@@', 6, { site })
     const config = await client.get(site.name)
     if (!config) continue
     configs.push(decode(config))
   }
 
   await client.disconnect()
-
-  console.log('@@@', 7, { configs })
 
   return new Response(JSON.stringify(configs), { status: 200 })
 }
