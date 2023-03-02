f9f2ec3f45eeec5d724f8d5a8e31d842f04e2ee8
Yash Agrawal <iagrawalyash@gmail.com>
2023-03-02 13:07:50 +0530

    feat: rename the `generateClubPluginsId`

--- src/fixtures/api/keys.ts
+++ src/fixtures/api/keys.ts
@@ -1,4 +1,4 @@
 export const generateId = (identifier: string) => `id::${identifier}`
 
 export const generateClubPluginsId = (ientifier: string) =>
-  `clubs::plugins::${ientifier}`
+  `club::plugins::${ientifier}`
