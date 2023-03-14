d13f672c085284999bc933cb1278f01453d41397
Yash Agrawal <iagrawalyash@gmail.com>
2023-03-14 19:19:35 +0530

    feat: add meta info to me plugin

--- src/plugins/me/index.ts
+++ src/plugins/me/index.ts
@@ -6,6 +6,8 @@ import {
   ClubsPluginMeta,
 } from '@devprotocol/clubs-core'
 import { default as Index } from './index.astro'
+import { default as Icon } from '@assets/CaseStudyMedia1.png'
+import { default as Readme } from './README.md'
 
 export const getPagePaths: ClubsFunctionGetPagePaths = async (
   _,
@@ -15,8 +17,17 @@ export const getPagePaths: ClubsFunctionGetPagePaths = async (
 export const getAdminPaths: ClubsFunctionGetAdminPaths = async () => []
 
 export const meta: ClubsPluginMeta = {
+  id: 'devprotocol:clubs:plugin:me',
   displayName: 'Me',
   category: ClubsPluginCategory.Uncategorized,
+  icon: Icon,
+  offer: {
+    price: 0,
+    priceCurrency: 'DEV',
+  },
+  description: `is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+  previewImages: [Icon, Icon, Icon],
+  readme: Readme,
 }
 
 export default {
