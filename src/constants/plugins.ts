ef63d6318b34db8b605112b7ec49f1b84ebb3648
Yash Agrawal <iagrawalyash@gmail.com>
2023-03-15 11:46:04 +0530

    feat: update installable plugins list

--- src/constants/plugins.ts
+++ src/constants/plugins.ts
@@ -7,13 +7,49 @@ export type InstallablePlugins = {
 
 export const installablePlugins: InstallablePlugins[] = [
   {
-    name: '@plugins/community',
+    name: 'community',
     srcType: 'local',
     src: '.',
     main: 'index.ts',
   },
   {
-    name: '@plugins/join',
+    name: 'default-theme',
+    srcType: 'local',
+    src: '.',
+    main: 'index.ts',
+  },
+  {
+    name: 'join',
+    srcType: 'npm',
+    src: '.',
+    main: 'index.ts',
+  },
+  {
+    name: 'me',
+    srcType: 'npm',
+    src: '.',
+    main: 'index.ts',
+  },
+  {
+    name: 'members',
+    srcType: 'npm',
+    src: '.',
+    main: 'index.ts',
+  },
+  {
+    name: 'memberships',
+    srcType: 'npm',
+    src: '.',
+    main: 'index.ts',
+  },
+  {
+    name: 'message',
+    srcType: 'npm',
+    src: '.',
+    main: 'index.ts',
+  },
+  {
+    name: 'quests',
     srcType: 'npm',
     src: '.',
     main: 'index.ts',
