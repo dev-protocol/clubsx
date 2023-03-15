253799cc21368f5e539982e28fb4ed3ccf206453
Yash Agrawal <iagrawalyash@gmail.com>
2023-03-15 16:03:37 +0530

    feat: update plugins installable type

--- src/constants/plugins.ts
+++ src/constants/plugins.ts
@@ -4,6 +4,7 @@ export type InstallablePlugins = {
   isExternalModule: boolean
   entryPoint: string // File name representing main entry point of plugin config.
   tag: 'NEW & UPCOMING' | 'THEME' | 'BASICS'
+  moduleNameForImport: string
 }
 
 export const installablePlugins: InstallablePlugins[] = [
@@ -13,6 +14,7 @@ export const installablePlugins: InstallablePlugins[] = [
     entryPoint: 'index.ts',
     tag: 'BASICS',
     isExternalModule: false,
+    moduleNameForImport: 'community',
   },
   {
     id: 'devprotocol:clubs:theme-1',
@@ -20,6 +22,7 @@ export const installablePlugins: InstallablePlugins[] = [
     entryPoint: 'index.ts',
     tag: 'THEME',
     isExternalModule: false,
+    moduleNameForImport: 'default-theme',
   },
   {
     id: 'devprotocol:clubs:plugin:join',
@@ -27,6 +30,7 @@ export const installablePlugins: InstallablePlugins[] = [
     entryPoint: 'index.ts',
     tag: 'BASICS',
     isExternalModule: false,
+    moduleNameForImport: 'join',
   },
   {
     id: 'devprotocol:clubs:plugin:me',
@@ -34,6 +38,7 @@ export const installablePlugins: InstallablePlugins[] = [
     entryPoint: 'index.ts',
     tag: 'BASICS',
     isExternalModule: false,
+    moduleNameForImport: 'me',
   },
   {
     id: 'devprotocol:clubs:plugin:members',
@@ -41,6 +46,7 @@ export const installablePlugins: InstallablePlugins[] = [
     entryPoint: 'index.ts',
     tag: 'BASICS',
     isExternalModule: false,
+    moduleNameForImport: 'members',
   },
   {
     id: 'devprotocol:clubs:simple-memberships',
@@ -48,6 +54,7 @@ export const installablePlugins: InstallablePlugins[] = [
     entryPoint: 'index.ts',
     tag: 'BASICS',
     isExternalModule: false,
+    moduleNameForImport: 'memberships',
   },
   {
     id: 'devprotocol:clubs:gated-contact-form',
@@ -55,6 +62,7 @@ export const installablePlugins: InstallablePlugins[] = [
     entryPoint: 'index.ts',
     tag: 'BASICS',
     isExternalModule: false,
+    moduleNameForImport: 'message',
   },
   {
     id: 'devprotocol:clubs:plugin:quests',
@@ -62,5 +70,6 @@ export const installablePlugins: InstallablePlugins[] = [
     entryPoint: 'index.ts',
     tag: 'BASICS',
     isExternalModule: false,
+    moduleNameForImport: 'quests',
   },
 ]
