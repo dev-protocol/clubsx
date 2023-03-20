d0102c4d914882a670e9a8f82551173aa356bf06
Yash Agrawal <iagrawalyash@gmail.com>
2023-03-20 15:25:33 +0530

    feat: add computation while deciding routes

--- src/plugins/marketplace/index.ts
+++ src/plugins/marketplace/index.ts
@@ -3,6 +3,7 @@ import {
   ClubsFunctionGetAdminPaths,
   ClubsFunctionGetPagePaths,
   ClubsFunctionPlugin,
+  ClubsPlugin,
   ClubsPluginCategory,
   ClubsPluginMeta,
 } from '@devprotocol/clubs-core'
@@ -23,13 +24,30 @@ export const getAdminPaths: ClubsFunctionGetAdminPaths = async (_, config) => {
     allInstallablePlugins = []
   }
 
+  const allPluginsPromise: Promise<PluginMeta>[] = allInstallablePlugins.map(
+    async (plugin: InstallablePlugins) => {
+      const importedPlugin: ClubsFunctionPlugin = plugin.isExternalModule
+        ? await import(`${plugin.moduleNameForImport}/${plugin.entryPoint}`)
+        : await import(`../${plugin.moduleNameForImport}/${plugin.entryPoint}`)
+
+      return {
+        ...importedPlugin.meta,
+        added: !!config.plugins.find(
+          (plg: ClubsPlugin) => plg.id === plugin.id // TODO: use plugin.id in future.
+        ),
+        tag: plugin.tag,
+      }
+    }
+  )
+  const allPlugins: PluginMeta[] = await Promise.all(allPluginsPromise)
+
   return [
     {
       paths: ['marketplace'],
       component: Admin,
-      props: { config, showAside: false, allInstallablePlugins },
+      props: { config, showAside: false, allPlugins },
     },
-    ...allInstallablePlugins.map((plugin) => ({
+    ...allPlugins.map((plugin) => ({
       paths: ['marketplace', plugin.id],
       component: Plugins,
       props: { config, showAside: false },
