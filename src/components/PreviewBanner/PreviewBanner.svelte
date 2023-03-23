355e3f2cf2fb497e08e823e98d20389c9536adf8
aggre <hiroyuki.aggre@gmail.com>
2023-03-23 17:11:27 +0900

    ui tweaking: for home

--- src/components/PreviewBanner/PreviewBanner.svelte
+++ src/components/PreviewBanner/PreviewBanner.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   export let site: string | undefined
   export let baseUrl: string
+  export let inAdmin: boolean = false
 
   const dist = site ? `${baseUrl}/${site}/setup/publish` : `${baseUrl}/signin`
 </script>
@@ -9,9 +10,12 @@
   href={dist}
   class="fixed bottom-0 left-1 right-1 z-50 block rounded-t-3xl bg-black/50 py-4 px-8 backdrop-blur-3xl transition-all hover:bg-black/70 hover:py-8"
 >
-  <p class="mb-2 text-center text-sm opacity-70">
-    If you're using email sign-on, setting updating is unavailable in the draft.
-  </p>
+  {#if inAdmin}
+    <p class="mb-2 text-center text-sm opacity-70">
+      If you're using email sign-on, setting updating is unavailable in the
+      draft.
+    </p>
+  {/if}
   <div class="flex items-center justify-center">
     <p class="mr-4 rounded border py-1 px-4">Preview</p>
     <p class="font-bold">Send blockchain transactions to complete process</p>
