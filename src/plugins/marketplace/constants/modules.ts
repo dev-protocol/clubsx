1d8052ff67214376846815af34ad00d38e39a411
aggre <hiroyuki.aggre@gmail.com>
2023-03-23 15:53:45 +0900

    try a different way

--- src/plugins/marketplace/constants/modules.ts
+++ src/plugins/marketplace/constants/modules.ts
@@ -1,10 +1,19 @@
+import $1 from '@plugins/community'
+import $2 from '@plugins/default-theme'
+import $3 from '@plugins/join'
+import $4 from '@plugins/me'
+import $5 from '@plugins/members'
+import $6 from '@plugins/memberships'
+import $7 from '@plugins/message'
+import $8 from '@plugins/quests'
+
 export const modules = {
-  'devprotocol:clubs:plugin:community': () => import('@plugins/community'),
-  'devprotocol:clubs:theme-1': () => import('@plugins/default-theme'),
-  'devprotocol:clubs:plugin:join': () => import('@plugins/join'),
-  'devprotocol:clubs:plugin:me': () => import('@plugins/me'),
-  'devprotocol:clubs:plugin:members': () => import('@plugins/members'),
-  'devprotocol:clubs:simple-memberships': () => import('@plugins/memberships'),
-  'devprotocol:clubs:gated-contact-form': () => import('@plugins/message'),
-  'devprotocol:clubs:plugin:quests': () => import('@plugins/quests'),
+  'devprotocol:clubs:plugin:community': $1,
+  'devprotocol:clubs:theme-1': $2,
+  'devprotocol:clubs:plugin:join': $3,
+  'devprotocol:clubs:plugin:me': $4,
+  'devprotocol:clubs:plugin:members': $5,
+  'devprotocol:clubs:simple-memberships': $6,
+  'devprotocol:clubs:gated-contact-form': $7,
+  'devprotocol:clubs:plugin:quests': $8,
 }
