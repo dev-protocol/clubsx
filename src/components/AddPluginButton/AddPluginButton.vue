5a92ffce6cc3ae5c7ba55fa8e5b50a9fc331a53d
Yash Agrawal <iagrawalyash@gmail.com>
2023-03-23 13:38:11 +0530

    feat: seperate out add button from /admin/marketplace/plugin page

---
+++ src/components/AddPluginButton/AddPluginButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    class="m-0 w-full max-w-full whitespace-normal text-center text-base font-bold leading-normal text-white"
+    :disabled="isAdded"
+  >
+    Add to your {{ clubName }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import type { PluginMeta } from '@constants/plugins'
+
+export default defineComponent({
+  name: 'AddPluginButton',
+  props: {
+    clubName: {
+      type: String,
+      default: 'Club',
+    },
+    plugin: {
+      type: Object as PropType<PluginMeta>,
+      default: {},
+    },
+  },
+  computed: {
+    isAdded(): boolean {
+      return this.plugin.added
+    },
+  },
+})
+</script>
