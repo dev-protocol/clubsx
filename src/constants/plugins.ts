16ee7aa4397d37305888459abca21355c5707351
aggre <hiroyuki.aggre@gmail.com>
2023-03-26 00:01:41 +0900

    Add "Feeds" plugin as upcoming plugin

--- src/constants/plugins.ts
+++ src/constants/plugins.ts
@@ -1,37 +1,56 @@
 import type {
+  ClubsFunctionPlugin,
   ClubsPluginMeta,
   ClubsPluginOptions,
   ClubsThemePluginMeta,
 } from '@devprotocol/clubs-core'
+import Feeds from '@assets/Plugins/Feeds.svg'
 
 export type InstallablePlugins = {
   id: string
-  name: string
-  configName: string
-  isExternalModule: boolean
-  entryPoint: string // File name representing main entry point of plugin config.
+  name?: string
+  configName?: string
+  isExternalModule?: boolean
+  entryPoint?: string // File name representing main entry point of plugin config.
   tag: 'NEW & UPCOMING' | 'THEME' | 'BASICS'
-  moduleNameForImport: string
-  developer: string
-  clubsUrl: string
-  repositoryUrl: string
+  moduleNameForImport?: string
+  developer?: string
+  clubsUrl?: string
+  repositoryUrl?: string
   pluginOptions: ClubsPluginOptions
+  planned?: ClubsFunctionPlugin
 }
 
 export type PluginMeta = (ClubsPluginMeta & Partial<ClubsThemePluginMeta>) & {
   added: boolean
   tag: 'NEW & UPCOMING' | 'THEME' | 'BASICS'
-  developer: string
-  repositoryUrl: string
-  clubsUrl: string
+  developer?: string
+  repositoryUrl?: string
+  clubsUrl?: string
+  planned: boolean
 }
 
 export const installablePlugins: InstallablePlugins[] = [
   {
+    id: 'upcoming:feeds',
+    tag: 'NEW & UPCOMING',
+    pluginOptions: [],
+    planned: {
+      meta: {
+        id: '#',
+        category: '',
+        displayName: 'Feeds',
+        icon: Feeds,
+        description:
+          'Timeline-like post feed, where you can use token-gated posts by membership, or public posts, and communication with comments and emoji reaction.',
+      },
+    },
+  },
+  {
     id: 'devprotocol:clubs:plugin:community',
     name: 'community',
     entryPoint: 'index.ts',
-    tag: 'NEW & UPCOMING',
+    tag: 'BASICS',
     isExternalModule: false,
     moduleNameForImport: 'community',
     configName: 'community',
@@ -70,7 +89,7 @@ export const installablePlugins: InstallablePlugins[] = [
     id: 'devprotocol:clubs:plugin:me',
     name: 'me',
     entryPoint: 'index.ts',
-    tag: 'NEW & UPCOMING',
+    tag: 'BASICS',
     isExternalModule: false,
     moduleNameForImport: 'me',
     configName: 'me',
