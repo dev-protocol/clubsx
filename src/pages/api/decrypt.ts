a5d9c6ca68b44a6a67748ed02bd29fdfab28d4bd
aggre <hiroyuki.aggre@gmail.com>
2023-04-04 12:49:08 +0900

    revert single file: src/pages/api/decrypt.ts

--- src/pages/api/decrypt.ts
+++ src/pages/api/decrypt.ts
@@ -1,15 +1,37 @@
 import { authenticate, decode } from '@devprotocol/clubs-core'
 import { providers } from 'ethers'
 import jsonwebtoken from 'jsonwebtoken'
+import { createClient } from 'redis'
 
 export const post = async ({ request }: { request: Request }) => {
-  const { encryptedText, hash, sig, configuration } =
-    (await request.json()) as {
-      configuration: string
-      encryptedText: string
-      hash?: string
-      sig?: string
-    }
+  const { encryptedText, hash, sig, site } = (await request.json()) as {
+    site: string
+    encryptedText: string
+    hash?: string
+    sig?: string
+  }
+
+  const client = createClient({
+    url: process.env.REDIS_URL,
+    username: process.env.REDIS_USERNAME ?? '',
+    password: process.env.REDIS_PASSWORD ?? '',
+    socket: {
+      keepAlive: 1,
+      reconnectStrategy: 1,
+    },
+  })
+  await client.connect()
+
+  client.on('error', (e) => {
+    console.error('redis connection error: ', e)
+  })
+
+  const previousConfiguration = await client.get(site)
+  if (!previousConfiguration) {
+    return new Response(JSON.stringify({ error: 'Encoded config not found' }), {
+      status: 401,
+    })
+  }
 
   // We need either hash and sig
   const hashAndSignGiven = !!hash && !!sig
@@ -22,8 +44,10 @@ export const post = async ({ request }: { request: Request }) => {
   const authenticated = await authenticate({
     message: hash,
     signature: sig,
-    previousConfiguration: configuration,
-    provider: providers.getDefaultProvider(decode(configuration).rpcUrl),
+    previousConfiguration,
+    provider: providers.getDefaultProvider(
+      decode(previousConfiguration).rpcUrl
+    ),
   })
 
   if (!authenticated) {
