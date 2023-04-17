6c183261a4a768699e4eeb6aa948064aef5bb17c
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2023-04-18 02:34:59 +0700

    new changes

--- src/components/Modal/modal.tsx
+++ src/components/Modal/modal.tsx
@@ -1,56 +1,54 @@
-import { useState } from "react";
-import type { ClubsConfiguration } from "@devprotocol/clubs-core";
+import { useState } from 'react'
+import type { ClubsConfiguration } from '@devprotocol/clubs-core'
 
-export default function Modal(config: ClubsConfiguration, propertyAddress: string) {
-  const [showModal, setShowModal] = useState(false);
+export default function Modal() {
+  // config: ClubsConfiguration,
+  // propertyAddress: string
+  const [showModal, setShowModal] = useState(false)
   return (
-    <>
+    <div>
       <button
-        className="font-bold uppercase text-sm px-6 py-3 mr-1 mb-1 text-dp-blue-500 underline"
+        className="text-dp-blue-500 mb-1 mr-1 px-6 py-3 text-sm font-bold uppercase underline"
         type="button"
         onClick={() => setShowModal(true)}
       >
         Already Tokenised?↗
       </button>
       {showModal ? (
-        <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+        <div>
+          <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
+            <div className="relative mx-auto my-6 w-auto max-w-3xl">
               {/* content */}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
                 {/* header */}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
-                  </h3>
+                <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
+                  <h3 className="text-3xl font-semibold">Modal Title</h3>
                   <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    className="float-right ml-auto border-0 bg-transparent p-1 text-3xl font-semibold leading-none text-black opacity-5 outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    <span className="block h-6 w-6 bg-transparent text-2xl text-black opacity-5 outline-none focus:outline-none">
                       ×
                     </span>
                   </button>
                 </div>
                 {/* body */}
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                <div className="relative flex-auto p-6">
+                  <p className="my-4 text-lg leading-relaxed text-slate-500">
                     Property Address
                   </p>
                 </div>
                 {/* footer */}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                <div className="flex items-center justify-end rounded-b border-t border-solid border-slate-200 p-6">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="background-transparent mb-1 mr-1 px-6 py-2 text-sm font-bold uppercase text-red-500 outline-none transition-all duration-150 ease-linear focus:outline-none"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
                     Close
                   </button>
                   <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="mb-1 mr-1 rounded bg-emerald-500 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-emerald-600"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
@@ -60,9 +58,9 @@ export default function Modal(config: ClubsConfiguration, propertyAddress: strin
               </div>
             </div>
           </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
+          <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+        </div>
       ) : null}
-    </>
-  );
+    </div>
+  )
 }
