e63648e107f85a3e147990e60ffd700808ee5fb5
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-04-18 17:42:25 +0900

    adds DEV as default swap

--- src/components/Swap/Swap.tsx
+++ src/components/Swap/Swap.tsx
@@ -3,9 +3,15 @@ import { SwapWidget } from '@uniswap/widgets'
 import '@uniswap/widgets/fonts.css'
 
 const Swap: React.FC = () => {
+  const defaultOutputTokenAddress = {
+    1: '0x5cAf454Ba92e6F2c929DF14667Ee360eD9fD5b26',
+    137: '0xA5577D1cec2583058A6Bd6d5DEAC44797c205701', // mainnet polygon,
+    42161: '0x91F5dC90979b058eBA3be6B7B7e523df7e84e137', // mainnet arbitrum
+  }
+
   return (
     <div className="Uniswap">
-      <SwapWidget />
+      <SwapWidget defaultOutputTokenAddress={defaultOutputTokenAddress} />
     </div>
   )
 }
