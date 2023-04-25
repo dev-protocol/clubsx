340d68e8ccf43dbbc15566b02be1742192bfd952
Yash Agrawal <yash@devprotocol.xyz>
2023-04-25 15:39:51 +0530

    feat(design): add implementation of a single input field

---
+++ src/components/Collections/CreateCollections.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  const update = (e?: any) => {}
+</script>
+
+<form on:change|preventDefault={(e) => update(e)} class="w-full">
+  <div class="w-full">
+    <div
+      class="mb-[62px] flex w-[52.2%] flex-col items-start justify-start gap-[7px]"
+    >
+      <div class="m-0 w-full items-center p-0">
+        <span class="mr-[13px] font-body"> Collection name </span>
+        <span class="font-body text-[#EB48F8]"> * </span>
+      </div>
+      <input
+        class="w-full rounded border-[3px] border-black bg-[#040B10] px-8 py-6"
+        id="hero-image"
+        name="hero-image"
+      />
+    </div>
+  </div>
+</form>
