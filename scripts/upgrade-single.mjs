e7018868299b7047013e7dff9aaaf2b8179c5127
aggre <hiroyuki.aggre@gmail.com>
2023-05-09 16:43:04 +0900

    add "upgrade-single.mjs"

---
+++ scripts/upgrade-single.mjs
@@ -0,0 +1,48 @@
+import { createClient } from 'redis'
+import dotenv from 'dotenv'
+import { decode, encode } from '@devprotocol/clubs-core'
+
+dotenv.config()
+
+const KEY = 'xxx'
+
+const upgrade = (config) => {
+  const deocdedConfig = decode(config)
+  const upgradedConfig = { ...deocdedConfig }
+
+  /**
+   * Write upgrading script here
+   */
+
+  return encode(upgradedConfig)
+}
+
+const app = async () => {
+  try {
+    const client = createClient({
+      url: process.env.REDIS_URL,
+      username: process.env.REDIS_USERNAME ?? '',
+      password: process.env.REDIS_PASSWORD ?? '',
+    })
+    await client.connect()
+
+    const encodedConfig = await client.get(KEY)
+    const upgradedConfig = upgrade(encodedConfig)
+
+    if (encodedConfig === upgradedConfig) {
+      console.log('Up-to-date', KEY)
+    } else {
+      await client.set(KEY, upgradedConfig)
+      console.log('Upgraded:', KEY)
+      console.log('DB Upgraded')
+    }
+
+    await client.quit()
+    console.log('Closed the DB connection')
+    return
+  } catch (error) {
+    console.error('error upgrading db: ', error)
+  }
+}
+
+app()
