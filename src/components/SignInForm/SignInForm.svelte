2a90dcadc2fec165a3a3aee4e66a65d031164319
Matteu <matteugt@gmail.com>
2023-05-31 22:06:53 +0800

    UPDATE: Update signin UI

--- src/components/SignInForm/SignInForm.svelte
+++ src/components/SignInForm/SignInForm.svelte
@@ -27,6 +27,35 @@
   }
 </script>
 
+<style lang="scss">
+  @use '@devprotocol/hashi/hs-button';
+
+  @include hs-button.extend('filled.native-blue') {
+    @include hs-button.color((
+      fill: 'native-blue.400',
+      ink: 'native-blue.ink',
+      border: 'native-blue.400'
+    ));
+
+    &:hover,
+    &:focus {
+      @include hs-button.color((
+        fill: 'native-blue.300',
+        ink: 'native-blue.ink',
+        border: 'native-blue.300'
+      ));
+    }
+
+    &:active {
+      @include hs-button.color((
+        fill: 'native-blue.200',
+        ink: 'native-blue.ink',
+        border: 'native-blue.200'
+      ));
+    }
+  }
+</style>
+
 <div class="relative grid justify-center p-4 md:p-0">
   <section class="my-16 grid gap-8 text-center md:my-32">
     <h1 class="text-2xl font-bold md:text-5xl">Sign in to Clubs</h1>
@@ -37,7 +66,7 @@
       <span class="mb-4">Signin with your wallet</span>
 
       <button
-        class={`hs-button is-filled bg-native-blue-300 px-8 py-4 text-inherit ${
+        class={`hs-button is-filled is-native-blue is-large ${
           !GetModalProvider || !EthersProviderFrom
             ? 'animate-pulse bg-gray-500/60'
             : ''
@@ -48,7 +77,12 @@
         Sign with your wallet
       </button>
     </div>
-
+    <p
+      role="separator"
+      class="grid grid-cols-[1fr_auto_1fr] items-center gap-4 before:block before:border-b before:border-white/20 before:content-[''] after:block after:border-b after:border-white/20 after:content-['']"
+    >
+      or
+    </p>
     <div class="flex flex-col items-center">
       <span class="mb-4">Signin with email</span>
       <EmailConnect />
@@ -57,7 +91,7 @@
       role="separator"
       class="grid grid-cols-[1fr_auto_1fr] items-center gap-4 before:block before:border-b before:border-white/20 before:content-[''] after:block after:border-b after:border-white/20 after:content-['']"
     >
-      or
+      or maybe...
     </p>
     <p class="text-center">
       <a class="hs-button is-outlined text-native-blue-300" href="/domain"
