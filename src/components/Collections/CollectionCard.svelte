7d4ba73e55ff2e27dc2bf760ccfa9622fee53089
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2023-08-10 01:24:59 +0530

    add: new-limited-edition-admin-section

---
+++ src/components/Collections/CollectionCard.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+</script>
+
+<div
+  class="flex max-w-4xl flex-col items-start gap-4 rounded-2xl rounded-2xl bg-white"
+>
+  <div class="flex items-start gap-2.5 self-stretch">
+    <img
+      class="self-stretch rounded-t-2xl"
+      src="https://i.ibb.co/RbxFzn8/img.jpg"
+      alt="collection card banner"
+    />
+  </div>
+  <div class="flex flex-col items-start gap-4 self-stretch p-4">
+    <p class="text-center text-3xl font-normal text-black">
+      Foo this is a collection
+    </p>
+    <div class="flex flex-col items-start gap-2.5 rounded bg-[#6D9BFF] px-2.5">
+      <p class="text-center text-lg font-medium text-black">Time Limited</p>
+    </div>
+    <p class="overflow-hidden text-ellipsis text-base font-normal text-black">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <div class="flex items-start gap-2.5">
+      <img
+        class="h-16 w-16 rounded-md"
+        src="https://via.placeholder.com/60x60"
+        alt="membership image"
+      />
+      <img
+        class="h-16 w-16 rounded-md"
+        src="https://via.placeholder.com/60x60"
+        alt="membership image"
+      />
+      <img
+        class="h-16 w-16 rounded-md"
+        src="https://via.placeholder.com/60x60"
+        alt="membership image"
+      />
+      <img
+        class="h-16 w-16 rounded-md"
+        src="https://via.placeholder.com/60x60"
+        alt="membership image"
+      />
+      <img
+        class="h-16 w-16 rounded-md"
+        src="https://via.placeholder.com/60x60"
+        alt="membership image"
+      />
+      <img
+        class="h-16 w-16 rounded-md"
+        src="https://via.placeholder.com/60x60"
+        alt="membership image"
+      />
+    </div>
+  </div>
+</div>
