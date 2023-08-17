ddea3fdb3197cc8b9341ce4be577e0f613f5ae04
aggre <hiroyuki.aggre@gmail.com>
2023-08-17 14:50:06 +0900

    update

--- src/components/Checkout/Checkout.vue
+++ src/components/Checkout/Checkout.vue
@@ -27,29 +27,9 @@
             @click="approve"
             v-if="account && (approveNeeded || approveNeeded === undefined)"
             :disabled="isApproving || approveNeeded === undefined"
-            class="rounded-full bg-black px-8 py-4 text-center font-bold text-white disabled:bg-neutral-300"
+            :data-is-approving="isApproving"
+            class="rounded-full bg-black px-8 py-4 text-center font-bold text-white disabled:bg-neutral-300 data-[is-approving=true]:animate-pulse"
           >
-            <svg
-              v-if="isApproving"
-              class="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                class="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                stroke-width="4"
-              ></circle>
-              <path
-                class="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
             Sign with wallet and approve
           </button>
           <button
