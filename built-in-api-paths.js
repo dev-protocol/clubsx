e7d2d9bfdfd19bbca81a16e7fa8fdc89ed0bf380
aggre <hiroyuki.aggre@gmail.com>
2023-08-24 11:38:54 +0900

    commit

---
+++ built-in-api-paths.js
@@ -0,0 +1,14 @@
+export default [
+  '/api/verifySiteName',
+  '/api/updateDraftConfig',
+  '/api/updateConfig',
+  '/api/stats',
+  '/api/profile',
+  '/api/plugins',
+  '/api/mock',
+  '/api/hasCreationLimitReached',
+  '/api/fetchClubs',
+  '/api/encrypt',
+  '/api/decrypt',
+  '/api/addDaoToDraft',
+]
