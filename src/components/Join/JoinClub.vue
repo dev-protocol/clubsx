07e30fe2a510b86688a229191a79e9f77e1f195c
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2023-08-30 12:21:21 +0530

    lint

--- src/components/Join/JoinClub.vue
+++ src/components/Join/JoinClub.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="bg-dp-blue-grey-300 flex flex-col rounded-xl p-4 shadow">
+  <section class="flex flex-col rounded-xl bg-dp-blue-grey-300 p-4 shadow">
     <h2 class="mb-4 text-4xl font-bold">Join</h2>
     <!-- DAOName from YAML config -->
     <div class="mb-8">Join {{ tenantName }} in support of the project.</div>
