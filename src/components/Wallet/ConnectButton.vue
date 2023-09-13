cafc8be6e3f070ffd03246221fbe4925a2d7164d
aggre <hiroyuki.aggre@gmail.com>
2023-09-13 16:19:57 +0900

    Revert "revert it and make it client:only"
    This reverts commit 16cf524b009de2588070a8be8e7444b3931ffccb.

--- src/components/Wallet/ConnectButton.vue
+++ src/components/Wallet/ConnectButton.vue
@@ -1,9 +1,7 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue'
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/vue'
 import { mainnet, polygon, polygonMumbai } from '@wagmi/core/chains'
-import { useWeb3Modal } from '@web3modal/wagmi/vue'
-import { watchWalletClient } from '@wagmi/core'
+import type { useWeb3Modal } from '@web3modal/wagmi/vue'
 import { whenDefined } from '@devprotocol/util-ts'
 import { BrowserProvider } from 'ethers'
 
@@ -21,6 +19,7 @@ const projectId =
   props.projectId ?? import.meta.env.PUBLIC_WALLET_CONNECT_PROJECT_ID
 
 const truncatedAddress = ref<string>()
+const modal = ref<ReturnType<typeof useWeb3Modal>>()
 const error = ref<Error>()
 const truncateAddress = (address: string) => {
   const match = address.match(
@@ -39,18 +38,24 @@ const defaultChain =
     ? mainnet
     : polygon
 
-const wagmiConfig = defaultWagmiConfig({
-  chains: [polygon, polygonMumbai, mainnet],
-  projectId,
-  appName: 'Web3Modal',
-})
+const initWeb3Modal = async () => {
+  const [
+    { createWeb3Modal, defaultWagmiConfig, useWeb3Modal },
+    { watchWalletClient },
+    { connection },
+  ] = await Promise.all([
+    import('@web3modal/wagmi/vue'),
+    import('@wagmi/core'),
+    import('@devprotocol/clubs-core/connection'),
+  ])
+  const wagmiConfig = defaultWagmiConfig({
+    chains: [polygon, polygonMumbai, mainnet],
+    projectId,
+    appName: 'Web3Modal',
+  })
 
-createWeb3Modal({ wagmiConfig, projectId, chains, defaultChain })
+  createWeb3Modal({ wagmiConfig, projectId, chains, defaultChain })
 
-const modal = useWeb3Modal()
-
-onMounted(async () => {
-  const { connection } = await import('@devprotocol/clubs-core/connection')
   watchWalletClient({}, (wallet) => {
     console.log({ wallet })
     whenDefined(wallet, (wal) =>
@@ -73,6 +78,13 @@ onMounted(async () => {
         : undefined,
     )
   })
+
+  modal.value = useWeb3Modal()
+}
+
+onMounted(async () => {
+  initWeb3Modal()
+  document.addEventListener('astro:after-swap', initWeb3Modal)
 })
 </script>
 
@@ -91,8 +103,9 @@ onMounted(async () => {
           : 'hs-button is-filled is-large is-fullwidth relative data-[is-loading=true]:animate-pulse'
       } ${error ? 'is-error' : ''}`"
       v-bind:class="props.class"
+      :data-is-loading="!modal"
       :disabled="props.isDisabled"
-      @click="modal.open()"
+      @click="modal?.value.open()"
     >
       {{
         truncatedAddress
