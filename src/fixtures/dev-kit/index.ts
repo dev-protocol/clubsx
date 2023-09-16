7c52da4c314397a561d267e219b4d7b2a49184ab
aggre <hiroyuki.aggre@gmail.com>
2023-09-16 14:11:27 +0900

    Display warning when the user's token balance is insufficient (#1274)
    * reorganize local variables
    
    * add checking the user balance / fix `accessControl` bug

--- src/fixtures/dev-kit/index.ts
+++ src/fixtures/dev-kit/index.ts
@@ -19,7 +19,11 @@ import {
   whenDefined,
   whenDefinedAll,
 } from '@devprotocol/util-ts'
-import { type STokensContract, positionsCreate } from '@devprotocol/dev-kit'
+import {
+  type STokensContract,
+  positionsCreate,
+  addresses,
+} from '@devprotocol/dev-kit'
 import { CurrencyOption } from '@constants/currencyOption'
 import type { Log as Log_ } from '@ethersproject/abstract-provider'
 import { bytes32Hex } from '@fixtures/data/hexlify'
@@ -238,6 +242,26 @@ export const calculateRewardAmount = async (
   return (l1 || l2)?.calculateRewardAmount(propertyAddress)
 }
 
+export const getTokenAddress = (currency: CurrencyOption, chain: number) => {
+  return currency === CurrencyOption.USDC && chain === 137
+    ? '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
+    : currency === CurrencyOption.USDC && chain === 80001
+    ? '0xFEca406dA9727A25E71e732F9961F680059eF1F9'
+    : currency === CurrencyOption.ETH && chain === 137
+    ? '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619'
+    : currency === CurrencyOption.ETH && chain === 80001
+    ? '0x3c8d6A6420C922c88577352983aFFdf7b0F977cA'
+    : currency === CurrencyOption.MATIC && chain === 137
+    ? '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270'
+    : currency === CurrencyOption.MATIC && chain === 80001
+    ? '0x9c3c9283d3e44854697cd22d3faa240cfb032889'
+    : currency === CurrencyOption.DEV && chain === 137
+    ? addresses.polygon.mainnet.token
+    : currency === CurrencyOption.DEV && chain === 80001
+    ? addresses.polygon.mumbai.token
+    : (undefined as never)
+}
+
 export const stakeWithAnyTokens = async (
   {
     provider,
@@ -270,7 +294,7 @@ export const stakeWithAnyTokens = async (
   > =
     currency === CurrencyOption.USDC && chain === 137
       ? [
-          '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', // USDC
+          getTokenAddress(currency, chain), // USDC
           500n,
           '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', // WETH
           10000n,
@@ -278,7 +302,7 @@ export const stakeWithAnyTokens = async (
         ]
       : currency === CurrencyOption.USDC && chain === 80001
       ? [
-          '0xFEca406dA9727A25E71e732F9961F680059eF1F9', // USDC
+          getTokenAddress(currency, chain), // USDC
           10000n,
           '0x3c8d6A6420C922c88577352983aFFdf7b0F977cA', // devWETH
           10000n,
@@ -286,19 +310,19 @@ export const stakeWithAnyTokens = async (
         ]
       : currency === CurrencyOption.ETH && chain === 137
       ? [
-          '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', // WETH
+          getTokenAddress(currency, chain), // WETH
           10000n,
           '0xA5577D1cec2583058A6Bd6d5DEAC44797c205701', // DEV
         ]
       : currency === CurrencyOption.ETH && chain === 80001
       ? [
-          '0x3c8d6A6420C922c88577352983aFFdf7b0F977cA', // devWETH
+          getTokenAddress(currency, chain), // devWETH
           10000n,
           '0xcbc698ed514dF6e54932a22515d6D0C27E4DA091', // DEV
         ]
       : currency === CurrencyOption.MATIC && chain === 137
       ? [
-          '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', // WMATIC
+          getTokenAddress(currency, chain), // WMATIC
           3000n,
           '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', // WETH
           10000n,
@@ -306,7 +330,7 @@ export const stakeWithAnyTokens = async (
         ]
       : currency === CurrencyOption.MATIC && chain === 80001
       ? [
-          '0x9c3c9283d3e44854697cd22d3faa240cfb032889', // WMATIC
+          getTokenAddress(currency, chain), // WMATIC
           3000n,
           '0xFEca406dA9727A25E71e732F9961F680059eF1F9', // USDC
           10000n,
