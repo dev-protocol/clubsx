1e2ee24586becb85658f662faa1dc8ef0c084d10
Yash Agrawal <yash@devprotocol.xyz>
2023-09-26 16:59:24 +0530

    feat: setup components for desining funds page

---
+++ src/plugins/admin/Funds.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import type { connection as Connection } from '@devprotocol/clubs-core/connection'
+  import type { Signer } from 'ethers'
+  import { onMount } from 'svelte'
+  import truncateEthAddress from 'truncate-eth-address'
+
+  let signer: Signer | undefined
+  let connection: typeof Connection
+  let currentAddress: string | undefined
+
+  onMount(async () => {
+    connectOnMount()
+  })
+
+  const connectOnMount = async () => {
+    const _connection = await import('@devprotocol/clubs-core/connection')
+    connection = _connection.connection
+    connection().signer.subscribe((s) => {
+      signer = s
+    })
+    connection().account.subscribe((a) => {
+      currentAddress = a
+    })
+  }
+</script>
+
+<div class="grid gap-16">
+  <button
+    disabled={!!currentAddress}
+    on:click={connectOnMount}
+    class={`flex gap-[18px] rounded-md w-fit max-w-full px-8 py-4 bg-[#${currentAddress ? '3A4158' : 'FDAD00'}] justify-center items-center`}
+  >
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M15.9825 16.725C15.2838 15.7999 14.3798 15.0496 13.3417 14.5334C12.3036 14.0171 11.1599 13.7489 10.0005 13.75C8.84115 13.7489 7.69739 14.0171 6.65932 14.5334C5.62125 15.0496 4.71724 15.7999 4.0185 16.725M15.9825 16.725C17.346 15.5122 18.3076 13.9136 18.7417 12.1411C19.1758 10.3686 19.0608 8.50609 18.412 6.80048C17.7632 5.09487 16.6112 3.62678 15.1089 2.5909C13.6066 1.55502 11.8248 1.00029 10 1.00029C8.17516 1.00029 6.39343 1.55502 4.89111 2.5909C3.38878 3.62678 2.23683 5.09487 1.58804 6.80048C0.939242 8.50609 0.824253 10.3686 1.25832 12.1411C1.69239 13.9136 2.655 15.5122 4.0185 16.725M15.9825 16.725C14.3365 18.1932 12.2061 19.0031 10.0005 19C7.79453 19.0034 5.66474 18.1934 4.0185 16.725M13.0005 7.75C13.0005 8.54565 12.6844 9.30871 12.1218 9.87132C11.5592 10.4339 10.7962 10.75 10.0005 10.75C9.20485 10.75 8.44179 10.4339 7.87918 9.87132C7.31657 9.30871 7.0005 8.54565 7.0005 7.75C7.0005 6.95435 7.31657 6.19129 7.87918 5.62868C8.44179 5.06607 9.20485 4.75 10.0005 4.75C10.7962 4.75 11.5592 5.06607 12.1218 5.62868C12.6844 6.19129 13.0005 6.95435 13.0005 7.75Z" stroke={currentAddress ? 'white' : 'black'} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    <p class={`text-${currentAddress ? 'white' : 'black'}`}>
+      { #if currentAddress }
+        Logged in as { truncateEthAddress(currentAddress) }
+      { :else }
+        Login required to view data
+      {/if}
+    </button>
+</div>
--- src/plugins/admin/index.ts
+++ src/plugins/admin/index.ts
@@ -8,6 +8,7 @@ import {
 import { default as Index } from './index.astro'
 import { default as Plugins } from './plugins.astro'
 import { default as Overview } from './overview.astro'
+import { default as Funds } from './funds.astro'
 
 export const getPagePaths: ClubsFunctionGetPagePaths = async () => []
 
@@ -30,6 +31,11 @@ export const getAdminPaths: ClubsFunctionGetAdminPaths = async (_, config) => [
   {
     paths: [''],
     component: Overview,
+    props: { propertyAddress: config.propertyAddress, rpcUrl: config.rpcUrl },
+  },
+  {
+    paths: ['funds'],
+    component: Funds,
     props: { propertyAddress: config.propertyAddress, rpcUrl: config.rpcUrl },
   },
 ]
