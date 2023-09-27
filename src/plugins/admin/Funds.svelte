f04eb41eae274eb8f2f6c15e8bb2a92bc7800cd2
Yash Agrawal <yash@devprotocol.xyz>
2023-09-27 14:44:22 +0530

    feat: add withdrawal page banner

--- src/plugins/admin/Funds.svelte
+++ src/plugins/admin/Funds.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-  import type { connection as Connection } from '@devprotocol/clubs-core/connection'
-  import type { Signer } from 'ethers'
   import { onMount } from 'svelte'
+  import type { Signer } from 'ethers'
   import truncateEthAddress from 'truncate-eth-address'
+  import NotVerifiedBannerImg from './assets/NotVerifiedBannerImg.svg'
+  import type { connection as Connection } from '@devprotocol/clubs-core/connection'
 
   let signer: Signer | undefined
   let connection: typeof Connection
@@ -25,19 +26,29 @@
 </script>
 
 <div class="grid gap-16">
-  <button
-    disabled={!!currentAddress}
-    on:click={connectOnMount}
-    class={`flex gap-[18px] rounded-md w-fit max-w-full px-8 py-4 bg-[#${currentAddress ? '3A4158' : 'FDAD00'}] justify-center items-center`}
-  >
-    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M15.9825 16.725C15.2838 15.7999 14.3798 15.0496 13.3417 14.5334C12.3036 14.0171 11.1599 13.7489 10.0005 13.75C8.84115 13.7489 7.69739 14.0171 6.65932 14.5334C5.62125 15.0496 4.71724 15.7999 4.0185 16.725M15.9825 16.725C17.346 15.5122 18.3076 13.9136 18.7417 12.1411C19.1758 10.3686 19.0608 8.50609 18.412 6.80048C17.7632 5.09487 16.6112 3.62678 15.1089 2.5909C13.6066 1.55502 11.8248 1.00029 10 1.00029C8.17516 1.00029 6.39343 1.55502 4.89111 2.5909C3.38878 3.62678 2.23683 5.09487 1.58804 6.80048C0.939242 8.50609 0.824253 10.3686 1.25832 12.1411C1.69239 13.9136 2.655 15.5122 4.0185 16.725M15.9825 16.725C14.3365 18.1932 12.2061 19.0031 10.0005 19C7.79453 19.0034 5.66474 18.1934 4.0185 16.725M13.0005 7.75C13.0005 8.54565 12.6844 9.30871 12.1218 9.87132C11.5592 10.4339 10.7962 10.75 10.0005 10.75C9.20485 10.75 8.44179 10.4339 7.87918 9.87132C7.31657 9.30871 7.0005 8.54565 7.0005 7.75C7.0005 6.95435 7.31657 6.19129 7.87918 5.62868C8.44179 5.06607 9.20485 4.75 10.0005 4.75C10.7962 4.75 11.5592 5.06607 12.1218 5.62868C12.6844 6.19129 13.0005 6.95435 13.0005 7.75Z" stroke={currentAddress ? 'white' : 'black'} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-    </svg>
-    <p class={`text-${currentAddress ? 'white' : 'black'}`}>
-      { #if currentAddress }
-        Logged in as { truncateEthAddress(currentAddress) }
-      { :else }
-        Login required to view data
-      {/if}
-    </button>
+  <!-- Wallet connection notice -->
+  { #if !currentAddress }
+    <div
+      class="flex gap-[18px] mb-16 rounded-md w-fit max-w-full px-8 py-4 bg-[#FDAD00] justify-center items-center"
+    >
+      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M15.9825 16.725C15.2838 15.7999 14.3798 15.0496 13.3417 14.5334C12.3036 14.0171 11.1599 13.7489 10.0005 13.75C8.84115 13.7489 7.69739 14.0171 6.65932 14.5334C5.62125 15.0496 4.71724 15.7999 4.0185 16.725M15.9825 16.725C17.346 15.5122 18.3076 13.9136 18.7417 12.1411C19.1758 10.3686 19.0608 8.50609 18.412 6.80048C17.7632 5.09487 16.6112 3.62678 15.1089 2.5909C13.6066 1.55502 11.8248 1.00029 10 1.00029C8.17516 1.00029 6.39343 1.55502 4.89111 2.5909C3.38878 3.62678 2.23683 5.09487 1.58804 6.80048C0.939242 8.50609 0.824253 10.3686 1.25832 12.1411C1.69239 13.9136 2.655 15.5122 4.0185 16.725M15.9825 16.725C14.3365 18.1932 12.2061 19.0031 10.0005 19C7.79453 19.0034 5.66474 18.1934 4.0185 16.725M13.0005 7.75C13.0005 8.54565 12.6844 9.30871 12.1218 9.87132C11.5592 10.4339 10.7962 10.75 10.0005 10.75C9.20485 10.75 8.44179 10.4339 7.87918 9.87132C7.31657 9.30871 7.0005 8.54565 7.0005 7.75C7.0005 6.95435 7.31657 6.19129 7.87918 5.62868C8.44179 5.06607 9.20485 4.75 10.0005 4.75C10.7962 4.75 11.5592 5.06607 12.1218 5.62868C12.6844 6.19129 13.0005 6.95435 13.0005 7.75Z" stroke={currentAddress ? 'white' : 'black'} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      <p class="text-black">
+          Not connected to a wallet
+      </p>
+    </div>
+  { /if }
+
+  <!-- Banner -->
+  <div class="w-full max-w-full rounded-[18px] border-[1px] gap-[18px] py-[82px] px-8 bg-[#3A4158] border-[#3A4158]">
+    <p class="font-body text-[32px] text-center">Clubs KYC will be available soon...</p>
+    <div class="mt-[18px] w-fit max-w-full flex items-center justify-center gap-[18px] mx-auto">
+      <div class="w-fit max-w-[52.2%] rounded-lg p-[19px] bg-[#6D9BFF]">
+        <img src={NotVerifiedBannerImg.src} alt="Not verified" />
+      </div>
+      <p class="font-body font-bold text-base opacity-50">Not verified</p>
+    </div>
+    <p class="mt-[18px] font-body text-xl text-center">Clubs applies eKYC to fund withdrawals to help you build a secure and robust community. Once the process is successfully completed, you will be able to withdraw funds and the Clubs team is committed to building the eKYC process.</p>
+  </div>
 </div>
