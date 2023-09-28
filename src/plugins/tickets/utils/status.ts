6fc806f51147dfdb8c2cf2bddb91dfac9778e78b
aggre <hiroyuki.aggre@gmail.com>
2023-09-29 02:45:03 +0900

    Revert "add `strict` mode"
    This reverts commit 6c0a9b67e7d08674ca55afbc509fa522e5519d0c.

--- src/plugins/tickets/utils/status.ts
+++ src/plugins/tickets/utils/status.ts
@@ -68,24 +68,6 @@ export const factory =
                 direction: 'future',
               }),
             ),
-            strictPast: whenDefined(use.availability, (availability) =>
-              exploreSlots({
-                availability,
-                base,
-                find: 'start',
-                direction: 'past',
-                strict: true,
-              }),
-            ),
-            strictFuture: whenDefined(use.availability, (availability) =>
-              exploreSlots({
-                availability,
-                base,
-                find: 'start',
-                direction: 'future',
-                strict: true,
-              }),
-            ),
           },
         },
         end: {
@@ -104,24 +86,6 @@ export const factory =
                 base,
                 find: 'end',
                 direction: 'future',
-              }),
-            ),
-            strictPast: whenDefined(use.availability, (availability) =>
-              exploreSlots({
-                availability,
-                base,
-                find: 'end',
-                direction: 'past',
-                strict: true,
-              }),
-            ),
-            strictFuture: whenDefined(use.availability, (availability) =>
-              exploreSlots({
-                availability,
-                base,
-                find: 'end',
-                direction: 'future',
-                strict: true,
               }),
             ),
           },
@@ -176,10 +140,7 @@ export const factory =
       ? false
       : history
       ? whenDefinedAll(
-          [
-            slots.find.start.direction.strictPast,
-            slots.find.end.direction.strictFuture,
-          ],
+          [slots.find.start.direction.past, slots.find.end.direction.future],
           ([start, end]) =>
             base.isBetween(start, end) &&
             start.isBetween(base0, base24) &&
