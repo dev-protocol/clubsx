db841537ab2d30d36b4181cccc94ffd4428b2214
aggre <hiroyuki.aggre@gmail.com>
2023-10-05 00:00:10 +0900

    fix replacement

--- src/fixtures/url/index.ts
+++ src/fixtures/url/index.ts
@@ -4,8 +4,11 @@ export const replaceWithFwdHost = (base: Request) => {
   const url = new URL(base.url)
   const forwarded = base.headers.get('forwarded') // i.g., for=12.345.67.890;host=xxx.clubs.place;proto=https;sig=0QmV…iOTE=;exp=1696430045
   const list = whenDefined(forwarded, (fwd) => fwd.split(';'))
+  const reg = /^host=(.*)/i
   const host = whenDefined(list, (li) =>
-    li.find((val) => /^host=.*/i.test(val)),
-  )
-  return url.href.replace(url.host, host ?? url.host)
+    li.find((val) => reg.test(val)),
+  )?.replace(reg, '$1')
+  return url.href
+    .replace(url.host, host ?? url.host)
+    .replace(/\/sites_\/[a-z|0-9|-]+/i, '')
 }
