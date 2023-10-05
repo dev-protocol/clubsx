0b647cacb67e11fd3f18e74c2ea8c83c03cd2602
aggre <hiroyuki.aggre@gmail.com>
2023-10-05 13:01:33 +0900

    add logger for debugging

--- src/fixtures/url/index.ts
+++ src/fixtures/url/index.ts
@@ -8,6 +8,7 @@ export const replaceWithFwdHost = (base: Request) => {
   const host = whenDefined(list, (li) =>
     li.find((val) => reg.test(val)),
   )?.replace(reg, '$1')
+  console.log({ forwarded, 'base.url': base.url })
   return url.href
     .replace(url.host, host ?? url.host)
     .replace(/\/sites_\/[a-z|0-9|-]+/i, '')
