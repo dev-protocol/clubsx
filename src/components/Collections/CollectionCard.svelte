7aff327666edec299704ed1d9ee91eed473091b6
aggre <hiroyuki.aggre@gmail.com>
2023-10-16 13:58:16 +0900

    tweaks UI: Collection admin index, edit

--- src/components/Collections/CollectionCard.svelte
+++ src/components/Collections/CollectionCard.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import type { Collection} from '@plugins/collections'
+  import type { Collection } from '@plugins/collections'
   import { emptyDummyImage } from '@plugins/collections/fixtures'
   export let collections: Collection[] = []
   export let base: string = '/admin/collections'
@@ -7,49 +7,55 @@
 
 {#if collections.length > 0}
   {#each collections as collection}
-  <a href={`${base}/${collection.id}`}>
-    <div
-    class="flex max-w-4xl flex-col items-start gap-4 mb-16 rounded-2xl rounded-2xl bg-white"
-  >
-    <div class="flex items-start gap-2.5">
-      <img
-        class="md:w-[896px] rounded-t-2xl"
-        src={collection.imageSrc?.trim().length > 0 ? collection.imageSrc : emptyDummyImage(2400, 1200)}
-        alt={`${collection.name}-image`}
-      />
-    </div>
-    <div class="flex flex-col items-start gap-4 p-4">
-      <p class="text-center text-3xl font-normal text-black">
-        {collection.name}
-      </p>
+    <a href={`${base}/${collection.id}`}>
       <div
-        class={`flex flex-col items-start gap-2.5 rounded px-2.5  
-        ${
-          collection.isTimeLimitedCollection ? 'bg-[#6D9BFF]' : 'bg-[#FFC751]'
-        }`}
+        class="mb-16 grid max-w-4xl items-start gap-4 rounded-2xl rounded-2xl bg-white"
       >
-        <p class="text-center text-lg font-medium text-black">
-          {collection.isTimeLimitedCollection
-            ? 'Time Limited'
-            : 'Limited Amount'}
-        </p>
+        <div class="flex items-start gap-2.5">
+          <img
+            class="w-full max-w-4xl rounded-t-2xl object-cover"
+            src={collection.imageSrc?.trim().length > 0
+              ? collection.imageSrc
+              : emptyDummyImage(2400, 1200)}
+            alt={`${collection.name}-image`}
+          />
+        </div>
+        <div class="flex flex-col items-start gap-4 p-4">
+          <p class="text-center text-3xl font-normal text-black">
+            {collection.name}
+          </p>
+          <div
+            class={`flex flex-col items-start gap-2.5 rounded px-2.5
+        ${
+          collection.isTimeLimitedCollection
+            ? 'bg-dp-yellow-200'
+            : 'bg-native-blue-300'
+        }`}
+          >
+            <p class="text-center text-sm font-bold text-black">
+              {collection.isTimeLimitedCollection
+                ? 'Time Limited'
+                : 'Limited Amount'}
+            </p>
+          </div>
+          <p class="text-ellipsis text-base font-normal text-black">
+            {collection.description}
+          </p>
+          <div class="flex items-start gap-2.5 overflow-hidden">
+            {#if collection.memberships.length > 0}
+              {#each collection.memberships as membership, i}
+                <img
+                  class="h-16 w-16 rounded-md"
+                  src={membership.imageSrc
+                    ? membership.imageSrc
+                    : emptyDummyImage(400, 400)}
+                  alt={`membership-image ${i}`}
+                />
+              {/each}
+            {/if}
+          </div>
+        </div>
       </div>
-      <p class="text-ellipsis text-base font-normal text-black">
-        {collection.description}
-      </p>
-      <div class="flex items-start gap-2.5 overflow-hidden">
-        {#if collection.memberships.length > 0}
-          {#each collection.memberships as membership, i}
-            <img
-              class="h-16 w-16 rounded-md"
-              src={membership.imageSrc ? membership.imageSrc : emptyDummyImage(400, 400)}
-              alt={`membership-image ${i}`}
-            />
-          {/each}
-        {/if}
-      </div>
-    </div>
-  </div>
-  </a>
+    </a>
   {/each}
 {/if}
