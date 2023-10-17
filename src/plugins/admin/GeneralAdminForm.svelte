a21270b6382c7531204cc770ab62594d51453264
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-10-17 10:47:39 +0900

    removes danger zone

--- src/plugins/admin/GeneralAdminForm.svelte
+++ src/plugins/admin/GeneralAdminForm.svelte
@@ -5,16 +5,7 @@
 
   let { name, description, twitterHandle, propertyAddress, adminRolePoints } =
     config
-  let network = 'polygon'
-  let dangerLocked = true
   const MAX_OF_ADMIN_ROLE_HOLDER_POINTS = 95
-  const networkOptions = [
-    { name: 'Mainnet', value: 'mainnet' },
-    { name: 'Polygon', value: 'polygon' },
-    { name: 'Arbitrum', value: 'arbitrum' },
-    { name: 'Polygon Mumbai', value: 'polygon-mumbai' },
-    { name: 'Arbitrum Rinkeby', value: 'arbitrum-rinkeby' },
-  ]
 
   const updateConfig = () => {
     adminRolePoints =
@@ -86,56 +77,5 @@
         name="twitter"
       />
     </label>
-
-    <!-- Danger Zone-->
-    <div class="rounded-lg border-2 border-error-400 px-12 py-12">
-      <div class="mb-10 flex items-center justify-between">
-        <span class="font-title text-lg font-bold">Danger Zone</span>
-        <button
-          on:click|preventDefault={(_) => (dangerLocked = !dangerLocked)}
-          class="hs-button is-outlined is-contained"
-        >
-          {#if dangerLocked}
-            <span class="hs-button__label">Unlock</span>
-          {:else}
-            <span class="hs-button__label">Lock</span>
-          {/if}
-        </button>
-      </div>
-
-      <div class="mb-10 flex flex-col">
-        <label class="hs-select-field is-required" for="network">
-          <span class="hs-select-field__label">Network</span>
-          <select
-            class={`hs-select-field__input`}
-            disabled={dangerLocked}
-            bind:value={network}
-            on:change={updateConfig}
-            id="network"
-            name="network"
-          >
-            {#each networkOptions as option}
-              <option value={option.value}>
-                {option.name}
-              </option>
-            {/each}
-          </select>
-        </label>
-      </div>
-
-      <div class="flex flex-col">
-        <label class="hs-form-field is-required" for="tokenAddress">
-          <span class="hs-form-field__label">Token Address</span>
-          <input
-            disabled={dangerLocked}
-            class={`hs-form-field__input`}
-            bind:value={propertyAddress}
-            on:change={updateConfig}
-            id="tokenAddress"
-            name="tokenAddress"
-          />
-        </label>
-      </div>
-    </div>
   </div>
 </div>
