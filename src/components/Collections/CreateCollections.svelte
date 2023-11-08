b5e6ef22af9bef47b0449c1ce9320ca76a5c012d
Yash Agrawal <117558264+yashdesu@users.noreply.github.com>
2023-11-08 11:09:45 +0530

    fix: memberships depreciation issue solved (#1457)
    * fix: memberships depreciation issue solved
    
    * fix: memberhsip filtering and setting
    
    * fix: membership deprecation logic
    
    * fix: minor fix
    
    * fix: minor fixes
    
    * fix: add comments to a piece of code
    
    ---------
    
    Co-authored-by: aggre <hiroyuki.aggre@gmail.com>

--- src/components/Collections/CreateCollections.svelte
+++ src/components/Collections/CreateCollections.svelte
@@ -162,11 +162,13 @@
 
     const membership = collection.memberships.find(
       (m: CollectionMembership) =>
-        m.id === selectedMembership.id &&
-        m.name === selectedMembership.name &&
         JSON.stringify(m.payload) ===
           JSON.stringify(selectedMembership.payload),
     )
+
+    if (!membership) {
+      return;
+    }
 
     setOptions(
       [
@@ -177,9 +179,9 @@
               ...collection,
               memberships: [
                 ...collection.memberships.filter(
-                  (m: CollectionMembership) => m.id !== selectedMembership.id,
+                  (m: CollectionMembership) => m.id !== membership.id,
                 ),
-                { ...membership, deprecated: true },
+                { ...selectedMembership, deprecated: true },
               ],
             },
           ],
@@ -199,11 +201,13 @@
 
     const membership = selectedCollection.memberships.find(
       (m: CollectionMembership) =>
-        m.id === selectedMembership.id &&
-        m.name === selectedMembership.name &&
         JSON.stringify(m.payload) ===
           JSON.stringify(selectedMembership.payload),
     )
+
+    if (!membership) {
+      return;
+    }
 
     setOptions(
       [
@@ -217,9 +221,9 @@
               ...selectedCollection,
               memberships: [
                 ...selectedCollection.memberships.filter(
-                  (m: CollectionMembership) => m.id !== selectedMembership.id,
+                  (m: CollectionMembership) => m.id !== membership.id,
                 ),
-                { ...membership, deprecated: false },
+                { ...selectedMembership, deprecated: false },
               ],
             },
           ],
