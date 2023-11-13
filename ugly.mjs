e8827e19a9589886bdc5c336edf415572851fdd8
aggre <hiroyuki.aggre@gmail.com>
2023-11-13 13:12:40 +0900

    Revert "Revert "Revert "use forwarded header"""
    This reverts commit 8727845b89f2132dff025e18fc3d95d32cb2304b.

---
+++ ugly.mjs
@@ -0,0 +1,15 @@
+import fs from 'fs-extra'
+
+const pathRequestTransform =
+  './node_modules/@astrojs/vercel/dist/serverless/request-transform.js'
+
+// Rewrite the requested URL
+// Note: Requests rewritten by middleware (./middleware.ts) have `x-rewritten-url` header
+fs.outputFileSync(
+  pathRequestTransform,
+  ((file) =>
+    file.replace(
+      `base + req.url,`,
+      `(()=>req.headers['x-rewritten-url'] ? req.headers['x-rewritten-url'] : base + req.url)(),`,
+    ))(fs.readFileSync(pathRequestTransform, 'utf8')),
+)
