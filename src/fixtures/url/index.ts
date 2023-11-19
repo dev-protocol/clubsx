6eaa1f188361be3c5ff1c73c82789a1b9ae83b28
aggre <hiroyuki.aggre@gmail.com>
2023-11-19 23:47:08 +0900

    update

--- src/fixtures/url/index.ts
+++ src/fixtures/url/index.ts
@@ -4,16 +4,19 @@ export const replaceWithFwdHost = (base: Request) => {
   const url = new URL(base.url)
   const forwarded = base.headers.get('forwarded') // i.g., for=12.345.67.890;host=xxx.clubs.place;proto=https;sig=0QmV…iOTE=;exp=1696430045
   const list = whenDefined(forwarded, (fwd) => fwd.split(';'))
-  const reg = /^host=(.*)/i
-  const host = whenDefined(list, (li) =>
-    li.find((val) => reg.test(val)),
-  )?.replace(reg, '$1')
-  console.log({
-    headers: Object.fromEntries(base.headers),
-    host,
-    'base.url': base.url,
-  })
+  const regHeader = /^host=(.*)/i
+  const regParam = /\/sites_\/([a-z|0-9|-]+)\/?/i
+  const hostHeader = whenDefined(list, (li) =>
+    li.find((val) => regHeader.test(val)),
+  )?.replace(regHeader, '$1')
+  const hostParam = regParam.test(url.href)
+    ? `${url.href.match(regParam)?.[1]}.${url.host}`
+    : undefined
+  console.log({ forwarded, host: hostHeader, 'base.url': base.url })
   return url.href
-    .replace(url.host, host ?? url.host)
-    .replace(/\/sites_\/[a-z|0-9|-]+/i, '')
+    .replace(
+      url.host,
+      url.host === hostHeader ? hostParam ?? url.host : hostHeader ?? url.host,
+    )
+    .replace(regParam, '/')
 }
