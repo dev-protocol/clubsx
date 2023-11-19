4fe383d0e42f4e8c4d2c32c0b4431972f172b0a5
aggre <hiroyuki.aggre@gmail.com>
2023-11-19 17:27:34 +0900

    fix tax calculation method

--- scripts/populate/constants/tanadao.mjs
+++ scripts/populate/constants/tanadao.mjs
@@ -1,4 +1,5 @@
-const taxRate = 1.1
+const taxRate = 0.1
+const taxIncluded = (price) => price + price * taxRate
 
 export const override = [
   {
@@ -9,7 +10,7 @@ export const override = [
     payload:
       '0x007408bcd687ed9e07f48c0656773384c996f8e4ada7576a3c81d3d147166bb2',
     price: {
-      yen: 3_000 * taxRate,
+      yen: taxIncluded(3_000),
     },
   },
   {
@@ -20,7 +21,7 @@ export const override = [
     payload:
       '0xa0b3487e4bf288af6f386ce9dc2bf0ac7adbd6114d415b34a045c1206b57d267',
     price: {
-      yen: 10_000 * taxRate,
+      yen: taxIncluded(10_000),
     },
   },
   {
@@ -31,7 +32,7 @@ export const override = [
     payload:
       '0x779bccd239a972ce7837227e7c087c4ef6021f16fd0bbc909f328024a64fcd50',
     price: {
-      yen: 10_000 * taxRate,
+      yen: taxIncluded(10_000),
     },
   },
   {
@@ -42,7 +43,7 @@ export const override = [
     payload:
       '0x53fc50b3d0edf574480c9cd48e1c8493e04593e3a2e9c66f773562dfd8371967',
     price: {
-      yen: 35_000 * taxRate,
+      yen: taxIncluded(35_000),
     },
   },
   {
@@ -53,7 +54,7 @@ export const override = [
     payload:
       '0xb8e9750d7406859c235c5c5a511fd24577ead49a123e0d62a6dc47f9e9489014',
     price: {
-      yen: 10_000 * taxRate,
+      yen: taxIncluded(10_000),
     },
   },
   {
@@ -64,7 +65,7 @@ export const override = [
     payload:
       '0x549145c48b05874e36ee3e4aeb7861cc214d23865c43f0c71e4efa909ee57208',
     price: {
-      yen: 35_000 * taxRate,
+      yen: taxIncluded(35_000),
     },
   },
 ]
