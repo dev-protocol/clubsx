fba916f2b96fd7617bcc6ece872e0e4e9b4741e6
aggre <hiroyuki.aggre@gmail.com>
2023-11-28 15:04:47 +0900

    Use the result of `verify` before sending send.devprotocol.xyz request (#1637)
    * use the result of `verify` before sending send.devprotocol.xyz request
    
    * remove Greetings

--- src/plugins/clubs-payments/api/fulfillment.ts
+++ src/plugins/clubs-payments/api/fulfillment.ts
@@ -168,9 +168,9 @@ export const post: ({
       ? verification$1.order_id
       : 'clubs-payments'
 
-    const result$1 = await whenNotError(
-      params,
-      ([to, property, payload, token, input, gatewayAddress, fee]) =>
+    const result$1 = await whenNotErrorAll(
+      [params, verify],
+      ([[to, property, payload, token, input, gatewayAddress, fee]]) =>
         fetch(
           'https://send.devprotocol.xyz/api/send-transactions/SwapTokensAndStakeDev',
           {
