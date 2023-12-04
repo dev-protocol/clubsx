24e406ed8734396a6741eb32bbf22c5894ef07af
aggre <hiroyuki.aggre@gmail.com>
2023-12-04 15:25:14 +0900

    tweaking

--- scripts/send/index.mjs
+++ scripts/send/index.mjs
@@ -1,22 +1,32 @@
 import fetch from 'cross-fetch'
+import PQueue from 'p-queue'
 import dotenv from 'dotenv'
 import options from './options.mjs'
 
 dotenv.config()
 
+const queue = new PQueue({ concurrency: 1, interval: 10 * 1000 })
+
 const run = async () => {
-  const result = await Promise.all(
-    options.map(({ requestId, rpcUrl, chainId, args }) =>
-      fetch(
-        'https://send.devprotocol.xyz/api/send-transactions/SwapTokensAndStakeDev',
-        {
-          method: 'POST',
-          headers: {
-            Authorization: `Bearer ${process.env.SEND_DEVPROTOCOL_API_KEY}`,
-          },
-          body: JSON.stringify({ requestId, rpcUrl, chainId, args }),
-        },
-      ).catch((err) => new Error(err)),
+  const result = await queue.addAll(
+    options.map(
+      ({ requestId, rpcUrl, chainId, args }) =>
+        () =>
+          fetch(
+            'https://send.devprotocol.xyz/api/send-transactions/SwapTokensAndStakeDev',
+            {
+              method: 'POST',
+              headers: {
+                Authorization: `Bearer ${process.env.SEND_DEVPROTOCOL_API_KEY}`,
+              },
+              body: JSON.stringify({ requestId, rpcUrl, chainId, args }),
+            },
+          )
+            .then((res) => {
+              console.log({ res, args })
+              return res
+            })
+            .catch((err) => new Error(err)),
     ),
   )
 
--- scripts/send/options.mjs
+++ scripts/send/options.mjs
@@ -1,4 +1,4 @@
-import { ZeroAddress, randomBytes, uuidV4 } from 'ethers'
+import { ZeroAddress, parseUnits, randomBytes, uuidV4 } from 'ethers'
 
 /**
  * Define the base options. Please rewrite the common parameters as necessary.
@@ -7,18 +7,26 @@ const base = {
   rpcUrl: 'https://polygon-bor.publicnode.com',
   chainId: 137,
   args: {
-    to: ZeroAddress,
-    property: ZeroAddress,
+    to: '',
+    property: '',
     payload: '0x0',
-    gatewayAddress: ZeroAddress,
+    gatewayAddress: '',
     amounts: {
       token: ZeroAddress,
-      input: 0n,
-      fee: 0n,
+      input: parseUnits('1', 18).toString(),
+      fee: parseUnits('0.9', 18).toString(),
     },
   },
 }
 
+/**
+ * Define the destination list.
+ */
+const dest = [ZeroAddress, ZeroAddress, ZeroAddress]
+
+/**
+ * Utility functions.
+ */
 const createId = () => {
   const id = uuidV4(randomBytes(8))
   return (key) => `${id}-${key}`
@@ -28,8 +36,11 @@ const id = createId()
 /**
  * Export all the options.
  */
-export default [
-  { ...base, to: ZeroAddress, requestId: id(1) },
-  { ...base, to: ZeroAddress, requestId: id(2) },
-  { ...base, to: ZeroAddress, requestId: id(3) },
-]
+export default dest.map((to, i) => ({
+  ...base,
+  args: {
+    ...base.args,
+    to,
+  },
+  requestId: id(i),
+}))
