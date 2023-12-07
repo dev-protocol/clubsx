b195b900db826d1f39e9f13c554bfdf3760cc5e9
aggre <hiroyuki.aggre@gmail.com>
2023-12-07 17:29:38 +0900

    use lowercase HTTP methods

--- src/pages/sites_/[site]/api/[...path].ts
+++ src/pages/sites_/[site]/api/[...path].ts
@@ -3,7 +3,7 @@ import { config as _config } from '@fixtures/config'
 import { plugins } from '@constants/plugins'
 import type { APIRoute } from 'astro'
 
-export const all: APIRoute = async (event) => {
+export const ALL: APIRoute = async (event) => {
   const { params } = event
 
   const config = () => _config(params.site)
