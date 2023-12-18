2aa0bad321c0824ddbae87750a297750ea9bcc04
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-12-18 11:44:21 +0900

    domain localization

---
+++ src/components/Domain/i18n/index.ts
@@ -0,0 +1,24 @@
+import type { ClubsI18nParts } from '@devprotocol/clubs-core'
+
+export const Strings = {
+  Header: {
+    en: () => 'It All Starts with a Domain',
+    ja: () => '',
+  },
+  SubHeader: {
+    en: () => 'You can use your preferred domain for your club.',
+    ja: () => '',
+  },
+  DomainAvailable: {
+    en: () => 'Domain available',
+    ja: () => '',
+  },
+  DomainUnavailable: {
+    en: () => 'Domain unavailable',
+    ja: () => '',
+  },
+  Continue: {
+    en: () => 'Continue',
+    ja: () => '',
+  },
+} satisfies ClubsI18nParts
