81133ef09d7dfaaf4cd38421284dea110f111714
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-12-28 15:53:05 +0900

    removes feature flag requirements for shops and kyc

--- src/fixtures/flags/index.ts
+++ src/fixtures/flags/index.ts
@@ -1,16 +1,10 @@
 import { whenDefined, type UndefinedOr } from '@devprotocol/util-ts'
 
-export enum FeatureFlags {
-  StoresOnMarketplace = 'stores-on-marketplace',
-  Kyc = 'kyc',
-}
+export enum FeatureFlags {}
 export enum FeatureFlagValues {
   Enable = 'enable',
 }
-export const flags: Map<FeatureFlags, FeatureFlagValues[]> = new Map([
-  [FeatureFlags.StoresOnMarketplace, [FeatureFlagValues.Enable]],
-  [FeatureFlags.Kyc, [FeatureFlagValues.Enable]],
-])
+export const flags: Map<FeatureFlags, FeatureFlagValues[]> = new Map([])
 
 export const getFlagsByParams = ({
   url,
