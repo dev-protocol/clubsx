c381b3a414c85c70c241218747d3cf62a94af05b
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-01-04 15:50:25 +0900

    localization for memberships form

---
+++ src/components/AdminMembershipsForm/i18n/index.ts
@@ -0,0 +1,61 @@
+import type { ClubsI18nParts } from '@devprotocol/clubs-core'
+
+export const Strings = {
+  Creator: {
+    en: () => 'Creator',
+    ja: () => '',
+  },
+  CreatorDescription: {
+    en: () =>
+      'Start a subscription for your lessons, contents, experiences, and more.',
+    ja: () => '',
+  },
+  CreatorExampleName: {
+    en: () => 'Example: Dev Protocol',
+    ja: () => 'My Vlog',
+  },
+  Business: {
+    en: () => 'Business',
+    ja: () => '',
+  },
+  BusinessDescription: {
+    en: () => 'Provide special offers to members/non-members.',
+    ja: () => '',
+  },
+  BusinessExampleName: {
+    en: () => 'Clubs Hotel',
+    ja: () => '',
+  },
+  Public: {
+    en: () => 'Public',
+    ja: () => '',
+  },
+  PublicDescription: {
+    en: () => 'Start a co-creation project and invite members.',
+    ja: () => '',
+  },
+  PublicExampleName: {
+    en: () => 'Public Project',
+    ja: () => '',
+  },
+  Select: {
+    en: () => 'Select',
+    ja: () => '',
+  },
+  Example: {
+    en: () => 'Example',
+    ja: () => '',
+  },
+  ExistingMemberships: {
+    en: () => 'Existing Memberships',
+    ja: () => '',
+  },
+  Delete: {
+    en: () => 'Delete',
+    ja: () => '',
+  },
+  Activate: {
+    en: () => 'Activate',
+    ja: () => '',
+  },
+} satisfies ClubsI18nParts
