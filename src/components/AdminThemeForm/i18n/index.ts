5d69e79817f931d53211e7bd4b119897e7c54f03
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-01-04 14:50:02 +0900

    setup design localization

---
+++ src/components/AdminThemeForm/i18n/index.ts
@@ -0,0 +1,60 @@
+import type { ClubsI18nParts } from '@devprotocol/clubs-core'
+
+export const Strings = {
+  Preview: {
+    en: () => 'Preview',
+    ja: () => '',
+  },
+  ThemeColor: {
+    en: () => 'Theme Color',
+    ja: () => '',
+  },
+  Text: {
+    en: () => 'Text',
+    ja: () => '',
+  },
+  CoverImage: {
+    en: () => 'Cover Image',
+    ja: () => '',
+  },
+  UploadToChange: {
+    en: () => 'Upload to change',
+    ja: () => '',
+  },
+  RecommendedImageSize: {
+    en: () => 'Recommended image size is 2400 x 1200 px',
+    ja: () => '',
+  },
+  Description: {
+    en: () => 'Short description to introduce yourself',
+    ja: () => '',
+  },
+  YourIntroduction: {
+    en: () => 'Your introduction',
+    ja: () => '',
+  },
+  MarkdownSupported: {
+    en: () => 'Markdown supported',
+    ja: () => '',
+  },
+  NavigationLinks: {
+    en: () => 'Navigation Links',
+    ja: () => '',
+  },
+  SocialLinks: {
+    en: () => 'Social Links',
+    ja: () => '',
+  },
+  OGP: {
+    en: () => 'OGP (Open Graph Protocol) Image',
+    ja: () => '',
+  },
+  RecommendedImageSize2: {
+    en: () => 'Image size should be 1200 x 630 pixels',
+    ja: () => '',
+  },
+  Design: {
+    en: () => 'Design',
+    ja: () => '',
+  },
+} satisfies ClubsI18nParts
