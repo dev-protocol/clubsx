87202280c85cc6ec7000c1cc3a7d93dda7b11718
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-01-31 17:02:36 +0900

    localization for publish network component

---
+++ src/components/PublishNetworkSelection/i18n/index.ts
@@ -0,0 +1,125 @@
+import type { ClubsI18nParts } from '@devprotocol/clubs-core'
+
+export const Strings = {
+  ChooseNetwork: {
+    en: () => 'Choose Network',
+    ja: () => '',
+  },
+  NetworkDifference: {
+    en: () => 'What is the difference between networks?',
+    ja: () => '',
+  },
+  Recommended: {
+    en: () => 'Recommended',
+    ja: () => '',
+  },
+  Activate: {
+    en: () => 'Activate',
+    ja: () => '',
+  },
+  ExistingProperty: {
+    en: () =>
+      'Already have a property on Niwa? Click to enter your property address.',
+    ja: () => '',
+  },
+  NoToken: {
+    en: () => "Don't have a token for your Club yet? Click here to activate.",
+    ja: () => '',
+  },
+  ActivateToken: {
+    en: () => 'Activate your Club token.',
+    ja: () => '',
+  },
+  MissingStep: {
+    en: () => 'Complete Basic info, Design, Memberships before activation.',
+    ja: () => '',
+  },
+  EnableMemberships: {
+    en: () => 'Enable a memberships contract to use memberships.',
+    ja: () => '',
+  },
+  StoreMemberships: {
+    en: () => 'Store your memberships to a contract.',
+    ja: () => '',
+  },
+  InitMemberships: {
+    en: () => 'Initialize your memberships',
+    ja: () => '',
+  },
+  InitMembershipsFailed: {
+    en: () => 'Failed to initialize memberships, try again!',
+    ja: () => '',
+  },
+  SettingUpMemberships: {
+    en: () => 'Setting up memberships...',
+    ja: () => '',
+  },
+  SettingUpMembershipsFailed: {
+    en: () => 'Setting up memberships failed, try again!',
+    ja: () => '',
+  },
+  MembershipSetupComplete: {
+    en: () => 'Membership setup complete!',
+    ja: () => '',
+  },
+  FetchingDetails: {
+    en: () => 'Fetching initialization details...',
+    ja: () => '',
+  },
+  MembershipInitialized: {
+    en: () => 'Membership initialized, fetching setup details...',
+    ja: () => '',
+  },
+  ActivationInProgress: {
+    en: () => 'Activation in progress...',
+    ja: () => '',
+  },
+  ActivationFailed: {
+    en: () => 'Activating failed, try again!',
+    ja: () => '',
+  },
+  Activated: {
+    en: () => 'Activated, setting up initialization..',
+    ja: () => '',
+  },
+  Finalizing: {
+    en: () => 'Finalizing publishing...',
+    ja: () => '',
+  },
+  PublishFailed: {
+    en: () => 'Publishing failed, try again!',
+    ja: () => '',
+  },
+  LoadingOverview: {
+    en: () => 'Your Club is published, loading overview...',
+    ja: () => '',
+  },
+  Publishing: {
+    en: () => 'Publishing your club...',
+    ja: () => '',
+  },
+  PreparingPublishing: {
+    en: () => 'Preparing publishing your club...',
+    ja: () => '',
+  },
+  AwaitingConfirmation: {
+    en: () => 'Awaiting transaction confirmation on wallet...',
+    ja: () => '',
+  },
+  TransactionProcessing: {
+    en: () => 'Transaction processing on the blockchain...',
+    ja: () => '',
+  },
+  InitializationComplete: {
+    en: () => 'Initialization complete!',
+    ja: () => '',
+  },
+  NoProvider: {
+    en: () => 'No provider provided, try again!',
+    ja: () => '',
+  },
+  AddressFailed: {
+    en: () => 'Entered address failed, try again!',
+    ja: () => '',
+  },
+} satisfies ClubsI18nParts
