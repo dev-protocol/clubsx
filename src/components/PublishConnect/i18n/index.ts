b85e952081a28ece0f45b5ee2a1c3fe433476170
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-02-02 15:46:27 +0900

    PublishConnect component localization

---
+++ src/components/PublishConnect/i18n/index.ts
@@ -0,0 +1,24 @@
+import type { ClubsI18nParts } from '@devprotocol/clubs-core'
+
+export const Strings = {
+  Connect: {
+    en: () => 'Connect',
+    ja: () => '',
+  },
+  Connected: {
+    en: () => 'Connected',
+    ja: () => '',
+  },
+  ConnectYourWallet: {
+    en: () => 'Connect your wallet',
+    ja: () => '',
+  },
+  FetchingWalletDetails: {
+    en: () => 'Fetching wallet details...',
+    ja: () => '',
+  },
+  HowToCreate: {
+    en: () => 'How do I create a wallet?',
+    ja: () => '',
+  },
+} satisfies ClubsI18nParts
