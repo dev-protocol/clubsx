e993af9729c26290cf5fbac74bb8ce1586a564ec
aggre <hiroyuki.aggre@gmail.com>
2024-03-11 15:50:30 +0900

    major public APIs have cors header

--- src/pages/api/hasCreationLimitReached/[...identifier].ts
+++ src/pages/api/hasCreationLimitReached/[...identifier].ts
@@ -1,4 +1,5 @@
 import { hasCreationLimitReached } from './util'
+import { headers } from '@fixtures/api/headers'
 
 export const GET = async ({
   params: { identifier },
@@ -18,5 +19,6 @@ export const GET = async ({
     await hasCreationLimitReached(identifier)
   return new Response(JSON.stringify({ isCreationLimitReached }), {
     status: 200,
+    headers,
   })
 }
