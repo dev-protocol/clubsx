ca4da47505024b276ab607ca2538df1a318d0672
aggre <hiroyuki.aggre@gmail.com>
2024-03-18 13:07:23 +0900

    update

--- src/pages/api/clubs.ts
+++ src/pages/api/clubs.ts
@@ -1,3 +1,4 @@
+import { whenDefined } from '@devprotocol/util-ts'
 import {
   getClub,
   getDefaultClient,
@@ -7,21 +8,38 @@ import type { APIRoute } from 'astro'
 
 /**
  * /api/clubs?p=0x1234&p=0x5678&p=0x9abc
+ * @returns {Promise<Response>}
  * @example How to call this API with multiple property addresses
  * ```ts
  * const url = new URL(...)
  * url.searchParams.append('p', '0x1234')
  * url.searchParams.append('p', '0x5678')
  * url.searchParams.append('p', '0x9abc')
+ *
+ * const response = await fetch(url.toString())
+ * const data = await response.json()
+ * console.log(data)
+ * // => [{ id: 'foo', propertyAddress: '0x1234', encodedConfig: 'ENCODED_CLUBS_CONFIGURATION' }, ...]
  * ```
  */
-export const GET: APIRoute = async ({ request, url }) => {
+export const GET: APIRoute = async ({ request, url }): Promise<Response> => {
   const propertyAddresses = url.searchParams.getAll('p')
 
   const client = await withCheckingIndex(getDefaultClient)
   const data = await Promise.all(
     propertyAddresses.map((p) => getClub(p, client)),
   )
+  const res = await whenDefined(data, (allClubs) =>
+    Promise.all(
+      allClubs.map((club) =>
+        whenDefined(club, async ({ id, propertyAddress }) => ({
+          id,
+          propertyAddress,
+          encodedConfig: await client.get(id),
+        })),
+      ),
+    ),
+  )
 
-  return new Response(JSON.stringify({ data }), { status: 200 })
+  return new Response(JSON.stringify(res), { status: 200 })
 }
