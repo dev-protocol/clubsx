5be1fba4e1f2d4e217ca642589e3bbc62874ed38
Kent / Kuro-chan <89888685+kuro-chan-dev@users.noreply.github.com>
2024-03-27 11:51:39 +0900

    Update index.ts
    Hi @yashdesu, translation review is done. Hope OK.

--- src/plugins/achievements/i18n/index.ts
+++ src/plugins/achievements/i18n/index.ts
@@ -3,7 +3,7 @@ import type { ClubsI18nParts } from '@devprotocol/clubs-core'
 export const Strings = {
   YouAreNowHolding: {
     en: 'You are now holding',
-    ja: 'あなたは今持っています',
+    ja: '現在所持しています',
   },
   Congratulations: {
     en: 'Congratulation!',
@@ -11,46 +11,46 @@ export const Strings = {
   },
   TxErrorMsg: {
     en: 'Error occured, try agian or contact support.',
-    ja: 'エラーが発生しました。agian を試すか、サポートにお問い合わせください。',
+    ja: 'エラーが発生しました。もう一度試すか、サポートにお問い合わせください',
   },
   TxSigRejected: {
     en: 'Wallet signature rejected, try again.',
-    ja: 'ウォレットの署名が拒否されました。もう一度お試しください。',
+    ja: 'ウォレットの署名が拒否されました。もう一度お試しください',
   },
   CantClaimMsg: {
     en: `Looks like you can't claim this achievement.`,
-    ja: `この実績は申請できないようです。`,
+    ja: `この実績を取得できないようです`,
   },
   SignTxMsg: {
     en: 'Sign to claim the achievement.',
-    ja: '成果を主張するために署名します。',
+    ja: '署名して実績を取得する',
   },
   AlreadyClaimed: {
     en: 'Achievement is already claimed.',
-    ja: '成果はすでに主張されています。',
+    ja: '実績を取得済みです',
   },
   Claiming: {
     en: 'Claiming',
-    ja: '主張する',
+    ja: '取得する',
   },
   Claimed: {
     en: 'Claimed',
-    ja: '主張した',
+    ja: '取得済み',
   },
   Claim: {
     en: 'Claim',
-    ja: '請求',
+    ja: '取得する',
   },
   Achievement: {
     en: 'Achievement',
-    ja: '成果',
+    ja: '実績',
   },
   AchievementDataNotFound: {
     en: 'We could not find the Achievement, try again or contact the team for support.',
-    ja: '実績が見つかりませんでした。再試行するか、チームに連絡してサポートを求めてください。',
+    ja: '実績が見つかりませんでした。もう一度試すか、サポートにお問い合わせください',
   },
   SignInMsg: {
     en: 'Please sign in.',
-    ja: 'サインインしてください。',
+    ja: 'サインインしてください',
   },
 } satisfies ClubsI18nParts
