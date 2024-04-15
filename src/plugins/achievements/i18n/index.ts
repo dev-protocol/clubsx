3f187b1ba51ff49b4cf88bdd4ab210b766ef4f99
Kent / Kuro-chan <89888685+kuro-chan-dev@users.noreply.github.com>
2024-04-15 16:07:40 +0900

    Update index.ts

--- src/plugins/achievements/i18n/index.ts
+++ src/plugins/achievements/i18n/index.ts
@@ -3,7 +3,7 @@ import type { ClubsI18nParts } from '@devprotocol/clubs-core'
 export const Strings = {
   YouAreNowHolding: {
     en: 'You are now holding',
-    ja: '現在所持しています',
+    ja: '今回取得した実績証明NFT：',
   },
   Congratulations: {
     en: 'Congratulation!',
@@ -19,19 +19,19 @@ export const Strings = {
   },
   CantClaimMsg: {
     en: `Looks like you can't claim this achievement.`,
-    ja: `この実績を取得できないようです`,
+    ja: `この実績証明NFTを取得できないようです`,
   },
   SignTxMsg: {
     en: 'Sign to claim the achievement.',
-    ja: '署名して実績を取得する',
+    ja: 'クリックして実績証明NFTを取得する',
   },
   AlreadyClaimed: {
     en: 'Achievement is already claimed.',
-    ja: '実績を取得済みです',
+    ja: '取得済みの実績証明NFTです',
   },
   Claiming: {
     en: 'Claiming',
-    ja: '取得する',
+    ja: '取得中',
   },
   Claimed: {
     en: 'Claimed',
@@ -43,11 +43,11 @@ export const Strings = {
   },
   Achievement: {
     en: 'Achievement',
-    ja: '実績',
+    ja: '実績証明NFT',
   },
   AchievementDataNotFound: {
     en: 'We could not find the Achievement, try again or contact the team for support.',
-    ja: '実績が見つかりませんでした。もう一度試すか、サポートにお問い合わせください',
+    ja: '実績証明NFTが見つかりません。もう一度試すか、サポートにお問い合わせください',
   },
   SignInMsg: {
     en: 'Please sign in.',
