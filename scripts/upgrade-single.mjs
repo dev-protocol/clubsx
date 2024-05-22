a4515d67cac838b0f7a2d9f47f691fbd2ac378bc
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-05-22 17:19:07 +0900

    upgrade single script creates tickets for metawater

--- scripts/upgrade-single.mjs
+++ scripts/upgrade-single.mjs
@@ -4,7 +4,20 @@ import { decode, encode } from '@devprotocol/clubs-core'
 
 dotenv.config()
 
-const KEY = 'xxx'
+const KEY = 'test-mizu'
+
+const createAvailabilities = (startHour, endHour) => {
+  return Array.from({ length: 5 }, (_, i) => ({
+    type: 'weekday-time',
+    weekday: i + 1,
+    start: `${startHour} hour`,
+    end: `${endHour} hour`,
+    tz: 'Asia/Tokyo',
+  }))
+}
+
+const ticketWebhook =
+  'https://clubs-userland-mw-test.vercel.app/api/webhooks/tickets/Fbd64p/dest/airtable/{AIRTABLE_TABLE_ID}'
 
 const upgrade = (config) => {
   const deocdedConfig = decode(config)
@@ -13,6 +26,69 @@ const upgrade = (config) => {
   /**
    * Write upgrading script here
    */
+  const metawaterTicketsPlugin = {
+    id: 'devprotocol:clubs:plugin:tickets',
+    options: [
+      {
+        key: 'tickets',
+        value: [
+          {
+            // Regional Coupon
+            payload:
+              '0x4873ac86f0764d57d7e71d276c7341ec9532d1dc6a4fdf6997220723a643ee32',
+            importedFrom: {
+              plugin: 'devprotocol:clubs:simple-memberships',
+              key: 'memberships',
+            },
+            name: '地域クーポン',
+            uses: [
+              {
+                id: '500yen',
+                name: '500円',
+                description: '地域クーポン（500円分）として使用できます',
+                duration: '1 hour',
+                within: '1 year',
+                availability: createAvailabilities(9, 22),
+                refreshCycle: undefined,
+              },
+            ],
+            webhooks: {
+              used: {
+                encrypted: ticketWebhook,
+              },
+            },
+          },
+          {
+            // Proof of service NFT
+            payload: '0xAA821D4397B6253BF5d42a9e6B6AaE6B5C52723d', // this is the achievement NFT address
+            importedFrom: {
+              plugin: 'devprotocol:clubs:simple-memberships',
+              key: 'memberships',
+            },
+            name: '役務証明報酬チケット',
+            uses: [
+              {
+                id: 'proof-of-service',
+                name: '​報酬引き換え',
+                description: 'NFTに表示されている報酬額と引き換えられます',
+                duration: '1 hour',
+                within: '1 year',
+                availability: createAvailabilities(9, 18),
+                refreshCycle: undefined,
+              },
+            ],
+            webhooks: {
+              used: {
+                encrypted: ticketWebhook,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  }
+
+  upgradedConfig.plugins = [...upgradedConfig.plugins, metawaterTicketsPlugin]
 
   return encode(upgradedConfig)
 }
