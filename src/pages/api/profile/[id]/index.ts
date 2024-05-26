68776cb5abe0e884d229825d39fddc15e2a5a6e4
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2024-05-27 07:58:55 +0900

    Add avatar generation for profiles not found
    Added truncated ethereum address and a default avatar generation for profiles that are not found. The avatar is generated using the Boring Avatars library. If a profile is not found, a response with a truncated Ethereum address used as the username and an auto-generated avatar is returned.

--- src/pages/api/profile/[id]/index.ts
+++ src/pages/api/profile/[id]/index.ts
@@ -2,6 +2,34 @@ import { generateProfileId } from '@fixtures/api/keys'
 import { headers, cache } from '@fixtures/api/headers'
 import { createClient } from 'redis'
 
+const truncateEthAddress = (address: string) => {
+  const match = address.match(
+    /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/,
+  )
+  if (!match) return address
+  return `${match[1]}\u2026${match[2]}`
+}
+
+const AVATAR_URL = 'https://source.boringavatars.com/beam'
+
+let cachedSvgDataURL = '';
+const getBoringAvatar = async () => {
+  if (cachedSvgDataURL) {
+    return cachedSvgDataURL;
+  }
+
+  try {
+    const response = await fetch(AVATAR_URL);
+    const body = await response.text();
+    const dataUrl = 'data:image/svg+xml;base64,' + Buffer.from(body).toString('base64');
+    cachedSvgDataURL = dataUrl;
+    return dataUrl;
+  } catch (err) {
+    console.error(err);
+    return '';
+  }
+}
+
 export const GET = async ({
   params: { id },
 }: {
@@ -29,12 +57,18 @@ export const GET = async ({
   const userProfile = (await client.get(profileId)) ?? undefined
   await client.quit()
 
-  return userProfile
-    ? new Response(userProfile, {
+  if (!userProfile) {
+    return new Response(JSON.stringify({
+      username: truncateEthAddress(id),
+      avatar: await getBoringAvatar(),
+    }), {
+      status: 200,
+      headers: { ...headers, ...cache({ maxAge: 30 }) },
+    })
+  }
+
+  return new Response(userProfile, {
         status: 200,
         headers: { ...headers, ...cache({ maxAge: 30 }) },
-      })
-    : new Response(JSON.stringify({ error: 'Profile not found' }), {
-        status: 404,
       })
 }
