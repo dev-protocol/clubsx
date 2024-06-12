a98a27b533d88294558faf850e78a7b92f600afe
Yash Agrawal <yash@devprotocol.xyz>
2024-06-12 12:17:13 +0530

    feat: minor fixes

---
+++ src/pages/passport/i18n/index.ts
@@ -0,0 +1,40 @@
+import type { ClubsI18nParts } from '@devprotocol/clubs-core'
+
+export const Strings = {
+  Profile: {
+    en: 'Profile',
+    ja: 'プロフィール',
+  },
+  Avatar: {
+    en: 'Avatar',
+    ja: 'プロフィール画像',
+  },
+  Username: {
+    en: 'Username',
+    ja: 'ユーザー名',
+  },
+  Save: {
+    en: 'Save',
+    ja: '保存',
+  },
+  Saved: {
+    en: 'Saved',
+    ja: '保存されました',
+  },
+  Error: {
+    en: 'Error',
+    ja: 'エラー',
+  },
+  Achievements: {
+    en: 'Achievements',
+    ja: 'アチーブメント',
+  },
+  Clubs: {
+    en: `Clubs`,
+    ja: `Clubsのメンバーシップ`,
+  },
+  Empty: {
+    en: `Empty`,
+    ja: `まだありません`,
+  },
+} satisfies ClubsI18nParts
