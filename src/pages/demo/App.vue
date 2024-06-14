1cd3c38b06d1f0cc454b5b57b6fc4725261b5e82
Aggre <hiroyuki.aggre@gmail.com>
2024-06-14 18:20:53 +0900

    tweaking

--- src/pages/demo/App.vue
+++ src/pages/demo/App.vue
@@ -5,11 +5,9 @@ import { whenDefined } from '@devprotocol/util-ts'
 const account = ref<string>()
 const achivementId = ref<string>()
 const pageUrl = computed(() => {
-  return (
-    whenDefined(
-      achivementId.value,
-      (id) => `https://developers.clubs.place/achivement/${id}`,
-    ) ?? 'x'
+  return whenDefined(
+    achivementId.value,
+    (id) => `https://developers.clubs.place/achivement/${id}`,
   )
 })
 
@@ -38,7 +36,7 @@ watch(account, async (userAccount) => {
     class="p-8 rounded-3xl bg-black/10 flex flex-col lg:flex-row justify-between items-center gap-5 transition-opacity duration-700"
     :class="account ? 'opacity-30' : ''"
   >
-    <h2 class="font-bold text-3xl">Sign in</h2>
+    <h2 class="font-bold text-3xl">Sign up free</h2>
     <slot />
   </div>
   <div
@@ -50,6 +48,26 @@ watch(account, async (userAccount) => {
       v-if="!pageUrl"
       class="h-8 w-full max-w-48 animate-pulse rounded-full bg-gray-400/60 text-transparent"
     />
-    <a v-if="pageUrl" :href="pageUrl" class="font-bold text-4xl">Get</a>
+    <a
+      v-if="pageUrl"
+      :href="pageUrl"
+      class="font-bold text-4xl flex items-center gap-2"
+    >
+      Get
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-7"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m8.25 4.5 7.5 7.5-7.5 7.5"
+        />
+      </svg>
+    </a>
   </div>
 </template>
