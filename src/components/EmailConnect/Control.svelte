43591abcb5064d9315c803aac87e0fe8ac918edf
aggre <hiroyuki.aggre@gmail.com>
2024-06-20 14:49:53 +0900

    Feat/dynamic for built-in pages (#2561)
    * replace major connect buttons to Dynamic

---
+++ src/components/EmailConnect/Control.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import EmailConnect from './EmailConnect.svelte'
+
+  let open = false
+</script>
+
+<div class="my-8 grid justify-center gap-4">
+  {#if open}
+    <EmailConnect />
+    <button
+      class="hs-button is-outlined is-small text-xs rounded-full px-4"
+      on:click={() => (open = !open)}>Close</button
+    >
+  {:else}
+    <button
+      class="hs-button is-outlined is-small text-xs rounded-full px-4"
+      on:click={() => (open = !open)}
+      >I'm using Magic Link sign-in with email.</button
+    >
+  {/if}
+</div>
