5323325c8db39b698272c5ce002ed4fc7bb29610
Yash Agrawal <yash@devprotocol.xyz>
2024-06-25 15:46:11 +0530

    feat: add support for shops in clubs-payment

--- src/plugins/clubs-payments/utils/compose-items.ts
+++ src/plugins/clubs-payments/utils/compose-items.ts
@@ -3,9 +3,11 @@ import {
   type ClubsFactoryUtils,
   type ClubsPluginOptions,
 } from '@devprotocol/clubs-core'
-import type { ComposedItem, Override } from '..'
 import { whenDefined, type UndefinedOr } from '@devprotocol/util-ts'
+
+import type { ComposedItem, Override } from '..'
 import type { Membership } from '@plugins/memberships'
+import type { Collection, CollectionMembership } from '@plugins/collections'
 
 export const composeItems = (
   options: ClubsPluginOptions,
@@ -23,7 +25,17 @@ export const composeItems = (
         sourceConfig?.options?.find((op) => op.key === ov.key)?.value as
           | undefined
           | Membership[]
-      )?.find((mem) => bytes32Hex(mem.payload) === bytes32Hex(ov.payload))
+          | Collection[]
+      )?.find((s) => {
+        // Check if the keys from collection object are present in s, if yes then it's a collection.
+        return 'memberships' in s ||
+          'requiredMemberships' in s ||
+          'endTime' in s
+          ? !!(s as Collection).memberships.find( // This is a collection, so find the membership and return boolean.
+              (m) => bytes32Hex(m.payload) === bytes32Hex(ov.payload),
+            )
+          : bytes32Hex(s.payload) === bytes32Hex(ov.payload) // This is a membership so directly compare payload.
+      })
       const composed = whenDefined(source, (so) => ({ ...ov, source: so }))
       return composed
     })
