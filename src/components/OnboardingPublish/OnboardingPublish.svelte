7abaf4f9590a3deaed37973259bd584e2fb89954
Yash Agrawal <yash@devprotocol.xyz>
2024-07-24 11:05:05 +0530

    feat(onboarding-ip): add new component for new ip onboarding flow

---
+++ src/components/OnboardingPublish/OnboardingPublish.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import { i18nFactory } from '@devprotocol/clubs-core'
+
+  import { Strings } from './i18n'
+
+  const i18nBase = i18nFactory(Strings)
+  let i18n = i18nBase(['en'])
+
+  onMount(() => {
+    i18n = i18nBase(navigator.languages)
+  })
+</script>
+
+<div class="relative grid justify-center p-4 md:p-0">
+  <section class="my-16 grid gap-8 text-center md:my-32">
+    <h1 class="text-2xl font-bold md:text-5xl">
+      {i18n('Header')}
+    </h1>
+    <!-- <p>{ i18n('SubHeader') }</p> -->
+  </section>
+
+  <div class="grid justify-center gap-16 md:gap-32">
+    <section></section>
+  </div>
+</div>
---
+++ src/components/OnboardingPublish/i18n/index.ts
@@ -0,0 +1,12 @@
+import type { ClubsI18nParts } from '@devprotocol/clubs-core'
+
+export const Strings = {
+  Header: {
+    en: () => 'Publish',
+    ja: () => '公開',
+  },
+  SubHeader: {
+    en: () => '',
+    ja: () => '',
+  },
+} satisfies ClubsI18nParts
