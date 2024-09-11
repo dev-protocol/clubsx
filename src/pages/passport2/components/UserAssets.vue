ce4ada358b8f643257e50d7524fdcf71a68354c2
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2024-09-12 00:28:06 +0530

    wip

---
+++ src/pages/passport2/components/UserAssets.vue
@@ -0,0 +1,98 @@
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import { i18nFactory } from '@devprotocol/clubs-core'
+
+import { Strings } from '../i18n'
+import UserAsset from './UserAsset.vue'
+import Skeleton from '@components/Global/Skeleton.vue'
+import type { AssetDocument } from '@fixtures/api/assets/schema'
+import { JsonRpcProvider } from 'ethers'
+
+const { PUBLIC_ALCHEMY_KEY } = import.meta.env
+
+const props = defineProps<{ account: string; local: boolean }>()
+
+const i18nBase = i18nFactory(Strings)
+let i18n = ref<ReturnType<typeof i18nBase>>(i18nBase(['en']))
+
+const assetsNft = ref<AssetDocument[]>()
+const assetsSbt = ref<AssetDocument[]>()
+const rpcProvider = new JsonRpcProvider(
+  `https://polygon-mainnet.g.alchemy.com/v2/${PUBLIC_ALCHEMY_KEY}`,
+)
+
+onMounted(async () => {
+  i18n.value = i18nBase(navigator.languages)
+
+  const [nfts, sbts] = await Promise.all([
+    fetch(
+      `https://clubs.place/api/assets/related/account/${props.account}/?type=nft&size=999`,
+    ).then((res) => res.json()),
+    fetch(
+      `https://clubs.place/api/assets/related/account/${props.account}/?type=sbt&size=999`,
+    ).then((res) => res.json()),
+  ])
+  assetsNft.value = nfts.data
+  assetsSbt.value = sbts.data
+})
+</script>
+
+<template>
+  <section class="grid gap-8">
+    <h2 class="font-bold text-3xl flex items-center gap-2">
+      {{ i18n('Achievements') }}
+      <span v-if="!assetsSbt" class="h-full w-12"><Skeleton /></span
+      ><span v-if="assetsSbt" class="font-inherit text-inherit"
+        >({{ assetsSbt.length }})</span
+      >
+    </h2>
+
+    <ul class="grid gap-16 grid-cols-[repeat(auto-fill,minmax(280px,1fr))]">
+      <li
+        v-if="assetsSbt?.length"
+        v-for="item in assetsSbt"
+        class="empty:hidden"
+      >
+        <UserAsset :item="item" :provider="rpcProvider" :local="props.local" />
+      </li>
+      <div
+        v-if="assetsSbt?.length === 0"
+        class="rounded-md border border-surface-400 p-8 text-accent-200"
+      >
+        {{ i18n('Empty') }} :)
+      </div>
+
+      <li v-if="!assetsSbt" v-for="item in new Array(6)">
+        <span class="block h-96"><Skeleton /></span>
+      </li>
+    </ul>
+  </section>
+  <section class="grid gap-8">
+    <h2 class="font-bold text-3xl flex items-center gap-2">
+      {{ i18n('Clubs') }}
+      <span v-if="!assetsNft" class="h-full w-12"><Skeleton /></span
+      ><span v-if="assetsNft" class="font-inherit text-inherit"
+        >({{ assetsNft?.length }})</span
+      >
+    </h2>
+
+    <ul class="grid gap-16 grid-cols-[repeat(auto-fill,minmax(280px,1fr))]">
+      <li
+        v-if="assetsNft?.length"
+        v-for="item in assetsNft"
+        class="empty:hidden"
+      >
+        <UserAsset :item="item" :provider="rpcProvider" :local="props.local" />
+      </li>
+      <div
+        v-if="assetsNft?.length === 0"
+        class="rounded-md border border-surface-400 p-8 text-accent-200"
+      >
+        {{ i18n('Empty') }} :)
+      </div>
+      <li v-if="!assetsNft" v-for="item in new Array(6)">
+        <span class="block h-96"><Skeleton /></span>
+      </li>
+    </ul>
+  </section>
+</template>
---
+++ src/pages/passport2/i18n/index.ts
@@ -0,0 +1,80 @@
+import type { ClubsI18nParts } from '@devprotocol/clubs-core'
+
+export const Strings = {
+  Profile: {
+    en: 'Profile',
+    ja: 'プロフィール',
+  },
+  Avatar: {
+    en: 'Avatar',
+    ja: 'プロフィール画像',
+  },
+  Username: {
+    en: 'Username',
+    ja: 'ユーザー名',
+  },
+  Save: {
+    en: 'Save',
+    ja: '保存',
+  },
+  Saved: {
+    en: 'Saved',
+    ja: '保存されました',
+  },
+  Error: {
+    en: 'Error',
+    ja: 'エラー',
+  },
+  Achievements: {
+    en: 'Achievements',
+    ja: 'アチーブメント',
+  },
+  Clubs: {
+    en: `Clubs`,
+    ja: `Clubsのメンバーシップ`,
+  },
+  Empty: {
+    en: `Empty`,
+    ja: `まだありません`,
+  },
+  Edit: {
+    en: 'Edit',
+    ja: '編集',
+  },
+  Manage: {
+    en: 'Manage',
+    ja: '管理',
+  },
+  ClubPage: {
+    en: 'Club page',
+    ja: 'クラブページ',
+  },
+  Published: {
+    en: 'Published',
+    ja: '発行済み',
+  },
+  PublishDraftClubs: {
+    en: 'No published clubs found. You have clubs in drafting phase. Publish them to unlock their full potential and make them accessible.',
+    ja: '公開されたクラブは見つかりませんでした。作成したクラブを公開して、他のユーザーがアクセスできるようにしましょう',
+  },
+  NoClubFound: {
+    en: 'No clubs found.',
+    ja: 'クラブが見つかりませんでした',
+  },
+  Draft: {
+    en: 'Draft',
+    ja: '作成中のクラブ',
+  },
+  NoDraftClub: {
+    en: 'No draft clubs found. Create clubs now',
+    ja: '作成中のクラブが見つかりませんでした。クラブを作成してください',
+  },
+  NoDraftClubFound: {
+    en: 'No draft clubs found.',
+    ja: '作成中のクラブが見つかりませんでした',
+  },
+  CreateClubsNow: {
+    en: 'Create clubs now',
+    ja: '今すぐクラブを作成する',
+  },
+} satisfies ClubsI18nParts
