18af8271c597b6c3a26013a890c86ca628009b71
yashdesu <yash@devprotocol.xyz>
2024-09-16 22:19:56 +0530

    feat: add skeleton for profile edit page

--- src/pages/passport/i18n/index.ts
+++ src/pages/passport/i18n/index.ts
@@ -6,7 +6,7 @@ export const Strings = {
     ja: 'パスポートスキン',
   },
   PinnedItems: {
-    en: 'Pinned items',
+    en: 'Pinned Items',
     ja: 'ピン留めされたアイテム',
   },
   MemebershipsAndAchievements: {
