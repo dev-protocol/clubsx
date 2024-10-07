c0e3898ced0707068957a9fa6ec7f1b06c3be1f8
yashdesu <yash@devprotocol.xyz>
2024-10-07 15:06:21 +0530

    feat: add passport plugin in superuser page

--- src/constants/superuser.ts
+++ src/constants/superuser.ts
@@ -1,6 +1,7 @@
 export const superusers = [
   '0x57E21bd98612DE0Bd1723F4bf81A944eF7BfF526',
   '0xbA0Fd235B04dc3ac9D7076d3aFA9025f2B3A8AE5',
+  '0xfc4a9e2B406C515415BBcF502A632aefB185A875',
 ]
 
 export const signatureExpiration = 10000
