723121895bb498679dd934397aa1d98bed2ef032
yashdesu <yash@devprotocol.xyz>
2024-10-23 14:04:37 +0900

    feat: add a spotlight section [wip]

--- src/pages/passport/components/UserProfileEdit.svelte
+++ src/pages/passport/components/UserProfileEdit.svelte
@@ -834,6 +834,7 @@
     />
   </label>
 
+  <!-- Passport skins -->
   <label class="hs-form-field is-filled mt-[76px]">
     <div class="hs-form-field__label flex items-center justify-between mb-1">
       <span class="hs-form-field__label">
@@ -894,6 +895,68 @@
     {/if}
   </label>
 
+  <!-- Spotlight clips -->
+  <span class="hs-form-field is-filled mt-[76px]">
+    <div class="hs-form-field__label flex items-center justify-between mb-1">
+      <span class="hs-form-field__label">
+        {i18n('PassportSpotlightClips')} ({profile?.skins?.at(0)?.clips
+          ?.length ?? 0})
+      </span>
+      <button
+        disabled={!eoa ||
+          !passportNonSkinItems.length ||
+          profileFetching ||
+          passportItemFetching ||
+          profileUpdating}
+        on:click|preventDefault={() => resetPinnedNonSkinItems()}
+        class="hs-button is-filled is-large w-fit text-center">Reset</button
+      >
+    </div>
+
+    {#if !eoa}
+      <div class="rounded-md border border-surface-400 p-8 text-accent-200">
+        {i18n('ConnectWalletTryAgain')} :)
+      </div>
+    {:else if passportItemFetching || profileFetching}
+      <div
+        class="rounded-md border border-surface-400 p-8 text-accent-200 h-48"
+      >
+        <Skeleton />
+      </div>
+    {:else if !passportItemFetching && !profileFetching && !profile.skins?.at(0)?.clips?.length}
+      <div class="rounded-md border border-surface-400 p-8 text-accent-200">
+        {i18n('Empty')} :) <br />{@html i18n('PinClips')}
+      </div>
+    {:else if !passportItemFetching && !profileFetching && profile.skins?.at(0)?.clips?.length && passportNonSkinItems?.length}
+      <ul class="grid gap-16 grid-cols-[repeat(auto-fill,minmax(280px,1fr))]">
+        {#each passportNonSkinItems as item, i}
+          {#if item.payload && profile?.skins
+              ?.at(0)
+              ?.clips?.find((clip) => clip.payload === item.payload)}
+            <li id={`assetsPassportItems-${i.toString()}`} class="empty:hidden">
+              <PassportAsset
+                props={((clip) => ({
+                  item: item,
+                  provider: rpcProvider,
+                  local: isLocal,
+                  isEditable: true,
+                  editAction: () => onEditClip(item),
+                  description: clip?.description,
+                  frameColorHex: clip?.frameColorHex,
+                }))(
+                  profile?.skins
+                    ?.at(0)
+                    ?.clips?.find((clip) => clip.payload === item.payload),
+                )}
+              />
+            </li>
+          {/if}
+        {/each}
+      </ul>
+    {/if}
+  </span>
+
+  <!-- Pinned clips -->
   <span class="hs-form-field is-filled mt-[76px]">
     <div class="hs-form-field__label flex items-center justify-between mb-1">
       <span class="hs-form-field__label">
