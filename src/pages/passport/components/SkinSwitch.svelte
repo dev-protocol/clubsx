766c23db2531fe83dd5d161ca916af09bb0bdae1
yashdesu <yash@devprotocol.xyz>
2024-10-27 22:47:07 +0530

    feat: improve css of switcher

--- src/pages/passport/components/SkinSwitch.svelte
+++ src/pages/passport/components/SkinSwitch.svelte
@@ -12,47 +12,40 @@
   }
 </script>
 
-<div class="grow">
-  <div class="ml-4">
-    <div class="flex py-2 px-4 items-center gap-[15px] rounded-md bg-white">
-      <div class="relative">
-        <button on:click={() => toggleSelectedPassportSkins()}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-6 h-6"
-            viewBox="0 0 24 24"
-            fill="none"
-          >
-            <path
-              d="M3.75 6.75H20.25M3.75 12H20.25M3.75 17.25H20.25"
-              stroke="black"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </button>
-      </div>
-      <div
-        class="overflow-hidden text-black truncate font-sans text-base font-bold leading-normal line-clamp-1"
+<div class="relative">
+  <div class="flex py-2 px-4 items-center gap-[15px] rounded-lg bg-surface-200">
+    <button on:click={() => toggleSelectedPassportSkins()}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-6 h-6"
+        viewBox="0 0 24 24"
+        fill="none"
       >
-        {skins?.find((item) => item.id === selectedSkinId)?.name ?? 'Default'}
-      </div>
-      {#if dropdownVisible}
-        <ul class="">
-          {#each skins as skin, i}
-            <li>
-              <a
-                href={`/passport/${eoa}/${skin?.id}`}
-                class="overflow-hidden text-black truncate font-sans text-base font-bold leading-normal line-clamp-1"
-                >{skin?.name ?? `Profile ${i}`}</a
-              >
-            </li>
-          {/each}
-        </ul>
-      {/if}
-    </div>
+        <path
+          d="M3.75 6.75H20.25M3.75 12H20.25M3.75 17.25H20.25"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </button>
+    <p class="font-sans text-base font-bold leading-normal line-clamp-1">
+      {skins?.find((item) => item.id === selectedSkinId)?.name ?? 'Default'}
+    </p>
   </div>
+
+  {#if dropdownVisible}
+    <div class="absolute left-0 mt-2 grid gap-[1px] w-full max-w-full">
+      {#each skins as skin, i}
+        <a
+          href={`/passport/${eoa}/${skin?.id}`}
+          class="py-2 px-4 rounded-lg font-sans text-base font-bold leading-normal line-clamp-1 bg-surface-200 hover:bg-surface-400"
+          >{skin?.name ?? `Profile ${i}`}</a
+        >
+      {/each}
+    </div>
+  {/if}
 </div>
 
 <style>
