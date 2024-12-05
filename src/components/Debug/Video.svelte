a6906c556c235edf03b93f740cf8e43276ecb505
yashdesu <yash@devprotocol.xyz>
2024-12-05 22:41:24 +0530

    feat: minor fixes

---
+++ src/components/Debug/Video.svelte
@@ -0,0 +1,22 @@
+<div>
+  <h1>Hello</h1>
+  <video
+    id="videoElement"
+    controls
+    class="w-full max-w-2xl mx-auto rounded-lg border border-gray-700 shadow-lg"
+  >
+    <source
+      src="https://clubs-storage-git-feat-add-uuid-mapping-devprtcl.vercel.app/9-Fz9_dMJyQLdcNpa31x2"
+      type="video/mp4"
+    />
+  </video>
+
+  <div id="video-info" class="p-4 bg-gray-800 rounded-lg mt-4">
+    <p>mimeCodec:<span id="mimeCodec">0</span></p>
+    <p>Current Time: <span id="currentTime">0</span></p>
+    <p>Current Segment: <span id="currentSegment">0</span></p>
+    <p>Segment Duration: <span id="segmentDuration">0</span></p>
+    <p>Bytes Fetched: <span id="bytesFetched">0</span></p>
+    <p>Requested Segments: <span id="requestedSegments">0</span></p>
+  </div>
+</div>
