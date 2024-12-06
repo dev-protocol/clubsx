0aeea9aa9a2ec26331c6268fe74ea784f991402a
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2024-12-06 20:48:33 +0530

    wip: segmentation

--- src/components/Content/VideoScript.svelte
+++ src/components/Content/VideoScript.svelte
@@ -1,91 +1,11 @@
 <script lang="ts">
   import { onMount } from 'svelte'
-  import { FFmpeg } from '@ffmpeg/ffmpeg'
-  import { fetchFile, toBlobURL } from '@ffmpeg/util'
-
-  const ffmpegRef = new FFmpeg()
-
-  // async function getMimeCodec(videoChunk) {
-  //   // Load FFmpeg
-  //   if (!ffmpeg.isLoaded()) {
-  //     await ffmpeg.load();
-  //   }
-
-  //   // Write the video chunk to FFmpeg's virtual file system
-  //   ffmpeg.FS('writeFile', 'input_video', await fetchFile(videoChunk));
-
-  //   // Run FFprobe to get metadata
-  //   await ffmpeg.run('-i', 'input_video');
-
-  //   // Read the FFmpeg logs to extract codec information
-  //   const logs = ffmpeg.logs;
-  //   let containerFormat = '';
-  //   let videoCodec = '';
-  //   let audioCodec = '';
-
-  //   for (const log of logs) {
-  //     // Extract container format
-  //     if (!containerFormat) {
-  //       const formatMatch = log.message.match(/Input #0, (\w+),/);
-  //       if (formatMatch) {
-  //         containerFormat = formatMatch[1];
-  //       }
-  //     }
-
-  //     // Extract video codec
-  //     if (!videoCodec) {
-  //       const videoMatch = log.message.match(/Video: (\w+)/);
-  //       if (videoMatch) {
-  //         videoCodec = videoMatch[1];
-  //       }
-  //     }
-
-  //     // Extract audio codec
-  //     if (!audioCodec) {
-  //       const audioMatch = log.message.match(/Audio: (\w+)/);
-  //       if (audioMatch) {
-  //         audioCodec = audioMatch[1];
-  //       }
-  //     }
-
-  //     // Break the loop if all information is gathered
-  //     if (containerFormat && videoCodec && audioCodec) {
-  //       break;
-  //     }
-  //   }
-
-  //   // Map container format to MIME type
-  //   const mimeTypes = {
-  //     mov: 'video/quicktime',
-  //     mp4: 'video/mp4',
-  //     mkv: 'video/x-matroska',
-  //     avi: 'video/x-msvideo',
-  //     // Add more mappings as needed
-  //   };
-
-  //   const mimeType = mimeTypes[containerFormat] || 'application/octet-stream';
-
-  //   // Map codecs to MIME codecs
-  //   const codecMappings = {
-  //     h264: 'avc1.42E01E',
-  //     aac: 'mp4a.40.2',
-  //     // Add more mappings as needed
-  //   };
-
-  //   const videoCodecMime = codecMappings[videoCodec.toLowerCase()] || videoCodec;
-  //   const audioCodecMime = codecMappings[audioCodec.toLowerCase()] || audioCodec;
-
-  //   // Construct the MIME codec string
-  //   const mimeCodec = `${mimeType}; codecs="${videoCodecMime}, ${audioCodecMime}"`;
-
-  //   return mimeCodec;
-  // }
-
-  // // Usage example
-  // const videoChunk = 'path/to/video/chunk';
-  // getMimeCodec(videoChunk).then((mimeCodec) => {
-  //   console.log(`MIME codec: ${mimeCodec}`);
-  // });
+  let MIME_CODEC: string
+  let TOTAL_SEGMENTS: number
+  let CURRENT_SEGMENT: number
+  let BYTES_FETCHED: number
+  let REQUESTED_SEGMENTS: string
+  let segmentDuration = 0
 
   onMount(async () => {
     if (!window.MediaSource) {
@@ -94,60 +14,64 @@
       return
     }
 
-    const video = document.getElementById('videoElement')
+    const video = document.getElementById('videoElement') as HTMLVideoElement
     const assetURL =
       'https://e54a8car3bcq7q8h.public.blob.vercel-storage.com/frag_bunny-ZJaX6ShbMi5ciqThXkW4zyuynptZcT.mp4'
     // Need to be specific for Blink regarding codecs
     // ./mp4info frag_bunny.mp4 | grep Codec
-    var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
-    var totalSegments = 5
-    var segmentLength = 0
-    var segmentDuration = 0
-    var bytesFetched = 0
-    var requestedSegments = []
+    MIME_CODEC = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+    TOTAL_SEGMENTS = 5
+    let segmentLength = 0
+    segmentDuration = 0
+    let bytesFetched = 0
+    let requestedSegments: boolean[] = []
 
-    for (var i = 0; i < totalSegments; ++i) requestedSegments[i] = false
+    for (let i = 0; i < TOTAL_SEGMENTS; ++i) requestedSegments[i] = false
 
-    var mediaSource = null
-    if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
+    let mediaSource: MediaSource | null = null
+    if ('MediaSource' in window && MediaSource.isTypeSupported(MIME_CODEC)) {
       mediaSource = new MediaSource()
       //console.log(mediaSource.readyState); // closed
       video.src = URL.createObjectURL(mediaSource)
       mediaSource.addEventListener('sourceopen', sourceOpen)
     } else {
-      console.error('Unsupported MIME type or codec: ', mimeCodec)
+      console.error('Unsupported MIME type or codec: ', MIME_CODEC)
     }
 
-    var sourceBuffer = null
-    function sourceOpen(_) {
-      sourceBuffer = mediaSource.addSourceBuffer(mimeCodec)
-      getFileLength(assetURL, function (fileLength) {
+    let sourceBuffer: SourceBuffer | null = null
+    
+    function sourceOpen(_: Event) {
+      if (mediaSource) {
+        sourceBuffer = mediaSource.addSourceBuffer(MIME_CODEC)
+      }
+      getFileLength(assetURL, function (fileLength: number) {
         console.log((fileLength / 1024 / 1024).toFixed(2), 'MB')
         //totalLength = fileLength;
-        segmentLength = Math.round(fileLength / totalSegments)
+        segmentLength = Math.round(fileLength / TOTAL_SEGMENTS)
         //console.log(totalLength, segmentLength);
         fetchRange(assetURL, 0, segmentLength, appendSegment)
         requestedSegments[0] = true
         video.addEventListener('timeupdate', checkBuffer)
         video.addEventListener('canplay', function () {
-          segmentDuration = video.duration / totalSegments
+          segmentDuration = video.duration / TOTAL_SEGMENTS
           video.play()
         })
         video.addEventListener('seeking', seek)
       })
     }
 
-    function getFileLength(url, cb) {
-      var xhr = new XMLHttpRequest()
+    function getFileLength(url: string, cb: (length: number) => void) {
+      let xhr = new XMLHttpRequest()
       xhr.open('head', url)
       xhr.onload = function () {
-        cb(xhr.getResponseHeader('content-length'))
+        const length = xhr.getResponseHeader('content-length');
+        cb(length ? parseInt(length, 10) : 0);
       }
       xhr.send()
     }
 
-    function fetchRange(url, start, end, cb) {
-      var xhr = new XMLHttpRequest()
+    function fetchRange(url: string, start: number, end: number, cb: (response: ArrayBuffer) => void) {
+      let xhr = new XMLHttpRequest()
       xhr.open('get', url)
       xhr.responseType = 'arraybuffer'
       xhr.setRequestHeader('Range', 'bytes=' + start + '-' + end)
@@ -156,19 +80,25 @@
         bytesFetched += end - start + 1
         cb(xhr.response)
       }
+      BYTES_FETCHED = end - start + 1
+      REQUESTED_SEGMENTS = `${start} - ${end}`
       xhr.send()
     }
 
-    async function appendSegment(chunk) {
-      // await getMimeCodec(chunk).then((mimeCodec) => {
-      //   console.log(`MIME codec: ${mimeCodec}`);
+    async function appendSegment(chunk: ArrayBuffer) {
+      // await getMIME_CODEC(chunk).then((MIME_CODEC) => {
+      //   console.log(`MIME codec: ${MIME_CODEC}`);
       // });
-      sourceBuffer.appendBuffer(chunk)
+      if (sourceBuffer) {
+        sourceBuffer.appendBuffer(chunk)
+      } else {
+        console.error('sourceBuffer is null')
+      }
     }
 
-    function checkBuffer(_) {
-      var currentSegment = getCurrentSegment()
-      if (currentSegment === totalSegments && haveAllSegments()) {
+    function checkBuffer(_: Event) {
+      let currentSegment = getCurrentSegment()
+      if (currentSegment === TOTAL_SEGMENTS && haveAllSegments() && mediaSource) {
         console.log('last segment', mediaSource.readyState)
         mediaSource.endOfStream()
         video.removeEventListener('timeupdate', checkBuffer)
@@ -185,8 +115,9 @@
       //console.log(video.currentTime, currentSegment, segmentDuration);
     }
 
-    function seek(e) {
-      console.log(e)
+    function seek(e: Event) {
+      if(sourceBuffer && mediaSource) {
+        console.log(e)
       if (mediaSource.readyState === 'open') {
         sourceBuffer.abort()
         console.log(mediaSource.readyState)
@@ -194,10 +125,14 @@
         console.log('seek but not open?')
         console.log(mediaSource.readyState)
       }
+      } else {
+        console.error('sourceBuffer is null')
+      }
     }
 
     function getCurrentSegment() {
-      return ((video.currentTime / segmentDuration) | 0) + 1
+      CURRENT_SEGMENT = ((video.currentTime / segmentDuration) | 0) + 1
+      return CURRENT_SEGMENT
     }
 
     function haveAllSegments() {
@@ -206,7 +141,7 @@
       })
     }
 
-    function shouldFetchNextSegment(currentSegment) {
+    function shouldFetchNextSegment(currentSegment: number) {
       return (
         video.currentTime > segmentDuration * currentSegment * 0.8 &&
         !requestedSegments[currentSegment]
@@ -224,11 +159,11 @@
   >
   </video>
   <div id="video-info" class="p-4 bg-gray-800 rounded-lg mt-4">
-    <p>mimeCodec:<span id="mimeCodec">0</span></p>
-    <p>Current Time: <span id="currentTime">0</span></p>
-    <p>Current Segment: <span id="currentSegment">0</span></p>
-    <p>Segment Duration: <span id="segmentDuration">0</span></p>
-    <p>Bytes Fetched: <span id="bytesFetched">0</span></p>
-    <p>Requested Segments: <span id="requestedSegments">0</span></p>
+    <p>MIME_CODEC: <span id="MIME_CODEC">{MIME_CODEC}</span></p>
+    <p>Total Segment: <span id="currentTime">{TOTAL_SEGMENTS}</span></p>
+    <p>Current Segment: <span id="currentSegment">{CURRENT_SEGMENT}</span></p>
+    <p>Segment Duration: <span id="segmentDuration">{segmentDuration}</span></p>
+    <p>Bytes Fetched: <span id="bytesFetched">{BYTES_FETCHED}</span></p>
+    <p>Requested Segments: <span id="requestedSegments">{REQUESTED_SEGMENTS}</span></p>
   </div>
 </div>
