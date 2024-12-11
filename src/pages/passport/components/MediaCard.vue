d72e81a8cd8a640df50af532d27724af8eefdc69
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2024-12-11 13:07:52 +0530

    fix: 'video' is possibly 'null'.

--- src/pages/passport/components/MediaCard.vue
+++ src/pages/passport/components/MediaCard.vue
@@ -15,14 +15,15 @@ let video: HTMLVideoElement | null = null
 if (props.type === 'short-video' || props.type === 'short-video-link') {
   onMounted(async () => {
     video = document.querySelector('video')
-
-    try {
-      const response = await fetch(props.src)
-      const blob = await response.blob()
-      const blobDataUrl = URL.createObjectURL(blob)
-      video.src = blobDataUrl
-    } catch (error) {
-      console.error('Error loading video:', error)
+    if(video) {
+      try {
+        const response = await fetch(props.src)
+        const blob = await response.blob()
+        const blobDataUrl = URL.createObjectURL(blob)
+        video.src = blobDataUrl
+      } catch (error) {
+        console.error('Error loading video:', error)
+      }
     }
   })
 }
