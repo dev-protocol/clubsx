f5de85d2f83dd81795667b2694c0f852d8a2a3ba
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2025-01-07 19:24:02 +0900

    Add layout with footer and feed content components
    Introduced a new layout with a header, footer, and dynamic theme support. Added feed page content and footer with navigation icons. Replaced placeholder index page with the new feed layout.

---
+++ src/components/Feed/FeedPageContent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import iconHome from '@assets/footer/icon-home.png'
+import iconClubs from '@assets/footer/icon-clubs.png'
+import iconMe from '@assets/footer/icon-me.png'
+</script>
+<template>
+  <div class="flex flex-col h-full">
+    <div class="flex flex-col flex-grow items-center gap-4">
+      Feed Contents
+    </div>
+  </div>
+</template>
