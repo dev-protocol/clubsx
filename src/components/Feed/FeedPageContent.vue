de633e30bb625ce3850bc550093b985e175206e5
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2025-01-08 09:49:13 +0900

    Refactor Feed page layout and add update cards.
    Reorganized the Feed page structure by adjusting layout spacing and introducing styled update cards. Added elements like avatars, descriptions, and nested components for better user engagement. This enhances the visual presentation and readability of the feed.

--- src/components/Feed/FeedPageContent.vue
+++ src/components/Feed/FeedPageContent.vue
@@ -4,7 +4,27 @@ import iconClubs from '@assets/footer/icon-clubs.png'
 import iconMe from '@assets/footer/icon-me.png'
 </script>
 <template>
-  <div class="flex flex-col h-full">
-    <div class="flex flex-col flex-grow items-center gap-4">Feed Contents</div>
+  <div class="flex flex-col px-2 h-full">
+    <p class="text-base font-bold">Latest updates</p>
+    <div class="flex flex-col gap-2">
+      <div class="flex gap-3 p-2 rounded-xl bg-yellow-400">
+        <div class="flex flex-col flex-grow">
+          <div class="flex items-center gap-3">
+            <img class="rounded-full w-14 h-14" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg" alt="avatar" />
+            <div class="flex flex-col flex-grow gap-0">
+              <div class="text-xs font-bold">name</div>
+              <div class="flex items-center gap-2 p-1 rounded-sm bg-white" style="width: fit-content">
+                <img class="w-7 rounded-sm" style="aspect-ratio: 16 / 9" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg" alt="avatar" />
+                <p class="text-xs font-bold">Animal</p>
+              </div>
+            </div>
+          </div>
+          <div class="w-full text-2xl font-bold">description</div>
+        </div>
+        <div class="flex items-end">
+          <img class="w-16 h-16 rounded" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg" alt="avatar" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
