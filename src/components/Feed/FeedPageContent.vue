3eb3299bdf4c97346403d4904e4c09bbbc1c9fd4
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2025-01-08 09:49:20 +0900

    Refactor Feed page layout and add update cards.
    Reorganized the Feed page structure by adjusting layout spacing and introducing styled update cards. Added elements like avatars, descriptions, and nested components for better user engagement. This enhances the visual presentation and readability of the feed.

--- src/components/Feed/FeedPageContent.vue
+++ src/components/Feed/FeedPageContent.vue
@@ -10,11 +10,23 @@ import iconMe from '@assets/footer/icon-me.png'
       <div class="flex gap-3 p-2 rounded-xl bg-yellow-400">
         <div class="flex flex-col flex-grow">
           <div class="flex items-center gap-3">
-            <img class="rounded-full w-14 h-14" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg" alt="avatar" />
+            <img
+              class="rounded-full w-14 h-14"
+              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg"
+              alt="avatar"
+            />
             <div class="flex flex-col flex-grow gap-0">
               <div class="text-xs font-bold">name</div>
-              <div class="flex items-center gap-2 p-1 rounded-sm bg-white" style="width: fit-content">
-                <img class="w-7 rounded-sm" style="aspect-ratio: 16 / 9" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg" alt="avatar" />
+              <div
+                class="flex items-center gap-2 p-1 rounded-sm bg-white"
+                style="width: fit-content"
+              >
+                <img
+                  class="w-7 rounded-sm"
+                  style="aspect-ratio: 16 / 9"
+                  src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg"
+                  alt="avatar"
+                />
                 <p class="text-xs font-bold">Animal</p>
               </div>
             </div>
@@ -22,7 +34,11 @@ import iconMe from '@assets/footer/icon-me.png'
           <div class="w-full text-2xl font-bold">description</div>
         </div>
         <div class="flex items-end">
-          <img class="w-16 h-16 rounded" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg" alt="avatar" />
+          <img
+            class="w-16 h-16 rounded"
+            src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg"
+            alt="avatar"
+          />
         </div>
       </div>
     </div>
