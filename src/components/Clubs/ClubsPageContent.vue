3e02afd74fc71b390a8d47d38af2cd999f855518
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2025-01-09 18:38:35 +0900

    Add Clubs page with assets and navigation updates
    Introduced a new Clubs page, including assets such as an MP4, SVG, and preview image. Updated navigation links in the footer and middleware to support the new route. Implemented page content with reusable components and styling.

--- src/components/Clubs/ClubsPageContent.vue
+++ src/components/Clubs/ClubsPageContent.vue
@@ -2,7 +2,6 @@
 import preview01 from '@assets/clubs/preview01.png'
 import arrowRight from '@assets/clubs/arrow-right.svg'
 import movie from '@assets/clubs/box.mp4'
-
 </script>
 
 <template>
@@ -15,24 +14,27 @@ import movie from '@assets/clubs/box.mp4'
       >
         <img
           class="w-44 rounded"
-          style="aspect-ratio: 1/1;"
-          :src="preview01.src" alt="Mystic Horizon" />
+          style="aspect-ratio: 1/1"
+          :src="preview01.src"
+          alt="Mystic Horizon"
+        />
         <p class="text-xs font-bold">Mystic Horizon</p>
       </a>
     </div>
     <div
       class="flex flex-col justify-center items-center gap-1 w-full border-dotted border-dp-white-400 border-4 rounded-xl"
-      style="aspect-ratio: 1/1;"
+      style="aspect-ratio: 1/1"
     >
       <p class="text-xs font-bold">More Upcoming Clubs Await.</p>
       <video
         class="w-24"
-        style="aspect-ratio: 1/1;"
-        src="/assets/box.mp4" autoplay loop></video>
+        style="aspect-ratio: 1/1"
+        src="/assets/box.mp4"
+        autoplay
+        loop
+      ></video>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
