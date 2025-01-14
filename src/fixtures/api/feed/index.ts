c8ba9637a1a36cd7d65c1e15bcf00398c137cb91
yashdesu <yash@devprotocol.xyz>
2025-01-14 11:41:36 +0530

    feat: fetch purchased assets in feed

---
+++ src/fixtures/api/feed/index.ts
@@ -0,0 +1,129 @@
+import { whenNotError, whenNotErrorAll } from '@devprotocol/util-ts'
+import { always } from 'ramda'
+import { createClient } from 'redis'
+import {
+  Index,
+  sTokenPayload as sTokenPayloadSchema,
+  type PassportItemDocument,
+} from '@devprotocol/clubs-plugin-passports'
+import { getDefaultClient } from '@fixtures/api/assets/redis'
+import { ACHIEVEMENT_ITEM_SCHEMA } from '@plugins/achievements/db/schema'
+import {
+  type AchievementItem,
+  type AchievementInfo,
+} from '@plugins/achievements/types'
+import {
+  generateKey,
+  AchievementPrefix,
+  AchievementIndex,
+} from '@plugins/achievements/utils'
+import { Index as AssetIndex } from '@fixtures/api/assets/redis'
+import type { AssetDocument } from '../assets/schema'
+import { type as assetTypeSchema } from '../assets/schema'
+
+const { REDIS_URL, REDIS_USERNAME, REDIS_PASSWORD } = import.meta.env
+
+export const getFeed = async () => {
+  const redis = await whenNotError(
+    createClient({
+      url: REDIS_URL,
+      username: REDIS_USERNAME ?? '',
+      password: REDIS_PASSWORD ?? '',
+    }),
+    (db) =>
+      db
+        .connect()
+        .then(always(db))
+        .catch((err) => new Error(err)),
+  )
+
+  const purchasedAssets = await whenNotErrorAll([redis], ([client]) =>
+    client.ft
+      .search(
+        AssetIndex.Asset,
+        `@${assetTypeSchema['$.type'].AS}:{passportItem}`,
+        {
+          SORTBY: {
+            BY: 'nBlock',
+            DIRECTION: 'DESC',
+          },
+          LIMIT: {
+            from: 0,
+            size: 10,
+          },
+        },
+      )
+      .then((res) =>
+        res.total && res.documents.length
+          ? res.documents.map((doc) => doc.value as AssetDocument)
+          : undefined,
+      )
+      .catch((err) => new Error(err)),
+  )
+
+  const result = await whenNotErrorAll([redis], ([client]) =>
+    client
+      .quit()
+      .then(always(purchasedAssets))
+      .catch((err) => new Error(err)),
+  )
+
+  return result
+}
+
+export const getSBTsForEOAFromClubsUrlHash = async (
+  clubsUrlHash: string,
+  clubsUrl: string,
+  eoa: string,
+) => {
+  const client = await getDefaultClient()
+  const getMetaData = async (
+    achievementInfoId: string,
+  ): Promise<AchievementInfo> => {
+    const achievementInfoDocument = (await whenNotErrorAll(
+      [achievementInfoId, client],
+      ([infoId, _client]) =>
+        _client.json
+          .get(generateKey(AchievementPrefix.AchievementInfo, infoId))
+          .catch((err: Error) => err),
+    )) as AchievementInfo
+    return achievementInfoDocument
+  }
+
+  const data: AchievementItem[] = await whenNotErrorAll(
+    [clubsUrlHash, eoa],
+    ([hash, addr]) =>
+      client.ft
+        .search(
+          AchievementIndex.AchievementItem,
+          `@${ACHIEVEMENT_ITEM_SCHEMA['$.clubsUrl'].AS}:{${hash}}@${ACHIEVEMENT_ITEM_SCHEMA['$.account'].AS}:{${addr}}`,
+          {
+            SORTBY: {
+              BY: ACHIEVEMENT_ITEM_SCHEMA['$.claimedOnTimestamp'].AS,
+              DIRECTION: 'DESC',
+            },
+          },
+        )
+        .then((res) => {
+          return JSON.parse(
+            JSON.stringify(res.documents.map(({ value }) => value)),
+          )
+        })
+        .catch((err: Error) => err),
+  )
+  const arrangeData = async (data: AchievementItem) => {
+    const achievementInfo = await getMetaData(data.achievementInfoId)
+    return {
+      name: achievementInfo.metadata.name,
+      url: `${clubsUrl}/achievement/${data.achievementDistId}`,
+      image: achievementInfo.metadata.image,
+    }
+  }
+  const finalData = await Promise.all(
+    data.map(async (item) => {
+      return await arrangeData(item)
+    }),
+  )
+  await client.quit()
+  return finalData
+}
