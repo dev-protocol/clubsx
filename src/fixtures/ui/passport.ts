d0fdb39f21a2221edf823be0dc8aec1e300b70eb
Aggre <hiroyuki.aggre@gmail.com>
2025-01-20 21:27:50 +0900

    add some css for debugging

--- src/fixtures/ui/passport.ts
+++ src/fixtures/ui/passport.ts
@@ -16,9 +16,8 @@ export const passportSpotlightClass = (i: 0 | 1 | 2 | 3 | number) => {
   return i === 1
     ? // when # of items == 1
       {
-        container:
-          'grid gap-1 lg:gap-4 items-center grid-cols-[2fr_3fr_2fr] grid-rows-[repeat(5,minmax(100px,1fr))]',
-        child: ['col-start-2 col-span-1 row-start-1 row-span-5'],
+        container: 'grid gap-1 lg:gap-4 items-center grid-cols-1 grid-rows-1',
+        child: ['row-start-1 row-span-5 max-w-2xl w-full mx-auto'],
       }
     : i === 2
       ? // when # of items == 2
