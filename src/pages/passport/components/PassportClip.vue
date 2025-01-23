f338309afeae8b5bc81f03e389053b7e4beca9f1
Aggre <hiroyuki.aggre@gmail.com>
2025-01-23 23:17:17 +0900

    apply masonry layout

--- src/pages/passport/components/PassportClip.vue
+++ src/pages/passport/components/PassportClip.vue
@@ -90,7 +90,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="@container/passport-asset w-full h-full">
+  <div class="@container/passport-asset">
     <div
       v-if="!!item"
       :id="elementId"
