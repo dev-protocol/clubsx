78a573c401e91df7e980af4680ee89bc71e0508a
Aggre <hiroyuki.aggre@gmail.com>
2025-01-23 23:22:24 +0900

    remove aspect-square

--- src/pages/passport/components/MediaCard.vue
+++ src/pages/passport/components/MediaCard.vue
@@ -48,7 +48,7 @@ onMounted(async () => {
         type === 'image-playable' ||
         type === 'image-playable-link')
     "
-    class="rounded-md w-full max-w-full object-cover aspect-square"
+    class="rounded-md w-full max-w-full"
     :class="props.class"
   />
 
