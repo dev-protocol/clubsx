43c1c2b90b79ffb96a6fe33296df6da45d3ff599
aggre <hiroyuki.aggre@gmail.com>
2025-01-26 18:38:00 +0900

    init

---
+++ src/components/Home/Frontpage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import FeedPageContent from '@components/Feed/FeedPageContent.vue'
+import type { FeedType } from '@fixtures/api/feed'
+
+const props = defineProps<{ feeds: FeedType[] }>()
+</script>
+
+<template>
+  <FeedPageContent :feeds="props.feeds" />
+</template>
