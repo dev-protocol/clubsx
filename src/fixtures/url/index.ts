74044e06fa239ae2bdb1e7e8f0f3ab2521345a4f
Aggre <hiroyuki.aggre@gmail.com>
2025-01-30 13:24:37 +0900

    fix replaceUrlConfigWithLocal

--- src/fixtures/url/index.ts
+++ src/fixtures/url/index.ts
@@ -1,15 +1,25 @@
 import type { ClubsConfiguration } from '@devprotocol/clubs-core'
 import { whenDefined } from '@devprotocol/util-ts'
 
-export const replaceWithFwdHost = (base: Request) => {
-  const url = new URL(base.url)
-  const forwarded = base.headers.get('forwarded') // i.g., for=12.345.67.890;host=xxx.clubs.place;proto=https;sig=0QmV…iOTE=;exp=1696430045
+/**
+ * Get original host name from forwarded header
+ * @param forwarded forwarded header i.g., for=12.345.67.890;host=xxx.clubs.place;proto=https;sig=0QmV…iOTE=;exp=1696430045
+ * @returns host name i.g., xxx.clubs.place
+ */
+const getHost = (forwarded: string): string | undefined => {
   const list = whenDefined(forwarded, (fwd) => fwd.split(';'))
   const regHeader = /^host=(.*)/i
-  const regParam = /\/sites_\/([a-z|0-9|-]+)\/?/i
   const host = whenDefined(list, (li) =>
     li.find((val) => regHeader.test(val)),
   )?.replace(regHeader, '$1')
+  return host
+}
+
+export const replaceWithFwdHost = (base: Request) => {
+  const url = new URL(base.url)
+  const forwarded = base.headers.get('forwarded')
+  const regParam = /\/sites_\/([a-z|0-9|-]+)\/?/i
+  const host = whenDefined(forwarded, getHost)
   const hostParam = regParam.test(url.href)
     ? `${url.href.match(regParam)?.[1]}.${url.host}`
     : undefined
@@ -33,18 +43,15 @@ export const replaceUrlConfigWithLocal = (
   url: URL,
   site?: string,
 ): ClubsConfiguration => {
-  console.log('$url$', req)
-  const forwarded = req.headers.get('forwarded') // i.g., for=12.345.67.890;host=xxx.clubs.place;proto=https;sig=0QmV…iOTE=;exp=1696430045
-  console.log('$forwarded$', forwarded)
+  const forwarded = req.headers.get('forwarded')
+  const reqHost = whenDefined(forwarded, getHost)
   const configUrl = new URL(config.url)
+  const hasReqHostTenant = hosts.every((h) => h !== reqHost) // If it's true, host name includes tenant name
   const newUrl =
-    configUrl.origin === url.origin
+    configUrl.host === reqHost
       ? config.url
-      : ((reqHost) =>
-          reqHost
-            ? config.url.replace(configUrl.host, `${site}.${reqHost}`)
-            : `${url.origin}/${site}`)(
-          hosts.find((h) => url.host.startsWith(`${site}.${h}`)),
-        )
+      : reqHost && hasReqHostTenant
+        ? config.url.replace(configUrl.host, reqHost)
+        : `${url.origin}/${site}`
   return { ...config, url: newUrl }
 }
