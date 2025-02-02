8a16f29055d060fd8b2ec7080dc8164f9e4cd238
aggre <hiroyuki.aggre@gmail.com>
2025-02-02 21:00:22 +0900

    wip: design tweaking for passport public page

---
+++ src/constants/passports.ts
@@ -0,0 +1,6 @@
+import type { PassportItem } from './types'
+
+export const DefaultSkin: PassportItem = {
+  itemAssetType: 'stylesheet-link',
+  itemAssetValue: 'https://dev-protocol.github.io/styles/skins/default.css',
+}
--- src/pages/og/image/passport/[eoa]/[passportId]/[...item]/components/Description.vue
+++ src/pages/og/image/passport/[eoa]/[passportId]/[...item]/components/Description.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { onMounted, useTemplateRef } from 'vue'
 import textFit from 'textfit'
+import { passportClass } from '@fixtures/ui/passport'
+import { ProseTextInherit } from '@devprotocol/clubs-core'
 
-const props = defineProps<{ text: string; html?: boolean }>()
+const props = defineProps<{ text: string; html?: boolean; class?: string }>()
 
 const wrapper = useTemplateRef('wrapper')
 
@@ -18,19 +20,27 @@ onMounted(() => {
     v-if="!props.html"
     ref="wrapper"
     class="wrapper text-center flex items-center"
+    :class="passportClass('description')"
   >
     {{ props.text }}
   </div>
   <div
     v-if="props.html"
-    ref="wrapper"
     v-html="props.text"
     class="wrapper text-center grid content-center items-center"
+    :class="{
+      [passportClass('description')]: true,
+      [ProseTextInherit]: true,
+      [props.class ?? '']: props.class,
+    }"
   ></div>
 </template>
 
 <style>
 .wrapper > * {
   font-weight: bold;
+}
+._p-description p {
+  font-size: inherit;
 }
 </style>
