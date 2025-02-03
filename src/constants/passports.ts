60bec5f31e56422e6fdc262b7fe48a6417a1d4b1
aggre <hiroyuki.aggre@gmail.com>
2025-02-03 11:05:19 +0900

    Passport ogp tweak (#3090)
    * wip: design tweaking for passport public page
    
    * fix type importing
    
    * update
    
    * new ogp design for passport page
    
    * update
    
    * fix linting error

---
+++ src/constants/passports.ts
@@ -0,0 +1,6 @@
+import type { PassportItem } from '@pages/passport/types'
+
+export const DefaultSkin: PassportItem = {
+  itemAssetType: 'stylesheet-link',
+  itemAssetValue: 'https://dev-protocol.github.io/styles/skins/default.css',
+}
