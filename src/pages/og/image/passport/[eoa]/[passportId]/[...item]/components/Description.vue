4927529fe5a7b3eed49a4fdac102c6ef96cd79fa
Aggre <hiroyuki.aggre@gmail.com>
2025-02-03 10:50:50 +0900

    new ogp design for passport page

--- src/pages/og/image/passport/[eoa]/[passportId]/[...item]/components/Description.vue
+++ src/pages/og/image/passport/[eoa]/[passportId]/[...item]/components/Description.vue
@@ -1,46 +1,49 @@
 <script setup lang="ts">
-import { onMounted, useTemplateRef } from 'vue'
+import { computed, onMounted, useTemplateRef } from 'vue'
 import textFit from 'textfit'
 import { passportClass } from '@fixtures/ui/passport'
-import { ProseTextInherit } from '@devprotocol/clubs-core'
 
-const props = defineProps<{ text: string; html?: boolean; class?: string }>()
+const {
+  text,
+  autofit = true,
+  class: className,
+} = defineProps<{ text: string; autofit?: boolean; class?: string }>()
 
 const wrapper = useTemplateRef('wrapper')
 
 onMounted(() => {
   console.log('xxxx')
-  if (wrapper.value) {
+  if (autofit && wrapper.value) {
     textFit(wrapper.value, { multiLine: true, minFontSize: 18 })
   }
 })
 </script>
 <template>
   <div
-    v-if="!props.html"
     ref="wrapper"
     class="wrapper text-center flex items-center"
-    :class="passportClass('description')"
+    :class="[
+      passportClass('description'),
+      { autofit: autofit },
+      { [className ?? '']: Boolean(className) },
+    ]"
   >
-    {{ props.text }}
+    <span v-if="!autofit">
+      {{ text }}
+    </span>
+    <template v-else>
+      {{ text }}
+    </template>
   </div>
-  <div
-    v-if="props.html"
-    v-html="props.text"
-    class="wrapper text-center grid content-center items-center"
-    :class="{
-      [passportClass('description')]: true,
-      [ProseTextInherit]: true,
-      [props.class ?? '']: props.class,
-    }"
-  ></div>
 </template>
 
 <style>
 .wrapper > * {
   font-weight: bold;
 }
-._p-description p {
+.wrapper:not(.autofit) > * {
   font-size: inherit;
+  font-weight: inherit;
+  font-family: inherit;
 }
 </style>
