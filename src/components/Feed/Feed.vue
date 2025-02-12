cae36225bddbd935d3509329fbdd805e29dc2b4a
aggre <hiroyuki.aggre@gmail.com>
2025-02-12 19:46:57 +0900

    Tweak the feed UI (#3129)
    * tweak the feed ui
    
    * install @devprotocol/clubs-core@3.25.3

--- src/components/Feed/Feed.vue
+++ src/components/Feed/Feed.vue
@@ -16,101 +16,98 @@ const assetLink = computed(
 
 <template>
   <div class="grid gap-2 p-2 h-full border-b boder-black/20">
-    <div class="flex flex-col gap-2">
-      <div class="grid grid-cols-[auto_1fr] items-center gap-3">
-        <a :href="`/passport/${address}`">
-          <img
-            class="w-12 h-12 rounded-full object-cover aspect-square bg-lightgray bg-cover bg-center bg-no-repeat _p-avatar"
-            :src="avatarSrc"
-            alt="avatar"
-          />
-        </a>
-        <div
-          :class="{
-            'grid grid-flow-col items-center justify-start gap-1': true,
-            'grid-rows-2': tag !== 'ugc',
-          }"
-        >
-          <span class="grid grid-flow-col items-center justify-start gap-2">
-            <span class="text-xs font-bold truncate">
-              {{ name }}
-            </span>
-            <span class="text-xs" v-if="props.parentPassport.description"
-              >·</span
-            >
-            <span class="text-xs truncate">
-              {{ props.parentPassport.description?.replace(/\\n/g, ' ') }}
-            </span>
+    <div class="grid grid-cols-[auto_1fr] items-center gap-3">
+      <a :href="`/passport/${address}`">
+        <img
+          class="w-12 h-12 rounded-full object-cover aspect-square bg-lightgray bg-cover bg-center bg-no-repeat _p-avatar"
+          :src="avatarSrc"
+          alt="avatar"
+        />
+      </a>
+      <div class="grid grid-flow-col items-center justify-start gap-1">
+        <span class="grid grid-flow-col items-center justify-start gap-2">
+          <span class="text-xs font-bold truncate">
+            {{ name }}
           </span>
-        </div>
+          <span class="text-xs" v-if="props.parentPassport.description">·</span>
+          <span class="text-xs truncate">
+            {{ props.parentPassport.description?.replace(/\\n/g, ' ') }}
+          </span>
+        </span>
       </div>
+    </div>
 
-      <div class="flex-grow grid gap-2 grid-cols-2 rounded">
-        <div class="flex flex-col gap-1">
-          <p
-            v-if="description"
-            class="text-2xl font-bold text-ellipsis overflow-hidden line-clamp-5 lg:line-clamp-8"
-          >
-            {{ description }}
-            <span
-              v-if="props.item.tags"
-              v-for="tag in props.item.tags"
-              class="text-sm text-violet-500 mr-1 last:mr-0"
-              >#{{ tag }}</span
-            >
-          </p>
-          <div
-            v-if="tag !== 'ugc'"
-            class="flex items-center gap-2 p-1 rounded-sm bg-white"
-            style="width: fit-content"
-          >
-            <img
-              v-if="!!badgeSrc && badgeSrc !== ''"
-              class="w-7 rounded-sm"
-              style="aspect-ratio: 16 / 9"
-              :src="badgeSrc"
-              alt="image"
-            />
-            <p v-if="!!badgeName && badgeName !== ''" class="text-xs font-bold">
-              {{ badgeName }}
-            </p>
-          </div>
-        </div>
-        <a
-          :href="assetLink"
-          target="_blank"
-          class="flex items-end block"
-          :class="{ 'p-3 rounded': frameHexColor }"
-          :style="
-            frameHexColor
-              ? {
-                  backgroundColor: frameHexColor,
-                }
-              : undefined
-          "
+    <div class="flex-grow grid gap-2 grid-cols-2 rounded">
+      <div class="flex flex-col gap-1">
+        <p
+          v-if="description"
+          class="text-2xl font-bold text-ellipsis overflow-hidden line-clamp-5 lg:line-clamp-8"
         >
-          <div v-if="tag !== 'ugc'">
-            <MediaCard
-              class="w-full rounded overflow-hidden"
-              :src="assetSrc"
-              :type="tag as PassportItemAssetType"
-              :found="!!assetSrc"
-            />
-          </div>
-          <div
-            v-if="tag === 'ugc'"
-            class="media-wrapper p-2 rounded-xl bg-violet-50 h-full w-full flex items-center"
+          {{ description }}
+          <span
+            v-if="props.item.tags"
+            v-for="tag in props.item.tags"
+            class="text-sm text-violet-500 mr-1 last:mr-0"
+            >#{{ tag }}</span
           >
-            <MediaEmbed
-              class="w-full rounded-xl mx-auto max-h-40 lg:max-h-48 lg:max-w-xs pointer-events-none overflow-hidden"
-              :found="!!assetSrc"
-              :src="assetSrc"
-              :type="tag"
-              :autoplay="false"
-            />
-          </div>
+        </p>
+        <a
+          v-if="tag !== 'ugc'"
+          :href="clubLink"
+          class="flex items-center gap-2 p-1 rounded-sm bg-white w-fit"
+        >
+          <img
+            v-if="!!badgeSrc && badgeSrc !== ''"
+            class="w-7 rounded-sm"
+            style="aspect-ratio: 16 / 9"
+            :src="badgeSrc"
+            alt="image"
+          />
+          <p v-if="!!badgeName && badgeName !== ''" class="text-xs font-bold">
+            {{ badgeName }}
+          </p>
         </a>
       </div>
+      <a
+        :href="assetLink"
+        target="_blank"
+        class="flex items-end block"
+        :class="{ 'p-3 rounded': frameHexColor }"
+        :style="
+          frameHexColor
+            ? {
+                backgroundColor: frameHexColor,
+              }
+            : undefined
+        "
+      >
+        <div v-if="tag !== 'ugc'" class="w-full h-full">
+          <MediaCard
+            class="w-full h-full mx-auto !object-contain object-contain! lg:max-w-xs pointer-events-none overflow-hidden"
+            :class="
+              frameHexColor
+                ? `max-h-[calc(10rem_-1.5rem)] lg:max-h-[calc(12rem-_1.5rem)]`
+                : 'max-h-40 lg:max-h-48'
+            "
+            :src="assetSrc"
+            :type="tag as PassportItemAssetType"
+            :found="!!assetSrc"
+            :video-class="`w-full h-full`"
+          />
+        </div>
+        <div
+          v-if="tag === 'ugc'"
+          class="media-wrapper p-2 rounded-xl bg-violet-50 h-full w-full flex items-center"
+        >
+          <MediaEmbed
+            class="w-full h-full rounded-xl mx-auto max-h-40 lg:max-h-48 lg:max-w-xs pointer-events-none overflow-hidden"
+            :found="!!assetSrc"
+            :src="assetSrc"
+            :type="tag"
+            :autoplay="false"
+          />
+        </div>
+      </a>
     </div>
   </div>
 </template>
--- src/pages/passport/components/MediaCard.vue
+++ src/pages/passport/components/MediaCard.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
-import { defineProps, onMounted, ref } from 'vue'
+import { computed, defineProps, onMounted, ref } from 'vue'
 import Skeleton from '@components/Global/Skeleton.vue'
 
-import VideoFetch from './VideoFetch.vue'
+import { VideoFetch } from '@devprotocol/clubs-core/ui/vue'
 
 import type { PassportItemIndexDoc } from '../types'
 
@@ -12,23 +12,29 @@ const props = defineProps<{
   posterSrc?: string
   type: PassportItemIndexDoc['itemAssetType']
   class?: string
+  videoClass?: string
 }>()
 
 const imageRef = ref<HTMLImageElement | null>(null)
+const backdropRef = ref<HTMLImageElement | null>(null)
+const isImageType = computed(
+  () =>
+    props.type === 'image' ||
+    props.type === 'image-link' ||
+    props.type === 'image-playable' ||
+    props.type === 'image-playable-link',
+)
 
 onMounted(async () => {
   try {
     const response = await fetch(props.src)
     const blob = await response.blob()
     const blobDataUrl = URL.createObjectURL(blob)
-    if (
-      (props.type === 'image' ||
-        props.type === 'image-link' ||
-        props.type === 'image-playable' ||
-        props.type === 'image-playable-link') &&
-      imageRef.value
-    ) {
+    if (isImageType.value && imageRef.value && backdropRef.value) {
       imageRef.value.src = blobDataUrl ? blobDataUrl : (props.posterSrc ?? '')
+      backdropRef.value.src = blobDataUrl
+        ? blobDataUrl
+        : (props.posterSrc ?? '')
     }
   } catch (error) {
     console.error('Error loading video:', error)
@@ -37,35 +43,87 @@ onMounted(async () => {
 </script>
 
 <template>
-  <!-- Image type clip -->
-  <img
-    alt="Passport clip"
-    ref="imageRef"
-    v-if="
-      found &&
-      (type === 'image' ||
-        type === 'image-link' ||
-        type === 'image-playable' ||
-        type === 'image-playable-link')
-    "
-    class="rounded-md w-full max-w-full object-cover [&:not([src])]:aspect-square"
-    :class="props.class"
-  />
+  <div
+    :class="{
+      'relative rounded-md outline-2 outline-offset-2 outline outline-solid outline-violet-500': false /* trial 1 */,
+      'relative overflow-hidden rounded-md': true /* trial 2, 3 */,
+    }"
+  >
+    <img
+      alt="Passport clip backdrop"
+      ref="backdropRef"
+      v-if="found && isImageType"
+      class="absolute w-full h-full blur-sm origin-center scale-150"
+    />
+    <span
+      :class="{
+        'text-[0.65rem] px-1 rounded-t-md bg-violet-500 text-white absolute left-2 -top-[calc(4px_+_1lh)] flex items-center gap-1': false /* trial 1 */,
+        'z-10 text-xs px-1 rounded-md border border-white/30 bg-black/30 backdrop-blur-sm text-white absolute left-1 top-1 flex items-center gap-1': false /* trial 2 */,
+        'z-10 text-xs px-1 rounded-md outline outline-solid outline-1 outline-white/80 __bg__ text-white absolute left-1 top-1 flex items-center gap-1': true /* trial 3 */,
+      }"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        fill="currentColor"
+        class="size-3"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M15 8c0 .982-.472 1.854-1.202 2.402a2.995 2.995 0 0 1-.848 2.547 2.995 2.995 0 0 1-2.548.849A2.996 2.996 0 0 1 8 15a2.996 2.996 0 0 1-2.402-1.202 2.995 2.995 0 0 1-2.547-.848 2.995 2.995 0 0 1-.849-2.548A2.996 2.996 0 0 1 1 8c0-.982.472-1.854 1.202-2.402a2.995 2.995 0 0 1 .848-2.547 2.995 2.995 0 0 1 2.548-.849A2.995 2.995 0 0 1 8 1c.982 0 1.854.472 2.402 1.202a2.995 2.995 0 0 1 2.547.848c.695.695.978 1.645.849 2.548A2.996 2.996 0 0 1 15 8Zm-3.291-2.843a.75.75 0 0 1 .135 1.052l-4.25 5.5a.75.75 0 0 1-1.151.043l-2.25-2.5a.75.75 0 1 1 1.114-1.004l1.65 1.832 3.7-4.789a.75.75 0 0 1 1.052-.134Z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      Official</span
+    >
+    <!-- Image type clip -->
+    <img
+      alt="Passport clip"
+      ref="imageRef"
+      v-if="found && isImageType"
+      class="relative rounded-md w-full max-w-full object-cover [&:not([src])]:aspect-square"
+      :class="props.class"
+    />
 
-  <!-- Short video type clip -->
-  <VideoFetch
-    v-if="found && type.includes('short-video')"
-    :url="src"
-    :posterUrl="posterSrc ?? ''"
-    :class="class"
-    :videoClass="`rounded-md w-full max-w-full object-cover aspect-square pointer-events-none`"
-    :isControlled="
-      type === 'short-video-controlled' ||
-      type === 'short-video-controlled-link'
-    "
-  />
+    <!-- Short video type clip -->
+    <VideoFetch
+      v-if="found && type.includes('short-video')"
+      :url="src"
+      :posterUrl="posterSrc ?? ''"
+      class="relative"
+      :class="class"
+      :videoClass="[
+        `rounded-md w-full max-w-full object-cover aspect-square pointer-events-none`,
+        { [videoClass ?? '']: Boolean(videoClass) },
+      ]"
+      :isControlled="
+        type === 'short-video-controlled' ||
+        type === 'short-video-controlled-link'
+      "
+    />
 
-  <div v-if="!found || !props.src" class="w-full aspect-square">
-    <Skeleton />
+    <div v-if="!found || !props.src" class="w-full aspect-square">
+      <Skeleton />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.__bg__ {
+  background: radial-gradient(
+      ellipse farthest-corner at right bottom,
+      #fedb37 0%,
+      #fdb931 8%,
+      #9f7928 30%,
+      #8a6e2f 40%,
+      transparent 80%
+    ),
+    radial-gradient(
+      ellipse farthest-corner at left top,
+      #ffffff 0%,
+      #ffffac 8%,
+      #d1b464 25%,
+      #5d4a1f 62.5%,
+      #5d4a1f 100%
+    );
+}
+</style>
