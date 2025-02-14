f0cda997d4b1acc7004de2c872e9c6b90806448c
Aggre <hiroyuki.aggre@gmail.com>
2025-02-14 14:42:03 +0900

    add passport class for the passport switcher

--- src/pages/passport/components/SkinSwitch.svelte
+++ src/pages/passport/components/SkinSwitch.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { passportClass } from '@fixtures/ui/passport'
   import type { Skin } from '@pages/api/profile'
 
   export let eoa: string = ''
@@ -13,34 +14,39 @@
   }
 </script>
 
-<div class="relative">
-  <button
-    class="flex items-center justify-center gap-2 hs-button is-outlined !border border! w-fit"
-    disabled={!skins?.length}
-    on:click={() => toggleDropdownVisibility()}
-  >
-    {#if skins.length > 1}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="size-4"
-        viewBox="0 0 24 24"
-        fill="none"
+<div class={`relative ${passportClass('passport-switcher-container')}`}>
+  <span class={passportClass('passport-switcher-content')}>
+    <button
+      class={`flex items-center justify-center gap-2 hs-button is-outlined !border border! w-fit ${passportClass('passport-switcher')}`}
+      disabled={!skins?.length}
+      on:click={() => toggleDropdownVisibility()}
+    >
+      {#if skins.length > 1}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="size-4"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M3.75 6.75H20.25M3.75 12H20.25M3.75 17.25H20.25"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      {/if}
+      <p
+        class={`font-bold line-clamp-1 ${passportClass('passport-switcher-label')}`}
       >
-        <path
-          d="M3.75 6.75H20.25M3.75 12H20.25M3.75 17.25H20.25"
-          stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
-    {/if}
-    <p class="font-bold line-clamp-1">
-      {selectedSkinId
-        ? (skins?.find((item) => item.id === selectedSkinId)?.name ?? 'Default')
-        : (skins?.at(0)?.name ?? 'Default')}
-    </p>
-  </button>
+        {selectedSkinId
+          ? (skins?.find((item) => item.id === selectedSkinId)?.name ??
+            'Default')
+          : (skins?.at(0)?.name ?? 'Default')}
+      </p>
+    </button>
+  </span>
 
   {#if isDropdownVisible}
     <div
